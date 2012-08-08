250cb8982db7168fbd43b3e7574374eeed4ab631
johanlofberg <johanl@isy.liu.se>
2012-08-08 12:46:59 +0200

    Preparing for refactoring for issue #7

---
+++ extras/solverCapable.m
@@ -0,0 +1,13 @@
+function featureSupported = solverCapable(solverList,solverName,feature);
+
+i = 1;
+featureSupported = 1;
+while i <= length(solverList)
+    if strcmpi([solverList(i).tag '-' solverList(i).version],solverName) | strcmpi(solverList(i).tag,solverName)
+        % dynamic fields not supported in old versions...
+        featureSupported = eval(['solverList(i).' feature]);
+        return
+    end
+    i = i+1;
+end
+            
