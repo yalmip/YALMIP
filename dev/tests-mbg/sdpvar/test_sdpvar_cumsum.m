b268ca8e036c88a9852d3bbdeb5d036b56993ddf
johanlofberg <johanl@isy.liu.se>
2012-08-12 08:33:51 +0200

    Forgot to save after final change

--- dev/tests-mbg/sdpvar/test_sdpvar_cumsum.m
+++ dev/tests-mbg/sdpvar/test_sdpvar_cumsum.m
@@ -1,8 +1,8 @@
 function test_sdpvar_cumsum
 
+dx = sdpvar(1,2);
 
-sdpvar t
+cs1 = [0 cumsum(dx) 1];
+cs2 = [0 dx(1) dx(1)+dx(2) 1];
 
-p = 1+t^2;
-p2 = replace(p,t,2*t);
-mbg_asserttrue(isequal(p2-(1+4*t^2),0))
+mbg_asserttrue(isequal(cs1-cs2),[0 0 0 1])
