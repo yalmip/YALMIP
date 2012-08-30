0786b48ce2a8c00fca6caa389a29eaba1371746b
johanlofberg <johanl@isy.liu.se>
2012-08-30 13:24:19 +0200

    Assertion test a bit too tight numerically

--- dev/tests-mbg/other/test_kktqp.m
+++ dev/tests-mbg/other/test_kktqp.m
@@ -4,4 +4,4 @@ Q = magic(5);
 x = sdpvar(5,1);
 solvesdp([-1 <= x <= 1],x'*Q*x,sdpsettings('solver','kktqp'))
 
-mbg_asserttolequal(double(OO), -80.9412, 1e-3);
+mbg_asserttolequal(double(OO), -80.9412, 1e-2);
