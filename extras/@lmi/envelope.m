2d4da303f95010c3b53e496f30cfc1a585ad1192
johanlofberg <johanl@isy.liu.se>
2012-09-19 08:39:42 +0200

    Add support for non-projected output

--- extras/@lmi/envelope.m
+++ extras/@lmi/envelope.m
@@ -1,4 +1,37 @@
 function [E,P] = envelope(C,x)
+%ENVELOPE Create linear approximation of envelope of nonlinear constraint
+%
+%     [E,P] = envelope(C,x)
+%
+% C: Constraint object involving nonlinear expresions
+% x: Optional: The linear variables of interest (projection onto these will be performed)
+% E: Constraint object representing the envelope approximation
+% P: Optional: Polyhedral object representing E (MPT Toolbox object)
+%
+% Examples
+%
+% In order to derive the outer approximation in the original linear
+% variables, the model has to be projected onto the linear variables
+% This projetion based format requires MPT3
+%   sdpvar x u
+%   E = envelope([-1 <= x <= 1, u == x^3],[x;u]);
+%   plot(E)
+%   xx = (-1:0.01:1);hold on;plot(xx,xx.^3)
+%
+%   E = envelope([-1 <= x <= 1,x+sin(pi*x) <= u <= 4-x^2],[x;u]);
+%   plot(E)
+%   xx = (-1:0.01:1);hold on;plot(xx,xx+sin(pi*xx),xx,4-xx.^2)
+%
+% Alternatively, we can create a model which adds the outer approximation
+% cuts for the envelopes of the nonlinear variables, but keep the nonlinear
+% variables, and then plot the projection of the outer approximation,
+% keeping in mind that we now have to relax the nonlinear variables (this
+% is the model which thus would be used in a branch&bound scheme)
+%   E = envelope([-1 <= x <= 1,x+sin(pi*x) <= u <= 4-x^2]);
+%   plot(E,[x;u],[],[],sdpsettings('relax',1))
+%   xx = (-1:0.01:1);hold on;plot(xx,xx+sin(pi*xx),xx,4-xx.^2)
+
+% Author Johan Löfberg
 
 [aux1,aux2,aux3,p] = export(C,[],sdpsettings('solver','bmibnb'));
 
@@ -12,28 +45,11 @@ end
 % Copied from bmibnb
 p.high_monom_model=[];
 p.originalModel = p;
-p = presolve_bounds_from_domains(p);
-p = presolve_bounds_from_modelbounds(p);
-p = presolve_bounds_from_quadratics(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
-p = presolve_bounds_from_equalities(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
+
+p = preSolveOneMagicRound(p);   
 p = convert_sigmonial_to_sdpfun(p);
 [p,changed] = convert_polynomial_to_quadratic(p);
-p = presolve_bounds_from_modelbounds(p);
-p = presolve_bounds_from_quadratics(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
-p = presolve_bounds_from_equalities(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
-p = update_eval_bounds(p);
-p = update_monomial_bounds(p);
-
+p = preSolveOneMagicRound(p);  
 p = compile_nonlinear_table(p);
 
 % Copied from solvelower
@@ -68,11 +84,15 @@ if nargin > 1
     b = b(1+p_cut.K.f:end,:);
     P = Polyhedron('A',A,'b',b,'Ae',Ae,'be',be);
     P = projection(P,1:length(xi));
-    
-   % P = projection(polytope([Akeep A],b),1:length(xi));
     E = ismember(x,P);
 else
-    E = p_cut.F_struc*[1;recover(p_cut.used_variables)]>=0;
+    z = recover(p_cut.used_variables);
+    % We might have introduced some local modelling variables here
+    m = size(p.F_struc,2)-1-length(z);
+    if m>0
+        z = [z;sdpvar(m,1)];
+    end
+    E = p_cut.F_struc*[1;z]>=0;
 end
 
 function p = mergeBoundsToModel(p);
