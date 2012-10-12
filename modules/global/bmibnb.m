7aca544a44769b5848a2faaf82f238a18c52b491
johanlofberg <johanl@isy.liu.se>
2012-10-12 12:29:49 +0200

    Turn on bound-propagation from inequalities which was turned off for some reason

--- modules/global/bmibnb.m
+++ modules/global/bmibnb.m
@@ -108,7 +108,7 @@ end
 p = presolve_bounds_from_equalities(p); 
 p = update_eval_bounds(p);
 p = update_monomial_bounds(p);
-%p = presolve_bounds_from_inequalities(p);
+p = presolve_bounds_from_inequalities(p);
 p = update_eval_bounds(p);
 p = update_monomial_bounds(p);
 
