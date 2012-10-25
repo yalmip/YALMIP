e928092c58c439174dbbf890dab1fcea3feab4b9
johanlofberg <johanl@isy.liu.se>
2012-10-25 08:50:43 +0200

    Commented code

--- modules/global/presolve_bounds_from_quadratics.m
+++ modules/global/presolve_bounds_from_quadratics.m
@@ -1,7 +1,11 @@
 function p = presolve_bounds_from_quadratics(p)
+% Any elementwise constraints?
 if p.K.l >0
+    % Any quadratic terms in model?
     if any(p.variabletype==2)
         for i = 1:p.K.l
+            % Look for sum U >= sum(a_ix_i^2) where a_i>=0
+            % Use it for x_i^2 <= U/a_i
             U = p.F_struc(p.K.f+i,1);
             if U>=0
                 [aux,col,val] = find(p.F_struc(p.K.f+i,2:end));
