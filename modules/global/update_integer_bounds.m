5f7dc28c70586e7a8aced64bba3a50252b8145ed
johanlofberg <johanl@isy.liu.se>
2012-10-25 08:55:47 +0200

    Extracted new function to take care of bound updates for integer valued variables

---
+++ modules/global/update_integer_bounds.m
@@ -0,0 +1,16 @@
+function model = update_integer_bounds(model);
+
+if ~isempty(model.integer_variables)
+    model.lb(model.integer_variables) = fix(model.lb(model.integer_variables)-1e-8);
+    model.ub(model.integer_variables) = fix(model.ub(model.integer_variables)+1e-8);
+end
+if ~isempty(model.binary_variables)
+    model.lb(model.binary_variables) = fix(model.lb(model.binary_variables)-1e-8);
+    model.ub(model.binary_variables) = fix(model.ub(model.binary_variables)+1e-8);
+end
+if any(model.lb(model.binary_variables) > model.ub(model.binary_variables))
+    model.feasible = 0;
+end
+if any(model.lb(model.integer_variables) > model.ub(model.integer_variables))
+    model.feasible = 0;
+end
