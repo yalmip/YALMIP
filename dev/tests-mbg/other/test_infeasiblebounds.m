7d606b279ad854713863c6e1e46e59d06ccf644b
johanlofberg <johanl@isy.liu.se>
2012-10-26 12:24:32 +0200

    Removed useless test

--- dev/tests-mbg/other/test_infeasiblebounds.m
+++ dev/tests-mbg/other/test_infeasiblebounds.m
@@ -5,9 +5,6 @@ sdpvar a x
 
 F = [ 0 <= x <= 1; a >= 0; a == -0.5 ];
 
-sol = solvesdp(F, x^3)
-mbg_asserttrue(sol.problem == 1);
-
 sol = solvesdp(F, x^3,sdpsettings('solver','bmibnb'))
 mbg_asserttrue(sol.problem == 1);
 
