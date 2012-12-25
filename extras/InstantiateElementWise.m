790b36489d9ce9af9592ad14ef2905673ad7a9e6
johanlofberg <johanl@isy.liu.se>
2012-12-25 16:46:07 +0100

    Generic fix for nonlinear operator applied to sparse matrix. Closes #51

--- extras/InstantiateElementWise.m
+++ extras/InstantiateElementWise.m
@@ -7,9 +7,23 @@ else
     args = varargin{1};
     dims = size(args);
     args = args(:);
+    Base = getbase(args);
+    iD = ~any(Base(:,2:end),2);
+    iS = any(Base(:,2:end),2);
+    if nnz(iD)>0
+        iD = find(iD);
+        varargin{1} = args(iD);
+        yDoubles = feval(funName,varargin{:});    
+    else
+        yDoubles = [];
+        iD = [];
+    end
+    iS = find(iS);
+    args = args(iS);
     for i = 1:length(args)
         varargin{1} = args(i);
-        y = [y;yalmip('define',funName,varargin{:})];
+        y = [y;yalmip('define',funName,varargin{:})];    
     end
+    y = sparse([iD;iS],ones(length(iD)+length(iS),1),[yDoubles;y],prod(dims),1);
     y = reshape(y,dims);
 end
