c6410f9d396f7ed42377c6b61226053c1f15a865
johanlofberg <johanl@isy.liu.se>
2013-01-10 14:20:12 +0100

    Add cut machine which multiplies linear equalities with monom

---
+++ modules/global/addMultipliedEqualityCuts.m
@@ -0,0 +1,39 @@
+function p = addMultipliedEqualityCuts(p)
+
+if ~p.options.bmibnb.cut.multipliedequality
+    return;
+end
+
+for i =  p.linears
+    newRows = [];
+    for j = 1:p.K.f
+        row = p.F_struc(j,:);
+        used = find(row(2:end));
+        if ~any(p.variabletype(used))
+            % Linear constraint
+            ok = 1;
+            for k = 1:length(used)
+                find1 = findrows(p.bilinears(:,2:end),[i used(k)]);
+                find2 = findrows(p.bilinears(:,2:end),[used(k) i]);
+                if (isempty(find1) & isempty(find2))
+                    ok = 0;
+                    break
+                else
+                    monom(k) = [find1 find2];
+                end
+            end
+            if ok
+                monom = p.bilinears(monom,1);
+                row(1+monom) = row(1+used);
+                row(1+used) = 0;
+                row(i) = row(1);
+                row(1)=0;
+                newRows = [newRows;row];
+            end
+        end                    
+    end
+    if ~isempty(newRows)
+        p.F_struc = [newRows;p.F_struc];
+        p.K.f = p.K.f + size(newRows,1);
+    end
+end
