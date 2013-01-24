565363d86845b9ae1b8eac6c867941e5fe9d7ca0
johanlofberg <johanl@isy.liu.se>
2013-01-24 09:17:52 +0100

    Removed unused code which killed performance

--- extras/append_normalized_socp.m
+++ extras/append_normalized_socp.m
@@ -1,9 +1,6 @@
 function [F_struc,K,c,Q,UB,LB] = append_normalized_socp(F_struc,K,c,Q,UB,LB)
 
 if K.q(1)>0
-    n_original = length(c);
-    
-    
     % To simplify code, we currently normalize everything to z'*z<x0^2
     nNEW = sum(K.q);
     
@@ -14,26 +11,12 @@ if K.q(1)>0
     c = [c;spalloc(nNEW,1,0)];
     Q = blkdiag(Q,spalloc(nNEW,nNEW,0));
     
-    iCone = n_original+1;
     ri = zeros(1,length(K.q));
-    Li = spalloc(n_original+nNEW,length(K.q),0);
-    n_TOT = n_original+nNEW;
-    for i = 1:length(K.q);
-        ind = iCone:iCone+K.q(i)-1;
-        vals = [-1 ones(1,K.q(i)-1)];
-        aux = sparse(ind,1,vals,n_TOT,1);
-        aux = diag(aux);
-        Qi{i} = aux;
+    for i = 1:length(K.q);   
         LB = [LB;0;-inf(K.q(i)-1,1)];
-        iCone = iCone + K.q(i);
     end
     F_struc = [F_strucSOCP;F_struc];
-    K.f = K.f + nNEW;
-    
-else
-    Qi = [];
-    ri = [];
-    Li = [];
+    K.f = K.f + nNEW;    
 end
 
 
