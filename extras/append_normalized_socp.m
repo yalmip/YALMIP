811e6ab3b9354a1c3bdad5beaefe10529c8597fd
johanlofberg <johanl@isy.liu.se>
2013-01-24 10:37:12 +0100

    Clean up

--- extras/append_normalized_socp.m
+++ extras/append_normalized_socp.m
@@ -4,31 +4,31 @@ if K.q(1)>0
     % To simplify code, we currently normalize everything to z'*z<z0^2
     % This done by writting [c^Tx+d;Ax+b] in cone as
     % [c^Tx+d;Ax+b]==[z0;z],  [z0;z] in cone
-    nNEW = sum(K.q);
-    nORIGINAL = length(c);
+    nNew = sum(K.q);
+    nOriginal = length(c);
     Ftemp = F_struc(1+K.f+K.l:end,:);
-    F_strucSOCP = [Ftemp -speye(nNEW)];
-    F_struc = [F_struc(1:K.f+K.l,:) spalloc(K.f+K.l,nNEW,0)];
+    F_strucSOCP = [Ftemp -speye(nNew)];
+    F_struc = [F_struc(1:K.f+K.l,:) spalloc(K.f+K.l,nNew,0)];
     F_struc = [F_strucSOCP;F_struc];
-    K.f = K.f + nNEW;
-    c = [c;spalloc(nNEW,1,0)];
-    Q = blkdiag(Q,spalloc(nNEW,nNEW,0));
-    UB = [UB;inf(nNEW,1)];
+    K.f = K.f + nNew;
+    c = [c;spalloc(nNew,1,0)];
+    Q = blkdiag(Q,spalloc(nNew,nNew,0));
+    UB = [UB;inf(nNew,1)];
     for i = 1:length(K.q);
         LB = [LB;0;-inf(K.q(i)-1,1)];
     end
     if nargout > 6
         % Cplex interface needs explicit representations of z'Q*z+Lz+r
-        iCone = nORIGINAL+1;
+        top = nOriginal+1;
         ri = zeros(1,length(K.q));
-        Li = spalloc(nORIGINAL+nNEW,length(K.q),0);
-        nTOT = nORIGINAL+nNEW;
+        Li = spalloc(nOriginal+nNew,length(K.q),0);
+        nTotal = nOriginal+nNew;
         for i = 1:length(K.q);
-            ind = iCone:iCone+K.q(i)-1;
+            ind = top:top+K.q(i)-1;
             vals = [-1 ones(1,K.q(i)-1)];
-            aux = diag(sparse(ind,1,vals,nTOT,1));
+            aux = diag(sparse(ind,1,vals,nTotal,1));
             Qi{i} = aux;
-            iCone = iCone + K.q(i);
+            top = top + K.q(i);
         end
     end
 else
