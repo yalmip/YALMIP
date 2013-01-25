8f53903398d7dc9f05fd8584e79b4ee114789bd6
johanlofberg <johanl@isy.liu.se>
2013-01-25 08:40:42 +0100

    Refactoring to simply second call if unsure about unbounded/infeasible

--- solvers/call_cplexibm_miqp.m
+++ solvers/call_cplexibm_miqp.m
@@ -58,7 +58,6 @@ ctype(binary_variables)  = 'B';  % Should not happen except from bmibnb
 ctype(setdiff(semicont_variables,integer_variables)) = 'S';  % Should not happen except from bmibnb
 ctype(intersect(semicont_variables,integer_variables)) = 'N';
 
-solvertime = clock;
 options.cplex.simplex.display = options.verbose;
 options.cplex.mip.display = options.verbose;
 options.cplex.barrier.display = options.verbose;
@@ -89,15 +88,115 @@ end
 if options.savedebug
     save cplexdebug
 end
+solvertime = clock;
+[x,fval,exitflag,output,lambda] = localSolverCall(H,f,Aineq,bineq,Aeq,beq,lb,ub,x0,options,integer_variables,binary_variables,semicont_variables,K,ctype);
+if output.cplexstatus == 4 | output.cplexstatus == 119
+    % CPLEX reports infeasible OR unbounded
+    % Remove objective and resolve
+    H = H*0;
+    f = f*0;
+    [x,fval,exitflag,output,lambda] = localSolverCall(H,f,Aineq,bineq,Aeq,beq,lb,ub,x0,options,integer_variables,binary_variables,semicont_variables,K,ctype);
+    switch output.cplexstatus
+        case {1,101,102} % It was ok, hence it must have been unbounded
+            output.cplexstatus = 2;
+        case {3,103,106} % Infeasible, so original was infeasible
+            output.cplexstatus = 3; % Infeasible
+        otherwise
+            output.cplexstatus = 4; % I give up
+    end
+end
+if interfacedata.getsolvertime solvertime = etime(clock,solvertime);else solvertime = 0;end
 
-% Bug in older versions of CPLEX
+% Inconstency in early version of CPLEX
+if str2num(interfacedata.solver.subversion)>=12.3
+    the_sign = 1;
+else
+    the_sign = -1;
+end
+if ~isempty(lambda)
+    D_struc = [the_sign*lambda.eqlin;the_sign*lambda.ineqlin];
+else
+    D_struc = [];    
+end
+
+if length(x) == length(f)
+    if ~isempty(NegativeSemiVar)
+        x(NegativeSemiVar) = -x(NegativeSemiVar);
+    end
+end
+
+showprogress('Calling CPLEX-IBM',options.showprogress);
+% Check, currently not exhaustive...
+switch output.cplexstatus
+    case {1,101,102}
+        problem = 0;
+    case {3,103,106}
+        problem = 1; % Infeasible
+    case {2,20,21,118}
+        problem = 2; % Unbounded
+    case {4,119}
+        problem = 15;    
+    case {10,11,104,105,107,108,111,112}
+        problem = 3; % Iteration/time
+    case 119
+        problem = 15;
+    case {5,6,109,110}
+        problem = 4; % Numerics
+    otherwise
+        problem = -1;
+end
+
+infostr = yalmiperror(problem,'CPLEX-IBM');
+
+% Save all data sent to solver?
+if options.savesolverinput
+    solverinput.H = H;
+    solverinput.f = f;
+    solverinput.Aineq = Aineq;
+    solverinput.Aineq = Aeq;
+    solverinput.bineq = bineq;
+    solverinput.beq = beq;
+    solverinput.LB = lb;
+    solverinput.UB = ub;
+    solverinput.x0 = x0;
+    solverinput.ctype = ctype;
+    solverinput.options = options.cplex;
+else
+    solverinput = [];
+end
+
+% Save all data from the solver?
+if options.savesolveroutput
+    solveroutput.x = x;
+    solveroutput.fval = fval;
+    solveroutput.exitflag = exitflag;
+    solveroutput.lambda = lambda;
+else
+    solveroutput = [];
+end
+
+if isempty(x)
+    x = zeros(length(f),1);
+end
+% Standard interface
+output.Primal      = x(:);
+output.Dual        = D_struc;
+output.Slack       = [];
+output.problem     = problem;
+output.infostr     = infostr;
+output.solverinput = solverinput;
+output.solveroutput= solveroutput;
+output.solvertime  = solvertime;
+
+
+
+function [x,fval,exitflag,output,lambda] = localSolverCall(H,f,Aineq,bineq,Aeq,beq,lb,ub,x0,options,integer_variables,binary_variables,semicont_variables,K,ctype);
 fixedAineqBug = 0;
 if isempty(Aineq) & isempty(Aeq)
     Aineq = zeros(1,length(f)) ;
     bineq = 1;
     fixedAineqBug = 1;
 end
-
 if isempty(integer_variables) & isempty(binary_variables) & isempty(semicont_variables) & isempty(K.sos.type)
     if options.verbose
         if isempty(H)
@@ -132,118 +231,3 @@ else
     end
     lambda = [];
 end
-if interfacedata.getsolvertime solvertime = etime(clock,solvertime);else solvertime = 0;end
-
-% Inconstency in early version of CPLEX
-if str2num(interfacedata.solver.subversion)>=12.3
-    the_sign = 1;
-else
-    the_sign = -1;
-end
-if ~isempty(lambda)
-    D_struc = [the_sign*lambda.eqlin;the_sign*lambda.ineqlin];
-else
-    D_struc = [];
-    lambda = [];
-end
-
-if length(x) == length(f)
-    if ~isempty(NegativeSemiVar)
-        x(NegativeSemiVar) = -x(NegativeSemiVar);
-    end
-end
-
-showprogress('Calling CPLEX-IBM',options.showprogress);
-% Check, currently not exhaustive...
-switch output.cplexstatus
-    case {1,101,102}
-        problem = 0;
-    case {3,103,106}
-        problem = 1; % Infeasible
-    case {2,20,21,118}
-        problem = 2; % Unbounded
-    case 4
-        if isempty(H)
-            options.cplex.Diagnostics = 'off';
-            options.cplex.Display = 'off';
-            if isempty(integer_variables) & isempty(binary_variables)
-                [x,fval,exitflag,output,lambda] = cplexlp(f*0,Aineq,bineq,Aeq,beq,lb,ub,[],options.cplex);
-            else
-                [x,fval,exitflag,output] = cplexmilp(f*0,Aineq,bineq,Aeq,beq,[],[],[],lb,ub,ctype',[],options.cplex);
-                lambda = [];
-            end            
-        else
-            if isempty(integer_variables) & isempty(binary_variables)
-                if isempty(Aineq) & isempty(Aeq) & isempty(lb) & isempty(ub)
-                    [x,fval,exitflag,output,lambda] = cplexqp(H*0,f*0,zeros(1,length(f)),1,Aeq,beq,lb,ub,[],options.cplex);
-                    if ~isempty(lambda)
-                        lambda.ineqlin = [];
-                    end
-                else
-                    [x,fval,exitflag,output,lambda] = cplexqp(H*0,f*0,Aineq,bineq,Aeq,beq,lb,ub,[],options.cplex);
-                end
-            else
-                if isempty(Aineq) & isempty(Aeq) & isempty(lb) & isempty(ub)
-                    [x,fval,exitflag,output] = cplexmiqp(H*0,f*0,zeros(1,length(f)),1,Aeq,beq,[],[],[],lb,ub,ctype',[],options.cplex);
-                else
-                    [x,fval,exitflag,output] = cplexmiqp(H*0,f*0,Aineq,bineq,Aeq,beq,[],[],[],lb,ub,ctype',[],options.cplex);
-                end
-                lambda = [];
-            end
-        end
-        if output.cplexstatus == 3
-            problem = 1;
-        else
-            problem = 2;
-        end       
-    case {10,11,104,105,107,108,111,112}
-        problem = 3; % Iteration/time
-    case 119
-        problem = 15;
-    case {5,6,109,110}
-        problem = 4; % Numerics
-    otherwise
-        problem = -1;
-end
-
-infostr = yalmiperror(problem,'CPLEX-IBM');
-
-% Save all data sent to solver?
-if options.savesolverinput
-    solverinput.H = H;
-    solverinput.f = f;
-    solverinput.Aineq = Aineq;
-    solverinput.Aineq = Aeq;
-    solverinput.bineq = bineq;
-    solverinput.beq = beq;
-    solverinput.LB = lb;
-    solverinput.UB = ub;
-    solverinput.X0 = [];
-    solverinput.ctype = ctype;
-    solverinput.options = options.cplex;
-else
-    solverinput = [];
-end
-
-% Save all data from the solver?
-if options.savesolveroutput
-    solveroutput.x = x;
-    solveroutput.fval = fval;
-    solveroutput.exitflag = exitflag;
-    solveroutput.lambda = lambda;
-else
-    solveroutput = [];
-end
-
-if isempty(x)
-    x = zeros(length(f),1);
-end
-% Standard interface
-output.Primal      = x(:);
-output.Dual        = D_struc;
-output.Slack       = [];
-output.problem     = problem;
-output.infostr     = infostr;
-output.solverinput = solverinput;
-output.solveroutput= solveroutput;
-output.solvertime  = solvertime;
