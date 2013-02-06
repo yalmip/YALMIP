45a117f1ea5baff5144b62368830d697c690928b
johanlofberg <johanl@isy.liu.se>
2013-02-06 12:10:24 +0100

    Use SDPT3 in test-suite

--- dev/tests-mbg/operators/test_optimizer3.m
+++ dev/tests-mbg/operators/test_optimizer3.m
@@ -4,14 +4,14 @@ yalmip('clear')
 sdpvar x
 sdpvar z
 sdpvar y
-P = optimizer([-1 <= y <= 1,y >= z*z, x <= x+x*z],x^2,sdpsettings('solver','+sedumi'),[y;z],x)
+P = optimizer([-1 <= y <= 1,y >= z*z, x <= x+x*z],x^2,sdpsettings('solver','+sdpt3'),[y;z],x)
 
 % Should be infeasible 
 [~,err] = P{[0;2]};
 mbg_asserttrue(err == 1 | err == 12);
 
 
-P = optimizer([-1 <= y <= 1,[y z;z 1]>=0, x <= x+x*z],x^2,sdpsettings('solver','+sedumi'),[y;z],x)
+P = optimizer([-1 <= y <= 1,[y z;z 1]>=0, x <= x+x*z],x^2,sdpsettings('solver','+sdpt3'),[y;z],x)
 [~,err] = P{[0;2]};
 mbg_asserttrue(err == 1 | err == 12);
 
