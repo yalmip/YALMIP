414a6fde35b2c3765ff8ab8f953ec54bde3afcf4
johanlofberg <johanl@isy.liu.se>
2013-02-13 07:50:23 +0100

    Improve help text

--- @sdpvar/parametric.m
+++ @sdpvar/parametric.m
@@ -1,10 +1,11 @@
-function x = integer(x)
+function x = parametric(x)
 %PARAMETRIC Defines a variable as parametric
 %
 %   F = PARAMETRIC(x) is used to describe the set of parametric variables
 %   in a multi-parametric program, as an alternative to using the 4th input
 %   in SOLVEMP
 %
+%   It can also be used to define the parametric variables in a SOS program
 %
 %   INPUT
 %    x : SDPVAR object
@@ -13,13 +14,9 @@ function x = integer(x)
 %    F : SET object
 %
 %   EXAMPLE
-%    F = set(prametric(x));           % Full verbose notation
 %    F = parametric(x);               % Short notation
 %
-%   See also SOLVEMP, SDPVAR
-
-% Author Johan Löfberg
-% $Id: parametric.m,v 1.3 2006-08-08 14:12:20 joloef Exp $
+%   See also SOLVEMP, SOLVESOS, SDPVAR
 
 x.typeflag = 13;
 x = lmi(x);
