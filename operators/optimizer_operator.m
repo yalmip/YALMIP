197c95e6e689e9d10819eafb412644148b9c0375
johanlofberg <johanl@isy.liu.se>
2013-02-14 15:21:59 +0100

    Correct size on output when solver failed

--- operators/optimizer_operator.m
+++ operators/optimizer_operator.m
@@ -8,9 +8,9 @@ switch class(varargin{1})
     case 'double'
         x = varargin{1}(:);
         O = varargin{2};
-        [varargout{1},problem] = O{x};
+        [varargout{1},problem] = O{x}
         if problem
-            varargout{1} = inf;
+            varargout{1} = inf(struct(O).dimout);
         end
 
     case {'sdpvar','optimizer'} % Overloaded operator for SDPVAR objects.
