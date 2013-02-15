0051bf92d1d0d2f2c4b7f07ee9173b6e9a05126d
johanlofberg <johanl@isy.liu.se>
2013-02-15 11:17:50 +0100

    Added new test

--- dev/tests-mbg/operators/test_optimizer4.m
+++ dev/tests-mbg/operators/test_optimizer4.m
@@ -4,3 +4,9 @@ sdpvar x y u z
 P = optimizer([x <= u,y <= z], -x-y,[],[u;z],[x;y]);
 sol = solvesdp([],(P{[u;z]}-[7;2])'*(P{[u;z]}-[7;2]));
 mbg_asserttrue(sol.problem == 0)
+
+yalmip('clear')
+sdpvar x y u z 
+P = optimizer([x <= u,y <= z], -x-y,[],[u;z],[x;y]);
+sol = solvesdp([],(P{1+[u;z]}-[7;2])'*(P{1+[u;z]}-[7;2]));
+mbg_asserttrue(sol.problem == 0)
