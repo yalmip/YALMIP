e33c81a1bed7cccd641e8b279e68a66b63613da6
johanlofberg <johanl@isy.liu.se>
2013-02-18 14:33:26 +0100

    Use original dimensions to determine size

--- extras/@optimizer/display.m
+++ extras/@optimizer/display.m
@@ -4,4 +4,12 @@ function display(X)
 % Author Johan Löfberg 
 % $Id: display.m,v 1.3 2007-07-31 13:30:39 joloef Exp $  
 
-disp(['Optimizer object with ' num2str(prod(X.dimin)) ' inputs and ' num2str(prod(X.dimout)) ' outputs.'])
+n = 0;
+for i = 1:length(X.diminOrig)
+    n = n + prod(X.diminOrig{i});
+end
+m = 0;
+for i = 1:length(X.dimoutOrig)
+    m = m + prod(X.dimoutOrig{i});
+end
+disp(['Optimizer object with ' num2str(n) ' inputs and ' num2str(m) ' outputs.'])
