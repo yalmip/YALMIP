2b9e8f7c03c94afa588a64ed5459c033128081d9
johanlofberg <johanl@isy.liu.se>
2013-02-19 10:48:50 +0100

    New functionality exclude binary

--- @sdpvar/exclude.m
+++ @sdpvar/exclude.m
@@ -3,8 +3,19 @@ function F = exclude(X,Y)
 %
 %    F = exclude(X,value)
 %
-% EXCLUDE is used to avoid a particular binary solution. This can be used
-% to repeatedly solve MILP problems while exluding all past solutions.
+%EXCLUDE is used to avoid a particular binary solution. This can be used
+% to repeatedly solve MILP problems while exluding all past solutions
+%
+% A = randn(30,15);
+% b = 25*rand(30,1);
+% c = randn(15,1);
+% x = binvar(15,1);
+% Model = A*x <= b;
+% sol = solvesdp(Model,c'*x);
+% while sol.problem == 0
+%    Model = [Model, exclude(x,double(x))];
+%    sol = solvesdp(Model,c'*x);
+% end
 
 if isa(X,'sdpvar') & is(X,'binary') &  isa(Y,'double') &  ismember(Y,[0 1])
     
