1467727619c1fb3d3620758b977c1b6761589fcd
johanlofberg <johanl@isy.liu.se>
2013-04-17 10:13:54 +0200

    Some more help text

--- extras/@optproblem/kkt.m
+++ extras/@optproblem/kkt.m
@@ -1,7 +1,7 @@
 function [KKTConstraints, details] = kkt(P,x,ops)
 %KKT Create KKT system for optimization system P with parametric variables x
 %
-% [KKTConstraints, details] = kkt(P,x)
+% [KKTConstraints, details] = kkt(Constraints,Objective,parameters,options)
 
 if nargin < 2
     [KKTConstraints, details] = kkt(P.Constraints,P.Objective);
