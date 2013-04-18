50227ca33340a57eb9f429e31691452196ee5476
johanlofberg <johanl@isy.liu.se>
2013-04-18 16:23:27 +0200

    Support operator inverse bound in #89 also for decreasing functions

--- modules/global/update_one_inverseeval_bound.m
+++ modules/global/update_one_inverseeval_bound.m
@@ -5,11 +5,19 @@ xL = p.lb(arg);
 xU = p.ub(arg);
 fL = p.lb(p.evalMap{i}.computes);
 fU = p.ub(p.evalMap{i}.computes);
-if ~isempty(p.evalMap{i}.properties.inverse)
-    if strcmpi(p.evalMap{i}.properties.monotonicity,'increasing')
-        invfiL = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
-        invfiU = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
-        p.lb(arg) = max(p.lb(arg),invfiL);
-        p.ub(arg) = min(p.ub(arg),invfiU);
+try
+    if ~isempty(p.evalMap{i}.properties.inverse)
+        if strcmpi(p.evalMap{i}.properties.monotonicity,'increasing')
+            invfiL = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
+            invfiU = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
+            p.lb(arg) = max(p.lb(arg),invfiL);
+            p.ub(arg) = min(p.ub(arg),invfiU);
+        elseif strcmpi(p.evalMap{i}.properties.monotonicity,'decreasing')
+            invfiL = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
+            invfiU = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
+            p.lb(arg) = max(p.lb(arg),invfiL);
+            p.ub(arg) = min(p.ub(arg),invfiU);
+        end
     end
+catch
 end
