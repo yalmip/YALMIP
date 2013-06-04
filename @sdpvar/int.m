d1b724a47be0be3313092cb1c697644170fa0e87
johanlofberg <johanl@isy.liu.se>
2013-06-04 13:43:40 +0200

    Integration support for vectors and matrices

--- @sdpvar/int.m
+++ @sdpvar/int.m
@@ -1,5 +1,5 @@
 function F = int(f,x,from,to)
-% INT Integration of scalar polynomial SDPVAR object
+% INT Integration of polynomial SDPVAR object
 %
 % F = int(f) Integrate w.r.t first variable
 % F = int(f,x) Integrate w.r.t x
@@ -7,15 +7,18 @@ function F = int(f,x,from,to)
 %
 % sdpvar x1 x2
 % p = 4*x1^4 + x1*x2;
-% int(p) returns 0.8x1^5 + 0.5x1^2x2
+% int(p) returns  0.25*x1^2*x2^2+0.8*x1^5*x2
 % int(p,[x1 x2]) returns  0.25*x1^2*x2^2+0.8*x1^5*x2
+% int(p,x1) returns 0.8*x1^5 + 0.5x1^2*x2
 % sdpvar s t
 % int(p,[x1],s,t) returns 0.8*t^5-0.8*s^5+0.5*x2*t^2-0.5*x2*s^2
-% See also JACOBIAN, HESSIAN
+% int(p,[x1 x2],[0 0],[1 1]) returns 1.05
+% int(p,[x2],[0],[2]) returns 2*x1+8*x1^4
+%
+% See also JACOBIAN, HESSIAN, SDISPLAY
 
 if nargin < 2
-    xi = depends(f);
-    xi = xi(1);
+    xi = depends(f);  
     x = recover(xi);
 end
 
--- extras/int_sdpvar.m
+++ extras/int_sdpvar.m
@@ -1,5 +1,16 @@
 function f = int_sdpvar(f,x,from,to)
 
+if length(f)>1
+    dim = size(f);
+    f = f(:);
+    F = [];
+    for i = 1:length(f)
+        F = [F;int_sdpvar(f(i),x,from,to)];
+    end
+    f = reshape(F,dim);
+    return
+end
+
 for i = 1:length(x)
     [c,v] = coefficients(f,x(i));
     vnew = [];
