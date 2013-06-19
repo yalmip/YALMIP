0656ca38e56fce658201a16f4c34eca14783ac1b
johanlofberg <johanl@isy.liu.se>
2013-06-19 10:18:18 +0200

    Rewritten slow but correct

--- extras/ncvar_coefficients.m
+++ extras/ncvar_coefficients.m
@@ -6,48 +6,42 @@ if size(monomtable,1)>size(nonCommutingTable,1)
     nonCommutingTable((1+size(nonCommutingTable,1)):(size(monomtable,1)),1) = (1+size(nonCommutingTable,1)):(size(monomtable,1));
 end
 
-% Cast commutative variables as nc temporarily by adding them to the table
-commuting = find(~any(nonCommutingTable,2));
-nonCommutingTable(commuting,1) = commuting;
-
-% Expand non-commuting nonlinears
-for i = 1:size(nonCommutingTable)
-    if variabletype(i) & ~isnan(nonCommutingTable(i,1))
-        [aux,monoms,powers] = find(monomtable(i,:));
-        r = [];
-        for j = 1:length(monoms)
-            r = [r repmat(monoms(j),1,powers(j))];
-        end
-        nonCommutingTable(i,1:length(r))=r;
-    end
-end
-
 base = getbase(p);
 vars = getvariables(p);
 xvar = getvariables(x);
-c = base(1);
-v = [1];
-for i = vars
+c = [];
+v = [];
+for k = 1:length(vars)
+    i = vars(k);
     monTerms = nonCommutingTable(i,:);
-    monTerms = monTerms(monTerms ~= 0);
-    monTerms = monTerms(~isnan(monTerms));
-    if any(ismember(xvar,monTerms))
-        newMonomial = 1;
-        newBase = 1;
-        for j = 1:length(monTerms)
-            if any(monTerms(j)==xvar)
-                newMonomial = newMonomial*recover(monTerms(j));
-            else
-                newBase = newBase*recover(monTerms(j));
-            end
-        end
-        c = [c;newBase];
-        v = [v;newMonomial];       
+    if isnan(monTerms(1))
+        % pure non-commuting, hence it can not involve x
+        c = [c; recover(i)];
+        v = [v; 1];
     else
-        c = c + recover(i);
+        % [v(x) non-commuting]. Call recursively on v(x)
+        [ctemp,vtemp] = coefficients(recover(monTerms(1)),x);
+        if ~isempty(ctemp)
+            multiplier = 1;
+            for j = 2:max(find(monTerms))
+                h = recover(monTerms(j));
+                multiplier = multiplier*h;
+            end
+            c = [c;ctemp*multiplier]
+            v = [v;vtemp];
+        end
     end
 end
-if isequal(c(1),0)
-    v = v(2:end);
-    c = c(2:end);
+
+% Compress to unique terms in basis
+vvar = [];
+for i = 1:length(v)
+    vvar = [vvar getvariables(v(i))];
 end
+[unique_monomials,keep,j] = unique(vvar);
+M = spalloc(length(unique_monomials),length(v),0);
+for i = 1:length(unique_monomials)
+    M(i,unique_monomials(i) == vvar) = 1;
+end
+c = M*c
+v = v(keep);
