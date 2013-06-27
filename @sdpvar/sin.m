46c7c377e113e305966ed016523b0fba1e5c93e6
johanlofberg <johanl@isy.liu.se>
2013-06-27 13:20:05 +0200

    Clean up

--- @sdpvar/sin.m
+++ @sdpvar/sin.m
@@ -15,32 +15,17 @@ switch class(varargin{1})
     case 'char'
 
         % General operator
-        model0 = struct('convexity','none',...
+        operator = struct('convexity','none',...
             'monotonicity','none',...
             'definiteness','none',...
             'model','callback');
 
-        model0.bounds     = @bounds;
-        model0.convexhull = @convexhull;
-        model0.derivative = @(x)(cos(x));
-        model0.range = [-1 1];
-        model0.domain = [-inf inf];
-        operator=model0;
-        %         % The following code is experimental
-        %         % Definition of concave segment
-        %         model1 = model0;
-        %         model1.convexity = 'concave';
-        %         model1.range = [0 1];
-        %         model1.domain = [0 pi];
-        %
-        %         % Definition of convex segment
-        %         model2 = model0;
-        %         model2.convexity = 'convex';
-        %         model2.range = [-1 0];
-        %         model2.domain = [-pi 0];
-        %
-        %         operator = {model0,model1,model2};
-
+        operator.bounds     = @bounds;
+        operator.convexhull = @convexhull;
+        operator.derivative = @(x)(cos(x));
+        operator.range = [-1 1];
+        operator.domain = [-inf inf];     
+  
         varargout{1} = [];
         varargout{2} = operator;
         varargout{3} = varargin{3};
