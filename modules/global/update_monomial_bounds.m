72e24ee0c50470e19f70b1eee75890f37cdca8e6
johanlofberg <johanl@isy.liu.se>
2013-06-27 08:29:57 +0200

    Propagate inverse monomial bounds. Closes #110

--- modules/global/update_monomial_bounds.m
+++ modules/global/update_monomial_bounds.m
@@ -10,10 +10,50 @@ end
 for i = 1:length(polynomials)
     j = polynomials(i);
     if j<=length(model.lb)
-        bound = powerbound(model.lb,model.ub,model.monomtable(j,:));
+        monomials = model.monomtable(j,:);
+        bound = powerbound(model.lb,model.ub,monomials);
         model.lb(j) = max(model.lb(j),bound(1));
         model.ub(j) = min(model.ub(j),bound(2));
+        [inversebound,var] = inversepowerbound(model.lb,model.ub,monomials, polynomials(i));
+        if ~isempty(var)
+            model.lb(var) = max(model.lb(var),inversebound(1));
+            model.ub(var) = min(model.ub(var),inversebound(2));
+        end
     end
+end
+
+function  [inversebound,var] = inversepowerbound(lb,ub,monomials,polynomial);
+inversebound = [];
+var = [];
+[i,var,val] = find(monomials);
+if length(var) == 1
+    if even(val)
+        if val > 2
+            inversebound = [-inf inf];
+            if ~isinf(lb(polynomial))
+                if lb(polynomial) >= 0
+                    inversebound(1,1) = lb(polynomial)^(1/val);
+                end
+            end
+            if ~isinf(ub(polynomial))
+                if ub(polynomial) >= 0
+                    inversebound(1,2) = ub(polynomial)^(1/val);
+                end
+            end
+        else
+            var = [];
+        end
+    elseif val >= 3
+        inversebound = [-inf inf];
+        if ~isinf(lb(polynomial))
+            inversebound(1,1) = sign(lb(polynomial))*abs(lb(polynomial))^(1/val);
+        end
+        if ~isinf(ub(polynomial))
+            inversebound(1,2) = sign(ub(polynomial))*abs(ub(polynomial))^(1/val);
+        end
+    end
+else
+    var = [];
 end
 
 
