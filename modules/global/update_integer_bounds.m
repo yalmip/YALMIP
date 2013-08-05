bb38189ffd4b67ed63aa421c60d2a63a5cfc66cc
johanlofberg <johanl@isy.liu.se>
2013-08-05 13:24:48 +0200

    Closes #122

--- modules/global/update_integer_bounds.m
+++ modules/global/update_integer_bounds.m
@@ -1,12 +1,34 @@
 function model = update_integer_bounds(model);
 
 if ~isempty(model.integer_variables)
+    % Clean up things like 0.00001 <= x <= 0.999999 to 0 <= x <= 1
+    % however, don't kill equalities 1 <= x <= 1
+    lbfixed = fix(model.lb(model.integer_variables));
+    ubfixed = fix(model.ub(model.integer_variables));
+    fixlb = find(model.lb(model.integer_variables) == lbfixed);
+    fixub = find(model.ub(model.integer_variables) == ubfixed);
     model.lb(model.integer_variables) = ceil(model.lb(model.integer_variables)-1e-4);
     model.ub(model.integer_variables) = floor(model.ub(model.integer_variables)+1e-4);
+    if ~isempty(fixlb)
+        model.lb(fixlb) = lbfixed;
+    end
+    if ~isempty(fixub)
+        model.ub(fixub) = ubfixed;
+    end
 end
 if ~isempty(model.binary_variables)
+    lbfixed = fix(model.lb(model.binary_variables));
+    ubfixed = fix(model.ub(model.binary_variables));
+    fixlb = find(model.lb(model.binary_variables) == lbfixed);
+    fixub = find(model.ub(model.binary_variables) == ubfixed);
     model.lb(model.binary_variables) = fix(model.lb(model.binary_variables)-1e-4);
     model.ub(model.binary_variables) = fix(model.ub(model.binary_variables)+1e-4);
+    if ~isempty(fixlb)
+        model.lb(model.binary_variables(fixlb)) = lbfixed;
+    end
+    if ~isempty(fixub)
+        model.ub(model.binary_variables(fixub)) = ubfixed;
+    end
 end
 if any(model.lb(model.binary_variables) > model.ub(model.binary_variables))
     model.feasible = 0;
