9802171856742468f3c839a4e8a1dceed4c540e1
johanlofberg <johanl@isy.liu.se>
2014-01-31 09:49:45 +0100

    Removed dead code

--- solvers/yalmip2SDPmosek.m
+++ solvers/yalmip2SDPmosek.m
@@ -57,31 +57,7 @@ for j = 1:length(model.K.s)
     prob.barc.subj = [prob.barc.subj j*ones(1,length(k))];
     prob.barc.subk = [prob.barc.subk k(:)'];
     prob.barc.subl = [prob.barc.subl l(:)'];
-    prob.barc.val = [prob.barc.val val(:)'];
-  
-    if 0
-    %Ais = model.A(top:top+n^2-1,:);
-    Ais = AA(:,top:top+n^2-1)';
-    [elementIndex,variableIndex,val] = find(Ais);
-    [row,column] = ind2sub([n n],elementIndex);
-    keep = find(row >= column);
-    row = row(keep);
-    column = column(keep);  
-    variableIndex = variableIndex(keep);
-    val = val(keep);
-    
-  
-    % Which LMI
-    prob.bara.subj = [prob.bara.subj j*ones(1,length(val))];
-    % Which variable
-    prob.bara.subi = [prob.bara.subi variableIndex(:)'];
-    % Row
-    prob.bara.subk = [prob.bara.subk row(:)'];
-    % Column
-    prob.bara.subl = [prob.bara.subl column(:)'];
-    % Value
-    prob.bara.val = [prob.bara.val val(:)'];
-    end
+    prob.barc.val = [prob.barc.val val(:)'];     
     top = top + n^2;
 end
 
