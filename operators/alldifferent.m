661e7edd4de57e751dfe8fa79b6348b2c00e1a72
johanlofberg <johanl@isy.liu.se>
2014-02-17 13:43:48 +0100

    Use simple model for alldifferent when length(x) = number of possibilities

--- operators/alldifferent.m
+++ operators/alldifferent.m
@@ -17,19 +17,25 @@ switch class(varargin{1})
         x = reshape(x,nx*mx,1);
 
         [M,m] = derivebounds(x);
+        if is(x,'lpcone') & is(x,'integer') & 1+(max(M)-min(m)) == length(x)
+            % Simple permutation of x from given numbers
+            n = nx*mx;
+            B = binvar(n,n,'full');
+            F = [x == B*(min(m):max(M))', sum(B,1)==1,sum(B,2)==1];            
+        else
+            % Add constraint |x(i)-x(j)| > 1
+            pairs = nchoosek(1:nx*mx,2);
+            d = binvar(length(pairs),1);
+            x1 = x(pairs(:,1));
+            x2 = x(pairs(:,2));
+            
+            % d(i) = 0  ==> x1>x2
+            % d(i) = 1  ==> x2>x1
+            
+            F =     set(x1 - x2 >= 1-(1+M(pairs(:,2))-m(pairs(:,1))).*d);
+            F = F + set(x2 - x1 >= 1-(1+M(pairs(:,1))-m(pairs(:,2))).*(1-d));
+        end
         
-        % Add constraint |x(i)-x(j)| > 1
-        pairs = nchoosek(1:nx*mx,2);
-        d = binvar(length(pairs),1);
-        x1 = x(pairs(:,1));
-        x2 = x(pairs(:,2));
-
-        % d(i) = 0  ==> x1>x2
-        % d(i) = 1  ==> x2>x1
-
-        F =     set(x1 - x2 >= 1-(1+M(pairs(:,2))-m(pairs(:,1))).*d);
-        F = F + set(x2 - x1 >= 1-(1+M(pairs(:,1))-m(pairs(:,2))).*(1-d));
-
         varargout{1} = F;
         varargout{2} = struct('convexity','none','monotonicity','none','definiteness','none','extra','marker','model','integer');
         varargout{3} = varargin{3};
