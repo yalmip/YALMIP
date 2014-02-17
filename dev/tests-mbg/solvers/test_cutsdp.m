0d9a7201ce391b6d950e3dd4ce1a57f9da0a9d3d
johanlofberg <johanl@isy.liu.se>
2014-02-17 08:35:40 +0100

    New test case

---
+++ dev/tests-mbg/solvers/test_cutsdp.m
@@ -0,0 +1,16 @@
+function test_cutsdp
+c=[1 -2 3]';
+x = binvar(3,1);
+sdpvar t
+A = [-1 2 0;-3 -4 1;0 0 -2];
+P = sdpvar(3,3);
+a = sdpvar(3,1);
+F = [P >= 0, P*A'+A*P <= 0];
+F = [F, a<=9*x];
+F = [F, a>=-9*x];
+F = [F, trace(P) == 1];
+F = [F, [t (c+a)';c+a P] >=0];
+ops = sdpsettings('solver','cutsdp','cutsdp.maxiter',10);
+solvesdp(F,t,ops)
+% Just assert that it ran
+mbg_asserttrue(sol.problem ~= -1);
