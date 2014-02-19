041a372f11269377732f20be922392a8b927b383
johanlofberg <johanl@isy.liu.se>
2014-02-19 12:50:44 +0100

    Removed dead code

--- solvers/getavailablesolvers.m
+++ solvers/getavailablesolvers.m
@@ -3,16 +3,6 @@ function [solvers,keep] = getavailablesolvers(findallsolvers,options);
 solvers = definesolvers;
 keep = ones(length(solvers),1);
 
-% solverstocheck = 1:length(solvers);
-% if length(options.solver)>0 & isempty(strfind(options.solver,'*')) & ~findallsolvers
-%     for i = 1:length(solvers)
-%         if strcmpi(options.solver,solvers(i).tag)
-%            if ~solvers(i).usesother                              
-%            end
-%         end
-%     end
-% end
-
 if ~findallsolvers
     for i = 1:length(solvers)
         isavailable = 1;
