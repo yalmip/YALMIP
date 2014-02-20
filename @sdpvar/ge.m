7ed642dc61a840f7b32b028f96184dbd8054816a
johanlofberg <johanl@isy.liu.se>
2014-02-20 13:05:36 +0100

    Test for blkvar is always done inside constraint later on

--- @sdpvar/ge.m
+++ @sdpvar/ge.m
@@ -1,14 +1,6 @@
 function y = ge(X,Y)
 %GE (overloaded)
 
-if isa(X,'blkvar')
-    X = sdpvar(X);
-end
-
-if isa(Y,'blkvar')
-    Y = sdpvar(Y);
-end
-
 try
     y = constraint(X,'>=',Y);
 catch
--- @sdpvar/le.m
+++ @sdpvar/le.m
@@ -1,16 +1,6 @@
 function y = le(X,Y)
 %LE (overloaded)
 
-% Author Johan Löfberg
-
-if isa(X,'blkvar')
-    X = sdpvar(X);
-end
-
-if isa(Y,'blkvar')
-    Y = sdpvar(Y);
-end
-
 try
     y = constraint(X,'<=',Y);
 catch
