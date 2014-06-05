f0659976b13ddf260419ea2c48fab9e041d3d05c
johanlofberg <johanl@isy.liu.se>
2014-06-05 11:58:21 +0200

    Move from method

---
+++ extras/eigv.m
@@ -0,0 +1,8 @@
+function [v,Model] = eigv(X)
+
+n = size(X,1);
+v = sdpvar(n,1);
+Model = [trace(X)==sum(v), v(1:end-1) >= v(2:end)];
+for i = 1:n-1
+    Model = [Model, sumk(X,i) <= sum(v(1:i))];
+end
