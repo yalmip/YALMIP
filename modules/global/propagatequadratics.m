286dadcf471874eddfc2ef805db745723e3e8960
johanlofberg <johanl@isy.liu.se>
2014-06-17 14:07:31 +0200

    Cleaned code

--- modules/global/propagatequadratics.m
+++ modules/global/propagatequadratics.m
@@ -16,8 +16,7 @@ if p.bilinears~=0
             for i = 1:length(quadratic_variables)
                 k = quadratic_variables(i);
                 
-                if (p.lb(k) < p.ub(k)-1e-4)
-                    %if p.lb(k) >= 0 & (p.lb(k) < p.ub(k)-1e-4)
+                if (p.lb(k) < p.ub(k)-1e-4)                   
                     x = p.bilinears(p.bilinears(:,1)==k,2);% x^2
                     candidates = find((InequalityConstraintState==1) & p.F_struc(1:p.K.f+p.K.l,1+k))';
                     for j = candidates
@@ -74,36 +73,7 @@ if p.bilinears~=0
                             if p.lb(x) > left
                                 p.lb(x) = max(p.lb(x),right);
                             end     
-                        end
-                        %
-                        %                         if aij > 0
-                        %                             indNEG = find(a < 0);
-                        %                             indPOS = find(a > 0);
-                        %                             LB = p.lb;
-                        %                             UB = p.ub;
-                        %                             LB(k) = 0;
-                        %                             UB(k) = 0;
-                        %                             a(k) = 0;
-                        %                             newLB = (-p.F_struc(j,1)-a([indPOS(:);indNEG(:)])*[UB(indPOS);LB(indNEG)])/aij;
-                        %                             p.lb(k) = max(p.lb(k),newLB);
-                        %                             if p.lb(x)>0
-                        %                                 p.lb(x) = max(p.lb(x),sqrt(max(0,newLB)));
-                        %                             elseif p.ub(x)<0
-                        %                                 p.ub(x) = min(p.ub(x),-sqrt(max(0,newLB)));
-                        %                             end
-                        %                         elseif aij < 0
-                        %                             indNEG = find(a < 0);
-                        %                             indPOS = find(a > 0);
-                        %                             LB = p.lb;
-                        %                             UB = p.ub;
-                        %                             LB(k) = 0;
-                        %                             UB(k) = 0;
-                        %                             a(k) = 0;
-                        %                             newUB = (p.F_struc(j,1)+a([indPOS(:);indNEG(:)])*[UB(indPOS);LB(indNEG)])/(-aij);
-                        %                             p.ub(k) = min(p.ub(k),newUB);
-                        %                             p.ub(x) = min(p.ub(x),sqrt(max(0,newUB)));
-                        %                         end
-                        
+                        end                                
                     end
                     
                 elseif p.lb(k)<0
