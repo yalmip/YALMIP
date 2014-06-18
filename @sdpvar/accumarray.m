a0cf707cd085032b2d1fb5cfef928ae3b27d0b77
johanlofberg <johanl@isy.liu.se>
2014-06-18 13:10:07 +0200

    Simple case supported in accumarray

---
+++ @sdpvar/accumarray.m
@@ -0,0 +1,12 @@
+function A=accumarray(subs,val)
+%ACCUMARRAY (overloaded)
+
+if size(subs,2)>1 || min(size(val))>1
+    error('SDPVAR/ACCUMARRAY currently only supports simple cases (subs=column, val=vector)');
+end
+
+S = sparse(max(subs),length(val),0);
+for i = 1:size(S,1)
+    S(i,find(i == subs)) = 1;
+end
+A = S*reshape(val,[],1);
