95a41107f6463cc3d0644fc6292c5bc90bb1694b
johanlofberg <johanl@isy.liu.se>
2014-06-25 12:08:11 +0200

    More xunit tests

---
+++ dev/xunit/mpt/test_mpt_norm_repeated.m
@@ -0,0 +1,68 @@
+function test_mpt_norm_repeated
+
+% As simple as it gets...
+yalmip('clear')
+sdpvar u0
+x0 = sdpvar(2,1);
+
+bounds(x0,-2,2);
+bounds(u0,-2,2);
+x1 = [0.1 0.2;0.3 0.4]*x0 + [0.5;0.6]*u0
+F = set([]);
+F = F + set(norm(x1,1) <= norm(x0,1));
+F = F +  set(-2<=x0<=2);
+F = F +  set(-2<=x1<=2);
+F = F +  set(-2<=u0<=2);
+obj = norm(x0,1);
+[sol,dgn,VV,JJ] = solvemp(F, obj, [], x0)
+
+mbg_asserttrue(length(sol) == 4);
+mbg_asserttrue(dgn.problem == 0);
+
+assign(x0,[1;1])
+mbg_asserttolequal(double(JJ),2,1e-5);
+
+yalmip('clear')
+sdpvar u0 u1
+x0 = sdpvar(2,1);
+x1 = sdpvar(2,1);
+x2 = sdpvar(2,1);
+bounds(x0,-2,2);
+bounds(x1,-2,2);
+bounds(x2,-2,2);
+bounds(u0,-2,2);
+bounds(u1,-2,2);
+%x1 = [0.1 0.2;0.3 0.4]*x0 + [0.5;0.6]*u0
+F = set(x1 ==[0.1 0.2;0.3 0.4]*x0 + [0.5;0.6]*u0);
+F = F + set(x2 ==[0.1 0.2;0.3 0.4]*x1 + [0.5;0.6]*u1);
+F = F + set(norm(x1,1) <= norm(x0,1));
+F = F + set(norm(x2,1) <= norm(x1,1));
+F = F +  set(-2<=x0<=2);
+F = F +  set(-2<=x1<=2);
+F = F +  set(-2<=x2<=2);
+F = F +  set(-2<=u0<=2);
+obj = norm(x0,1)+norm(x1,1);
+sol = solvemp(F, obj, [], x0)
+mbg_asserttrue(length(sol) == 6);
+
+yalmip('clear')
+sdpvar u0 u1
+x0 = sdpvar(2,1);
+x1 = sdpvar(2,1);
+x2 = sdpvar(2,1);
+bounds(x0,-2,2);
+bounds(x1,-2,2);
+bounds(x2,-2,2);
+bounds(u0,-2,2);
+bounds(u1,-2,2);
+F = set(x1 ==[0.1 0.2;0.3 0.4]*x0 + [0.5;0.6]*u0);
+F = F + set(x2 ==[0.1 0.2;0.3 0.4]*x1 + [0.5;0.6]*u1);
+F = F + set(norm(x2,1) <= norm(x1,1));
+F = F + set(norm(x1,1) <= norm(x0,1));
+F = F +  set(-2<=x0<=2);
+F = F +  set(-2<=x1<=2);
+F = F +  set(-2<=x2<=2);
+F = F +  set(-2<=u0<=2);
+obj = norm(x0,1)+norm(x1,1);
+sol = solvemp(F, obj, [], x0)
+mbg_asserttrue(length(sol) == 6);
---
+++ dev/xunit/robust/test_robust_11.m
@@ -0,0 +1,74 @@
+function test_robust_11
+ops = sdpsettings;
+yalmip('clear')
+A = [2.938 -0.7345 0.25;4 0 0;0 1 0];
+B = [0.25;0;0];
+C = [-0.2072 0.04141 0.07256];
+E = [0.0625;0;0];
+
+N = 10;
+U = sdpvar(N,1);
+W = sdpvar(N,1);
+x = sdpvar(3,1);
+
+Y = [];
+xk = x;
+for k = 1:N
+ xk = A*xk + B*U(k)+E*W(k);
+ Y = [Y;C*xk];
+end
+sY = sdpvar(length(Y),1);
+sU = sdpvar(length(U),1);
+F = [Y <= 1, -1 <= U <= 1,-sY<=Y-1<=sY,-sU<=U<=sU];
+objective = sum(sY) + sum(sU)*0.01;
+G = [-1 <= W <= 1]
+% future controls are functions (policies) of future states, i.e. they
+% depend on past disturbances w. 
+% We can hack YALMIP to eliminate/adjust 
+for i = 2:N
+    yalmip('setdependenceUser',U(i),W(1:i-1))
+end
+
+% since we are comparing with manually constructed affine feedback, we must
+% force YALMIP to use this. Note that it will be used on the real auxiliary
+% variables too, i.e. sub-optimal
+[Frobust,h] = robustify(F + G,objective,sdpsettings('robust.auxreduce','affine'),W);
+
+Ns = 3;
+xk1 = [0;0;0];
+ww = (-1+2*rand(Ns,1))
+
+for i = 1:Ns
+    solvesdp([Frobust, x == xk1(:,end)],h,ops);    
+    xk1 = [xk1 A*xk1(:,end) + B*double(U(1)) + E*ww(i)];
+end
+xk1
+
+V = sdpvar(N,1);
+L = sdpvar(N,N,'full').*(tril(ones(N))-eye(N));
+
+U = L*W + V;
+
+Y = [];
+xk = x;
+for k = 1:N
+ xk = A*xk + B*U(k)+E*W(k);
+ Y = [Y;C*xk];
+end
+
+F = [Y <= 1, -1 <= U <= 1,-sY<=Y-1<=sY,-sU<=U<=sU];
+objective = sum(sY) + sum(sU)*0.01;
+
+[Frobust,h] = robustify([F, G],objective,sdpsettings('robust.auxreduce','affine'),W);
+
+xk2 = [0;0;0];
+ops = sdpsettings;
+for i = 1:Ns
+    solvesdp([Frobust, x == xk2(:,end)],h,ops);
+    xk2 = [xk2 A*xk2(:,end) + B*double(U(1)) + E*ww(i)];
+end
+norm(xk1-xk2)
+mbg_asserttolequal(norm(xk1-xk2),0, 1e-3);
+
+
+
