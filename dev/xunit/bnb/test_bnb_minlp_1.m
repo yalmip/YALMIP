28f812012327227d5cffc075ca18d862f78e1df1
johanlofberg <johanl@isy.liu.se>
2014-06-25 11:33:22 +0200

    More xunit tests

---
+++ dev/xunit/bnb/test_bnb_minlp_1.m
@@ -0,0 +1,14 @@
+function minlp
+
+randn('seed',12345);
+rand('seed',12345);
+
+x = sdpvar(5,1);
+A = randn(15,5);
+b = rand(15,1)*10;
+
+obj = sum(x) + sum((x-3).^4);
+constraints = set(A*x <= b) + set(integer(x));
+sol = solvesdp(constraints,obj,sdpsettings('bnb.solver','fmincon','warning',0))
+
+mbg_asserttrue(sol.problem == 0);
---
+++ dev/xunit/bnb/test_bnb_sdp_1.m
@@ -0,0 +1,13 @@
+function test_bnb_sdp_1
+
+% Test two regression bugs
+% 1. quadratic costs in binary SDP
+% 2. OR on SDP constraints
+
+X = sdpvar(3,3);
+obj = trace((X-2*eye(3))*(X-2*eye(3))');
+sol = solvesdp(set (X<=3*eye(3)) + set((X>=eye(3)) | (X<=-eye(3))) + set(-50 <= X(:) <= 50),obj)
+
+mbg_asserttrue(sol.problem==0);
+mbg_asserttolequal(double(obj), 0, 1e-5);
+
