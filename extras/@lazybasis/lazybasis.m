60ea556acc8e2f9dfc6a98b8a94778d30c60f83d
johanlofberg <johanl@isy.liu.se>
2014-07-02 23:22:40 +0200

    remove old experimental code

--- extras/@lazybasis/lazybasis.m
+++
@@ -1,9 +0,0 @@
-function sys = lazybasis(n,m,i,j,s)
-
-%sys.basis = basis;
-sys.n = n;
-sys.m = m;
-sys.iX = i;
-sys.jX = j;
-sys.sX = s;
-sys = class(sys,'lazybasis');
--- extras/@lazybasis/uminus.m
+++
@@ -1,7 +0,0 @@
-function y = uminus(X)
-%PLUS (overloaded)
-
-% Author Johan Löfberg
-% $Id: uminus.m,v 1.1 2005-10-12 16:05:54 joloef Exp $
-
-y = -double(X);
