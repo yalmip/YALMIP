4071f8bdd8435e7243a0c8d89ecb8ea34172ad7c
johanlofberg <johanl@isy.liu.se>
2014-08-05 09:58:11 +0200

    Test for #183

---
+++ dev/xunit/dualize/test_dualize_sdp_11.m
@@ -0,0 +1,18 @@
+function test_dualize_sdp_11
+
+yalmip('clear')
+N = 2;
+X = sdpvar(N,N,'hermitian','complex');
+t = sdpvar;
+obj = t;
+F = [X>=0];
+F = [F,cone([1;t])];
+opts = sdpsettings('dualize',0);
+sol1 = solvesdp(F,-obj,opts);
+o1 = double(obj);
+opts = sdpsettings('dualize',1);
+sol2 = solvesdp(F,-obj,opts);
+o2 = double(obj);
+mbg_asserttolequal(sol1.problem,0);
+mbg_asserttolequal(sol2.problem,0);
+mbg_asserttolequal(o1,o2,1e-4);
