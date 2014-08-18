61fa1c67b7a3568ac30567f24d381a946c500f10
johanlofberg <johanl@isy.liu.se>
2014-08-18 09:22:54 +0200

    Changed test

--- dev/xunit/logic/test_logic_constraint_eq_1.m
+++ dev/xunit/logic/test_logic_constraint_eq_1.m
@@ -8,6 +8,9 @@ mbg_asserttrue(sol.problem == 0);
 % Since a is 0, b has to be false or constraint violated
 mbg_asserttrue(double(a)==0 && (double(b)==0 || double(x)>=5));
 
+sol = solvesdp(F,x^2 + 1-b)
+mbg_asserttrue(double(a)==1 && (double(b)==1 || double(x)==0));
+
 dX = binvar(1);
 dY = binvar(1);
 sys = [iff(dX,a), iff(dY,[b & (x<=4.5)]), dX == dY];
