2159412c8eac543f2875fe5a219e441cbe217702
johanlofberg <johanl@isy.liu.se>
2014-08-19 14:50:44 +0200

    Fixes #197

--- solvers/callmosek.m
+++ solvers/callmosek.m
@@ -1,43 +1,45 @@
-function output = callmosek(interfacedata)
+function output = callmosek(model)
 
 % Retrieve needed data
-options = interfacedata.options;
-F_struc = interfacedata.F_struc;
-c       = interfacedata.c;
-Q       = interfacedata.Q;
-K       = interfacedata.K;
-x0      = interfacedata.x0;
-integer_variables = interfacedata.integer_variables;
-binary_variables = interfacedata.binary_variables;
-extended_variables = interfacedata.extended_variables;
-ub      = interfacedata.ub;
-lb      = interfacedata.lb;
-mt      = interfacedata.monomtable;
+options = model.options;
+F_struc = model.F_struc;
+c       = model.c;
+Q       = model.Q;
+K       = model.K;
+x0      = model.x0;
+integer_variables = model.integer_variables;
+binary_variables = model.binary_variables;
+extended_variables = model.extended_variables;
+ub      = model.ub;
+lb      = model.lb;
+mt      = model.monomtable;
 
 % *********************************
 % What type of variables do we have
 % *********************************
-linear_variables = find((sum(abs(mt),2)==1) & (any(mt==1,2)));
-nonlinear_variables = setdiff((1:size(mt,1))',linear_variables);
-sigmonial_variables = find(any(0>mt,2) | any(mt-fix(mt),2));
+model.linear_variables = find((sum(abs(mt),2)==1) & (any(mt==1,2)));
+model.nonlinear_variables = setdiff((1:size(mt,1))',model.linear_variables);
+model.sigmonial_variables = find(any(0>mt,2) | any(mt-fix(mt),2));
 
 % Some meta-solver thinks we handle binaries
-if ~isempty(interfacedata.binary_variables)
-    integer_variables = union( interfacedata.integer_variables, interfacedata.binary_variables);
+if ~isempty(model.binary_variables)
+    integer_variables = union(model.integer_variables, model.binary_variables);
     if isempty(lb)
-        lb = repmat(-inf,1,length(interfacedata.c));
+        lb = repmat(-inf,1,length(model.c));
     end
-    lb(interfacedata.binary_variables) = max(lb(interfacedata.binary_variables),0);    
+    lb(model.binary_variables) = max(lb(model.binary_variables),0);    
     if isempty(ub)
-        ub = repmat(inf,1,length(interfacedata.c));
+        ub = repmat(inf,1,length(model.c));
     end
-    ub(interfacedata.binary_variables) = min(ub(interfacedata.binary_variables),1);
+    ub(model.binary_variables) = min(ub(model.binary_variables),1);
+    model.lb = lb;
+    model.ub = ub;
 end
 
-if ~isempty(sigmonial_variables) | isequal(interfacedata.solver.version,'GEOMETRIC')
-    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_geometric(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,extended_variables);          
+if ~isempty(model.sigmonial_variables) | isequal(model.solver.version,'GEOMETRIC')
+    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_geometric(model);          
 else
-    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdp(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,integer_variables,x0);        
+    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdp(model);        
 end
 
 infostr = yalmiperror(problem,'MOSEK');	
@@ -69,133 +71,30 @@ output.solverinput = solverinput;
 output.solveroutput= solveroutput;
 output.solvertime  = solvertime;
 
-function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdp(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,integer_variables,x0);
+function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdp(model);
 
-if nnz(Q)==0 & isempty(integer_variables) && isempty(x0)
-    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdpdual(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,integer_variables,x0);
+if nnz(model.Q)==0 & isempty(model.integer_variables) && isempty(model.x0)
+    % Standard cone problem
+    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdpdual(model);
     return
-end
-
-% SETUP a naive model interpreting directly in primal space. Will not be
-% efficient for large-scale problems, but integrality can only be appended
-% in this space. quadratic terms are easier here as well
-
-prob.c = c;
-if ~isempty(F_struc)
-    prob.a = -F_struc(:,2:end);
-    prob.buc = full(F_struc(:,1));
-    prob.blc = repmat(-inf,length(prob.buc),1);
+elseif model.K.s(1)>0
+    % Semidefinite program with integer variables or quadratic objective
+    % or specified initial point
+    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_sdp(model);
 else
-    prob.a = sparse(ones(1,length(c))); % Dummy constraint
-    prob.buc = inf;
-    prob.blc = -inf;
-end
-if isempty(lb)
-    prob.blx = repmat(-inf,1,length(c));
-else
-    prob.blx = lb;
-end
-if isempty(ub)
-    prob.bux = repmat(inf,1,length(c));
-else
-    prob.bux = ub;
+    % Integer conic program + possibly quadratic objective
+    [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqp(model);        
 end
 
-if K.f>0
-    prob.blc(1:K.f) = prob.buc(1:K.f);
-end
 
-[prob.qosubi,prob.qosubj,prob.qoval] = find(tril(sparse(2*Q)));    
-
-if K.q(1)>0
-    nof_new = sum(K.q);
-    prob.a = [prob.a [spalloc(K.f,nof_new,0);spalloc(K.l,nof_new,0);speye(nof_new);spalloc(sum(K.s.^2),nof_new,0)]];
-    prob.blc(1+K.f+K.l:K.f+K.l+sum(K.q)) = prob.buc(1+K.f+K.l:K.f+K.l+sum(K.q));    
-    prob.c = [prob.c;zeros(nof_new,1)];
-    
-    top = size(F_struc,2)-1;
-    for i = 1:length(K.q)
-        prob.cones{i}.type = 'MSK_CT_QUAD';
-        prob.cones{i}.sub  = top+1:top+K.q(i);
-        prob.blx(top+1:top+K.q(i)) = -inf;
-        prob.bux(top+1:top+K.q(i)) = inf;        
-        top = top + K.q(i);
-    end    
-end
-
-if ~isempty(integer_variables)
-    prob.ints.sub = integer_variables;
-end
-
-param = options.mosek;
-
-if ~isempty(x0)
-    if options.usex0
-        prob.sol.int.xx = zeros(max([length(Q) size(prob.a,2)]),1);
-        prob.sol.int.xx(integer_variables) = x0(integer_variables);
-        evalc('[res,sol] = mosekopt (''symbcon'')');
-        sc = sol.symbcon ;
-        param.MSK_IPAR_MIO_CONSTRUCT_SOL = sc.MSK_ON;
-    end
-end
-
-% Debug?
-if options.savedebug
-    save mosekdebug prob param
-end
-
-% Call MOSEK
-[r,res,solvertime] = doCall(prob,param,options);
-
-x = [];
-D_struc = [];
-if (r == 1010) || (r == 1011) | (r==1001)
-    problem = -5;
-elseif r == 1200
-    problem = 7;
-elseif r == 10007
-    problem = 16;    
-else
-    % Recover solutions
-    sol = res.sol;
-    if isempty(integer_variables)
-        x = sol.itr.xx(1:length(c)); % Might have added new ones                
-        D_struc = (sol.itr.suc-sol.itr.slc);        
-        error_message = sol.itr.prosta;
-    else        
-        try
-        x = sol.int.xx(1:length(c)); % Might have added new ones
-        D_struc = [];
-        error_message = sol.int.prosta;
-        catch
-            x = [];
-            error_message = 'crash';
-            D_struc = [];
-        end      
-    end
-    
-    switch error_message
-        case {'PRIMAL_AND_DUAL_FEASIBLE','PRIMAL_FEASIBLE'}
-            problem = 0;
-        case 'PRIMAL_INFEASIBLE'
-            problem = 1;
-        case 'DUAL_INFEASIBLE'
-            problem = 2;
-        case 'PRIMAL_INFEASIBLE_OR_UNBOUNDED'
-            problem = 12;
-        otherwise
-            problem = -1;
-    end
-end
-
-function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_geometric(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,extended_variables);
+function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_geometric(model);
    
 x = [];
 D_struc = [];
 r = [];
 res = [];
 solvertime = 0;
-[prob,problem] = yalmip2geometric(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,extended_variables);
+[prob,problem] = yalmip2geometric(model.options,model.F_struc,model.c,model.Q,model.K,model.ub,model.lb,model.monomtable,model.linear_variables,model.extended_variables);
 if problem == 0
 
     % Mosek does not support equalities
@@ -204,17 +103,17 @@ if problem == 0
         prob.b = [prob.b;prob.h;1./prob.h];
         prob.map = [prob.map;max(prob.map) + (1:2*length(prob.h))'];
     end
-    if options.savedebug
+    if model.options.savedebug
         save mosekdebug prob
     end
 
-    param = options.mosek;
+    param = model.options.mosek;
 
     % Call MOSEK
     solvertime = clock;
-    showprogress('Calling MOSEK',options.showprogress);
+    showprogress('Calling MOSEK',model.options.showprogress);
     solvertime = clock;
-    if options.verbose == 0  
+    if model.options.verbose == 0  
         res = mskgpopt(prob.b,prob.A,prob.map,param,'minimize echo(0)');
     else
     	res = mskgpopt(prob.b,prob.A,prob.map,param,'minimize');     
@@ -222,8 +121,8 @@ if problem == 0
     sol = res.sol;
     solvertime = etime(clock,solvertime);
 
-    x = zeros(length(c),1);
-    x(linear_variables)=exp(res.sol.itr.xx);
+    x = zeros(length(model.c),1);
+    x(model.linear_variables)=exp(res.sol.itr.xx);
     D_struc = [];
 
     % Check, currently not exhaustive...
@@ -241,36 +140,36 @@ if problem == 0
     end
 end
 
-function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdpdual(options,F_struc,c,Q,K,ub,lb,mt,linear_variables,integer_variables,x0)
+function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqpsocpsdpdual(model)
 
 % Convert if the caller is bnb or bmibnb which might have appended bounds
 % Sure, we could setup model with bounds, but... 
-[F_struc,K] = addbounds(F_struc,K,ub,lb);
+[model.F_struc,model.K] = addbounds(model.F_struc,model.K,model.ub,model.lb);
 
-param = options.mosek;
+param = model.options.mosek;
 
-prob.c = F_struc(1:K.f+K.l+sum(K.q),1);
-prob.a = -F_struc(1:K.f+K.l+sum(K.q),2:end)';
-prob.blc = -c;
-prob.buc = -c;
+prob.c = model.F_struc(1:model.K.f+model.K.l+sum(model.K.q),1);
+prob.a = -model.F_struc(1:model.K.f+model.K.l+sum(model.K.q),2:end)';
+prob.blc = -model.c;
+prob.buc = -model.c;
 prob.blx = -inf(size(prob.a,2),1);
 prob.bux = inf(size(prob.a,2),1);
-top = K.f+K.l;
-prob.blx(1+K.f:K.f+K.l) = 0;
+top = model.K.f+model.K.l;
+prob.blx(1+model.K.f:model.K.f+model.K.l) = 0;
 
-if K.q(1)>0
-    for i = 1:length(K.q)
+if model.K.q(1)>0
+    for i = 1:length(model.K.q)
         prob.cones{i}.type = 'MSK_CT_QUAD';
-        prob.cones{i}.sub  = top+1:top+K.q(i);
-        top = top + K.q(i);
+        prob.cones{i}.sub  = top+1:top+model.K.q(i);
+        top = top + model.K.q(i);
     end
 end
 
-if K.s(1)>0
-   prob = appendSDPdata(F_struc,K,prob);
+if model.K.s(1)>0
+   prob = appendSDPdata(model.F_struc,model.K,prob);
 end
 
-[r,res,solvertime] = doCall(prob,param,options);
+[r,res,solvertime] = doCall(prob,param,model.options);
 
 try
     x = res.sol.itr.y;
@@ -278,12 +177,12 @@ catch
     x = nan(length(model.c),1);    
 end
 
-if options.saveduals & ~isempty(x)
+if model.options.saveduals & ~isempty(x)
     D_struc = [res.sol.itr.xx];    
     top = 1;
-    for i = 1:length(K.s)
-        X = zeros(K.s(i));
-        n = K.s(i);
+    for i = 1:length(model.K.s)
+        X = zeros(model.K.s(i));
+        n = model.K.s(i);
         I = find(tril(ones(n)));
         X(I) = res.sol.itr.barx(top:((top+n*(n+1)/2)-1));
         X = X + tril(X,-1)';
@@ -409,4 +308,182 @@ for j = 1:length(K.s)
     prob.barc.subl = [prob.barc.subl l(:)'];
     prob.barc.val = [prob.barc.val val(:)'];     
     top = top + n^2;
+end
+
+function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_sdp(model)
+
+param = model.options.mosek;
+
+% Convert
+[model.F_struc,model.K] = addbounds(model.F_struc,model.K,model.ub,model.lb);
+prob = yalmip2SDPmosek(model);
+solvertime = clock;
+
+% Debug?
+if model.options.savedebug
+    save mosekdebug prob param
+end
+
+if model.options.verbose == 0
+    [r,res] = mosekopt('minimize echo(0)',prob,param);    
+else
+    [r,res] = mosekopt('minimize info',prob,param);
+end
+solvertime = etime(clock,solvertime);
+
+if res.rcode == 2001
+    res.sol.itr.prosta = 'DUAL_INFEASIBLE';
+end
+
+try
+    x = res.sol.itr.y;
+catch
+    x = nan(length(model.c),1);    
+end
+
+if model.options.saveduals & ~isempty(x)
+    D_struc = [res.sol.itr.xx];    
+    top = 1;
+    for i = 1:length(model.K.s)
+        X = zeros(model.K.s(i));
+        n = model.K.s(i);
+        I = find(tril(ones(n)));
+        X(I) = res.sol.itr.barx(top:((top+n*(n+1)/2)-1));
+        X = X + tril(X,-1)';
+        D_struc = [D_struc;X(:)];
+        top = top + n*(n+1)/2;
+    end
+else
+    D_struc = [];
+end
+
+switch res.sol.itr.prosta
+    case 'PRIMAL_AND_DUAL_FEASIBLE'
+        problem = 0;
+    case 'DUAL_INFEASIBLE'
+        problem = 1;
+    case 'PRIMAL_INFEASIBLE'
+        problem = 2;
+    case 'UNKNOWN'
+        problem = 9;
+    otherwise
+        problem = -1;
+end
+
+function [x,D_struc,problem,r,res,solvertime,prob] = call_mosek_lpqp(model);
+
+prob.c = model.c;
+if ~isempty(model.F_struc)
+    prob.a = -model.F_struc(:,2:end);
+    prob.buc = full(model.F_struc(:,1));
+    prob.blc = repmat(-inf,length(prob.buc),1);
+else
+    prob.a = sparse(ones(1,length(model.c))); % Dummy constraint
+    prob.buc = inf;
+    prob.blc = -inf;
+end
+if isempty(model.lb)
+    prob.blx = repmat(-inf,1,length(model.c));
+else
+    prob.blx = model.lb;
+end
+if isempty(model.ub)
+    prob.bux = repmat(inf,1,length(model.c));
+else
+    prob.bux = model.ub;
+end
+
+if model.K.f>0
+    prob.blc(1:model.K.f) = prob.buc(1:model.K.f);
+end
+
+[prob.qosubi,prob.qosubj,prob.qoval] = find(tril(sparse(2*model.Q)));    
+
+if model.K.q(1)>0
+    nof_new = sum(model.K.q);
+    prob.a = [prob.a [spalloc(model.K.f,nof_new,0);spalloc(model.K.l,nof_new,0);speye(nof_new);spalloc(sum(model.K.s.^2),nof_new,0)]];
+    prob.blc(1+model.K.f+model.K.l:model.K.f+model.K.l+sum(model.K.q)) = prob.buc(1+model.K.f+model.K.l:model.K.f+model.K.l+sum(model.K.q)); % Note, fixed the SDP cones too
+      
+    prob.c = [prob.c;zeros(nof_new,1)];
+    top = size(model.F_struc,2)-1;
+    for i = 1:length(model.K.q)
+        prob.cones{i}.type = 'MSK_CT_QUAD';
+        prob.cones{i}.sub  = top+1:top+model.K.q(i);
+        prob.blx(top+1:top+model.K.q(i)) = -inf;
+        prob.bux(top+1:top+model.K.q(i)) = inf;        
+        top = top + model.K.q(i);
+    end    
+end
+
+if ~isempty(model.integer_variables)
+    prob.ints.sub = model.integer_variables;
+end
+
+param = model.options.mosek;
+
+if ~isempty(model.x0)
+    if model.options.usex0
+        prob.sol.int.xx = zeros(max([length(model.Q) size(prob.a,2)]),1);
+        prob.sol.int.xx(model.integer_variables) = x0(model.integer_variables);
+        evalc('[r,res] = mosekopt (''symbcon'')');
+        sc = res.symbcon ;
+        param.MSK_IPAR_MIO_CONSTRUCT_SOL = sc.MSK_ON;
+    end
+end
+
+% Debug?
+if model.options.savedebug
+    save mosekdebug prob param
+end
+
+% Call MOSEK
+solvertime = clock; 
+showprogress('Calling MOSEK',model.options.showprogress);
+if model.options.verbose == 0
+    [r,res] = mosekopt('minimize echo(0)',prob,param); 
+else
+    [r,res] = mosekopt('minimize',prob,param);     
+end
+solvertime = etime(clock,solvertime);
+
+if (r == 1010) || (r == 1011) | (r==1001)
+    problem = -5;
+    x = [];
+    D_struc = [];
+elseif r == 1200
+    problem = 7;
+    x = [];
+    D_struc = [];
+else
+    % Recover solutions
+    sol = res.sol;
+    if isempty(model.integer_variables)
+        x = sol.itr.xx(1:length(model.c)); % Might have added new ones                
+        D_struc = (sol.itr.suc-sol.itr.slc);        
+        error_message = sol.itr.prosta;
+    else        
+        try
+        x = sol.int.xx(1:length(model.c)); % Might have added new ones
+        D_struc = [];
+        error_message = sol.int.prosta;
+        catch
+            x = [];
+            error_message = 'crash';
+            D_struc = [];
+        end
+      
+    end
+    
+    switch error_message
+    case {'PRIMAL_AND_DUAL_FEASIBLE','PRIMAL_FEASIBLE'}
+        problem = 0;
+    case 'PRIMAL_INFEASIBLE'
+        problem = 1;
+    case 'DUAL_INFEASIBLE'        
+        problem = 2;
+    case 'PRIMAL_INFEASIBLE_OR_UNBOUNDED'
+        problem = 12;
+    otherwise
+        problem = -1;
+    end
 end
