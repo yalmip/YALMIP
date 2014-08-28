6de2dad424a04d1612bdccb651b3e68807650a74
johanlofberg <johanl@isy.liu.se>
2014-08-28 08:44:17 +0200

    Use plus to implement

--- extras/@lmi/and.m
+++ extras/@lmi/and.m
@@ -3,9 +3,6 @@ function sys = and(X,Y)
 %   
 %   See also   LMI
 
-% Author Johan Löfberg
-% $Id: and.m,v 1.3 2005-02-04 10:10:26 johanl Exp $
-
 % TODO : Check if binaries etc
 if isa(X,'sdpvar')
     X = true(X);
@@ -21,11 +18,4 @@ if isa(Y,'constraint')
     Y = set(Y);
 end
 
-nX = size(X.clauses,2); % Number of clauses in X
-nY = size(Y.clauses,2); % Number of clauses in Y
-sys = X;
-% Add the two objects (append Y to X)
-for i = 1:nY;    
-    sys.clauses{i+nX} = Y.clauses{i};    
-end
-sys.LMIid = [X.LMIid Y.LMIid];
+sys = plus(X,Y);
