db63bb84f4b28a0f2df4b17be9bcfb8cbabf1d37
johanlofberg <johanl@isy.liu.se>
2014-08-28 10:49:06 +0200

    Cleaned up display of constraints

--- extras/@lmi/display.m
+++ extras/@lmi/display.m
@@ -35,16 +35,15 @@ lmiinfo{54} = 'Vectorized second order cone constraints';
 lmiinfo{55}= 'Complementarity constraint';
 lmiinfo{56}= 'Meta constraint';
 
-headers = {'ID','Constraint','Type','Tag'};
+headers = {'ID','Constraint','Tag'};
 rankVariables = yalmip('rankvariables');
 extVariables = yalmip('extvariables');
 if nlmi>0
     for i = 1:nlmi
         
-        data{i,1} = ['#' num2str(i)];
-        data{i,2} = X.clauses{i}.symbolic;
-        data{i,3} = lmiinfo{X.clauses{i}.type};
-        data{i,4} = '';
+        data{i,1} = ['#' num2str(i)];       
+        data{i,2} = lmiinfo{X.clauses{i}.type};
+        data{i,3} = '';
         if length(getvariables(X.clauses{i}.data)) == 1
             if any(ismember(getvariables(X.clauses{i}.data),rankVariables))
                  data{i,3} = 'Rank constraint';                
@@ -54,8 +53,8 @@ if nlmi>0
         if X.clauses{i}.type == 14
             
         elseif X.clauses{i}.type == 56
-            data{i,3} = [data{i,3} ' (' X.clauses{i}.data{1} ')'];  
-            data{i,4} = X.clauses{i}.handle;
+            data{i,2} = [data{i,3} ' (' X.clauses{i}.data{1} ')'];  
+            data{i,3} = X.clauses{i}.handle;
               
         else
             classification = '';
@@ -86,7 +85,7 @@ if nlmi>0
                 end                
             end
             
-            data{i,4} = X.clauses{i}.handle;
+            data{i,3} = X.clauses{i}.handle;
             if ~isreal(X.clauses{i}.data)                
                 classification = [classification ',complex'];
             end
@@ -97,11 +96,11 @@ if nlmi>0
             
             if length(classification)==0
             else                
-                data{i,3} = [data{i,3} ' (' classification(2:end) ')'];
+                data{i,2} = [data{i,2} ' (' classification(2:end) ')'];
             end
 
             if ismember(X.clauses{i}.type,[1 2 3 4 5 9]);
-                data{i,3} = [data{i,3} ' ' num2str(size(X.clauses{i}.data,1)) 'x' num2str(size(X.clauses{i}.data,2))];
+                data{i,2} = [data{i,2} ' ' num2str(size(X.clauses{i}.data,1)) 'x' num2str(size(X.clauses{i}.data,2))];
             end
         end
 
@@ -109,12 +108,10 @@ if nlmi>0
 end
 
 % If no tags, don't show...
-if length([data{:,4}])==0
-    headers = {headers{:,1:3}};
-    data = reshape({data{:,1:3}},length({data{:,1:3}})/3,3);
+if length([data{:,3}])==0
+    headers = {headers{:,1:2}};
+    data = reshape({data{:,1:2}},length({data{:,1:2}})/2,2);
 end
-
-
 
 yalmiptable('',headers,data)
 
