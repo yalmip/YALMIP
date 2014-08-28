5eae222ae0c4602d4129191ed2c9cb04a9ba9a54
johanlofberg <johanl@isy.liu.se>
2014-08-28 14:59:25 +0200

    Recast before flattening

--- extras/@constraint/flatten.m
+++ extras/@constraint/flatten.m
@@ -2,8 +2,4 @@ function sys = flatten(sys)
 
 % Go from an internal format which is hierarchical and performs better
 % when adding many constraint objects.
-if length(sys.clauses)>0
-    if isa(sys.clauses{1},'cell')
-        sys.clauses = [sys.clauses{:}];
-    end
-end
+sys = flatten(lmi(sys));
