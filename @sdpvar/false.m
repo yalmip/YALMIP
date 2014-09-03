d1b8a8ceea44a97bc263747c942f7299b5753cfc
johanlofberg <johanl@isy.liu.se>
2014-09-03 18:14:50 +0200

    Updated help

--- @sdpvar/false.m
+++ @sdpvar/false.m
@@ -1,12 +1,16 @@
 function x = false(x)
 % FALSE Constrains a variable to be negative
 %
-% FALSE(x) returns the constraint set(x<=0).
+% TRUE(x) returns the constraint (x<=0.5). It is assumed that x is
+% binary. The reason (x<=0.5) is used instead of x==0 is that some big-M
+% modelling turns out to be less sensitive to numerical issues in this
+% form. Once the model enters the solver, it will be trivially presolved
+% anyway.
 %
 % Example
 %
-% SET(FALSE(a))    constrains binary variable a to be 0 (i.e. false)
-% SET(FALSE(a|b))  constrains binary variables a and b to be 0 (i.e. false)
+% (FALSE(a))    constrains binary variable a to be 0 (i.e. false)
+% (FALSE(a|b))  constrains binary variables a and b to be 0 (i.e. false)
 %
 %   See also SDPVAR/TRUE, SDPVAR/AND, SDPVAR/OR, SDPVAR/NOT, BINVAR, BINARY
 
--- @sdpvar/true.m
+++ @sdpvar/true.m
@@ -1,16 +1,19 @@
 function x = true(x)
 % TRUE Constrains a variable to be positive
 %
-% TRUE(x) returns the constraint set(x>=1).
+% TRUE(x) returns the constraint (x>=0.5). It is assumed that x is
+% binary. The reason (x>=0.5) is used instead of x==1 is that some big-M
+% modelling turns out to be less sensitive to numerical issues in this
+% form. Once the model enters the solver, it will be trivially presolved
+% anyway.
 %
 % For safety, it is advised to use the TRUE operator when working with
 % logic constraints, instead of relying on the automatic constraints used
 % by YALMIP (expression generated using AND and OR are automatically
 % assumed to be constrained to be true.
 %
-% SET(a|b) is automatically changed to SET(TRUE(a|b)), or equivalently
-% SET((a|b) >= 1), while SET(a) will be interpreted as SET(a>=0). To
-% constrain a to be true, the user has to explicitely use SET(TRUE(a)).
+% (a|b) is automatically changed to (TRUE(a|b)). To constrain a to be true,
+% the user has to explicitely use (TRUE(a)). 
 %
 %   See also SDPVAR/FALSE, SDPVAR/AND, SDPVAR/OR, SDPVAR/NOT, BINVAR, BINARY
 
