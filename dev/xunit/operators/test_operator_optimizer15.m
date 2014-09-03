946f00e0a2a807623528eafc60693d56ea075886
johanlofberg <johanl@isy.liu.se>
2014-09-03 13:51:07 +0200

    Changed solver in test

--- dev/xunit/operators/test_operator_optimizer15.m
+++ dev/xunit/operators/test_operator_optimizer15.m
@@ -2,5 +2,5 @@ function test_operator_optimizer15
 
 yalmip('clear')
 sdpvar x y z
-P = optimizer([x <= y*z],x^2,sdpsettings('solver','+gurobi'),[y;z],x)
+P = optimizer([x <= y*z],x^2,sdpsettings('solver','+quadprog'),[y;z],x)
 mbg_asserttolequal(P{[-2;5]},-10,1e-4);
