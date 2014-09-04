b8f814c1daa475dba3f01f8eea99f14b45b2f298
johanlofberg <johanl@isy.liu.se>
2014-09-04 09:23:47 +0200

    More removal of SET references

--- dev/xunit/bmibnb/test_global_bmibnb_integer.m
+++ dev/xunit/bmibnb/test_global_bmibnb_integer.m
@@ -4,7 +4,7 @@ clear sin % To avoid aby problems from code above
 sdpvar x
 strange = @(x) sdpfun(x,'@(x) sin(10*x)+abs(sin(x))+x');
 obj = strange(x);
-sol=solvesdp(set(integer(x)) + (-pi <= x <= pi),strange(x),sdpsettings('solver','bmibnb'));
+sol=solvesdp((integer(x)) + (-pi <= x <= pi),strange(x),sdpsettings('solver','bmibnb'));
 
 mbg_asserttrue(sol.problem==0);
 mbg_asserttolequal(double(x),-2, 1e-4);
