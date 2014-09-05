59470f83fa762c3f6e3f5700767e8ddd96e1d98b
johanlofberg <johanl@isy.liu.se>
2014-09-05 15:50:10 +0200

    Test sos in optimizer

--- dev/xunit/operators/test_operator_optimizer16.m
+++ dev/xunit/operators/test_operator_optimizer16.m
@@ -12,3 +12,10 @@ p = x^4+(x-a)^2 + a^2;
 solvesos(sos(p-t),-t)
 s2 = double(t);
 mbg_asserttolequal(s1,s2,1e-4);
+
+sdpvar x
+sdpvar a t
+p = x^4+(x-a)^2 + a^2;
+P = optimizer(sos(p-t),-t,sdpsettings('solver','+sdpt3'),a,t);
+s3 = P{2};
+mbg_asserttolequal(s1,s3,1e-4);
