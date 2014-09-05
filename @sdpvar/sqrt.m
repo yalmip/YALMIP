06bf52885bb30baf36c67216e1855b86676b59aa
johanlofberg <johanl@isy.liu.se>
2014-09-05 20:09:20 +0200

    Clean up

--- @sdpvar/sqrt.m
+++ @sdpvar/sqrt.m
@@ -4,20 +4,19 @@ function varargout = sqrt(varargin)
 % t = sqrt(x)
 %
 % The variable t can only be used in concavity preserving
-% operations such as t>1, max t etc.
+% operations such as t>=1, max t etc.
 %
 % When SQRT is used in a problem, the domain constraint
 % (x>=0) is automatically added to the problem.
 %
-% See also SDPVAR, SDPVAR/GEOMEAN
+% In nonconvex cases, use sqrtm instead.
+%
+% See also CPOWER
 
 switch class(varargin{1})
-
-    case 'double' % What is the numerical value of this argument (needed for displays etc)
-        X = sqrt(varargin{1});
-
+    
     case 'sdpvar' % Overloaded operator for SDPVAR objects. Pass on args and save them.
-
+        
         X = varargin{1};
         [n,m] = size(X);
         if is(varargin{1},'real') %& (n*m==1)
@@ -25,60 +24,40 @@ switch class(varargin{1})
         else
             error('SQRT can only be applied to real scalars');
         end
-      
+        
         
     case 'char' % YALMIP send 'model' when it wants the epigraph or hypograph
         switch varargin{1}
             case 'graph'
-            
-            t = varargin{2}; % Second arg is the extended operator variable
-            X = varargin{3}; % Third arg and above are the args user used when defining t.
-            if is(X,'linear')
-                varargout{1} = (cone([(X-1)/2;t],(X+1)/2));
-                varargout{2} = struct('convexity','concave','monotonicity','increasing','definiteness','positive');
-                varargout{3} = X;
-            elseif is(X,'quadratic')
-                [F,x] = check_for_special_cases(X,t);
-                if isempty(F)
-                   varargout{1} = [];
-                varargout{2} = [];
-                varargout{3} = [];
+                
+                t = varargin{2}; % Second arg is the extended operator variable
+                X = varargin{3}; % Third arg and above are the args user used when defining t.
+                if is(X,'linear')
+                    varargout{1} = (cone([(X-1)/2;t],(X+1)/2));
+                    varargout{2} = struct('convexity','concave','monotonicity','increasing','definiteness','positive');
+                    varargout{3} = X;
+                elseif is(X,'quadratic')
+                    [F,x] = check_for_special_cases(X,t);
+                    if isempty(F)
+                        varargout{1} = [];
+                        varargout{2} = [];
+                        varargout{3} = [];
+                    else
+                        varargout{1} = F;
+                        varargout{2} = struct('convexity','convex','monotonicity','none','definiteness','positive');
+                        varargout{3} = x;
+                    end
+                    
                 else
-                    varargout{1} = F;
-                    varargout{2} = struct('convexity','convex','monotonicity','none','definiteness','positive');    
-                    varargout{3} = x;
+                    varargout{1} = [];
+                    varargout{2} = [];
+                    varargout{3} = [];
                 end
                 
-            else
-                varargout{1} = [];
-                varargout{2} = [];
-                varargout{3} = [];
-            end
-            
-            case 'callback'
-                
-                X = varargin{3};
-                [U,L] = derivebounds(X);
-                if L<0
-                    F = (X >= 0);
-                else
-                    F = [];
-                end
-                
-                operator = struct('convexity','concave','monotonicity','increasing','definiteness','positive','model','callback');
-                operator.range = [0 inf];
-                operator.domain = [0 inf];
-                operator.bounds = @bounds;
-                operator.convexhull = @convexhull;
-                operator.derivative = @(x) (1./(2*sqrt(abs(x)+eps)));%Yea, garbage for x<0
-                
-                varargout{1} = F;
-                varargout{2} = operator;
-                varargout{3} = X;
             otherwise
                 varargout{1} = [];
                 varargout{2} = [];
-                varargout{3} = []; 
+                varargout{3} = [];
         end
     otherwise
 end
@@ -116,45 +95,5 @@ if info==0 & nnz(Q)>0
     d = 0.5*(R'\c);
     if p==0 &  f-d'*d>-1e-12
         F = cone([R*x+d;sqrt(f-d'*d)],t);
-    end
-end
-
-
-
-function [L,U] = bounds(xL,xU)
-if xL < 0
-    % The variable is not bounded enough yet
-    L = 0;
-else
-    L = sqrt(xL);
-end
-if xU < 0
-    % This is an infeasible problem
-    L = inf;
-    U = -inf;
-else
-    U = sqrt(xU);
-end
-
-function [Ax, Ay, b] = convexhull(xL,xU)
-if xL < 0 | xU == 0
-    Ax = []
-    Ay = [];
-    b = [];
-else
-    xM = (1/(2*(sqrt(xU)-sqrt(xL))/(xU-xL)))^2;
-    fL = sqrt(xL);
-    fM = sqrt(xM);
-    fU = sqrt(xU);
-    dfL = 1/(2*sqrt(xL));
-    dfM = 1/(2*sqrt(xM));
-    dfU = 1/(2*sqrt(xU));
-    [Ax,Ay,b] = convexhullConcave(xL,xM,xU,fL,fM,fU,dfL,dfM,dfU);
-    remove = isinf(b) | isinf(Ax) | isnan(b);
-    if any(remove)
-        remove = find(remove);
-        Ax(remove)=[];
-        b(remove)=[];
-        Ay(remove)=[];
     end
 end
