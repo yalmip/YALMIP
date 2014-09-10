47d70875a60dda497e79e22239af5cebe3b6967a
johanlofberg <johanl@isy.liu.se>
2014-09-10 07:52:33 +0200

    Fixes #218

--- extras/@ndsdpvar/max.m
+++ extras/@ndsdpvar/max.m
@@ -1,10 +1,24 @@
-function Y = max(X)
+function Y = max(X,aux,workdim)
 % abs (overloaded)
 
-narginchk(1,1);
+switch nargin
+    case 1
+        workdim = 1;
+    case 2
+        error('Two argument version of max for ndsdpvar not implemented yet');  
+    case 3
+        if ~isempty(aux)
+            error('MAX with two matrices to compare and a working dimension is not supported in MATLAB.');
+        end
+    otherwise
+        error('Too many input arguments')
+end
 
-d = size(X);
-dnew = d;
-dnew(1) = 1;
-Y = reshape(max(reshape(X(:),d(1),[])),dnew);
+xdim = size(X);
 
+if workdim > length(xdim)
+    error('Index exceeds matrix dimensions.');
+end
+
+newdim = xdim;newdim(workdim)=1;
+Y = reshape(max(reshape(shiftdim(X,workdim-1),xdim(workdim),[])),newdim);
