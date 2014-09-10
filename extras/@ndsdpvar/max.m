bf69a3ca0813328f6caee71bcb6608c9e52ba18e
johanlofberg <johanl@isy.liu.se>
2014-09-10 13:06:26 +0200

    Support for two arguments in ndsdpvar/max

--- extras/@ndsdpvar/max.m
+++ extras/@ndsdpvar/max.m
@@ -5,7 +5,9 @@ switch nargin
     case 1
         workdim = 1;
     case 2
-        error('Two argument version of max for ndsdpvar not implemented yet');  
+        Y = max(reshape(X,[],1),reshape(aux,[],1));
+        Y = reshape(Y,size(X));
+        return
     case 3
         if ~isempty(aux)
             error('MAX with two matrices to compare and a working dimension is not supported in MATLAB.');
