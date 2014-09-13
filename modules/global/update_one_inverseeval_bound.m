4aa5842909926366070665998ca440446336ec19
johanlofberg <johanl@isy.liu.se>
2014-09-13 13:43:48 +0200

    Fixes #225 (don't perform inverse propagation if bounds are equal, if numerics fails the bounds might get inconsistent)

--- modules/global/update_one_inverseeval_bound.m
+++ modules/global/update_one_inverseeval_bound.m
@@ -3,21 +3,23 @@ function p = update_one_inverseeval_bound(p,i);
 arg = p.evalMap{i}.variableIndex;
 xL = p.lb(arg);
 xU = p.ub(arg);
-fL = p.lb(p.evalMap{i}.computes);
-fU = p.ub(p.evalMap{i}.computes);
-try
-    if ~isempty(p.evalMap{i}.properties.inverse)
-        if strcmpi(p.evalMap{i}.properties.monotonicity,'increasing')
-            invfiL = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
-            invfiU = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
-            p.lb(arg) = max(p.lb(arg),invfiL);
-            p.ub(arg) = min(p.ub(arg),invfiU);
-        elseif strcmpi(p.evalMap{i}.properties.monotonicity,'decreasing')
-            invfiL = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
-            invfiU = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
-            p.lb(arg) = max(p.lb(arg),invfiL);
-            p.ub(arg) = min(p.ub(arg),invfiU);
+if ~all(xL==xU)
+    fL = p.lb(p.evalMap{i}.computes);
+    fU = p.ub(p.evalMap{i}.computes);
+    try
+        if ~isempty(p.evalMap{i}.properties.inverse)
+            if strcmpi(p.evalMap{i}.properties.monotonicity,'increasing')
+                invfiL = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
+                invfiU = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
+                p.lb(arg) = max(p.lb(arg),invfiL);
+                p.ub(arg) = min(p.ub(arg),invfiU);
+            elseif strcmpi(p.evalMap{i}.properties.monotonicity,'decreasing')
+                invfiL = feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1});
+                invfiU = feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1});
+                p.lb(arg) = max(p.lb(arg),invfiL);
+                p.ub(arg) = min(p.ub(arg),invfiU);
+            end
         end
+    catch
     end
-catch
 end
