930e77610afe4b5b44a4497c24e0baa61dd175a0
johanlofberg <johanl@isy.liu.se>
2014-09-16 15:35:28 +0200

    Buffer randoms to speed up

--- extras/gen_rand_hash.m
+++ extras/gen_rand_hash.m
@@ -1,10 +1,20 @@
 function r = gen_rand_hash(k,n,m);
+persistent buffer
+persistent loc
 try
-    % Previous approach does not work with parallell toolbox!
-    s = rng;
-    rng(k);
-    r = rand(n,m);
-    rng(s);
+    % Preallocate 100 samples everytime and empty that buffer first
+    if isempty(buffer) || length(buffer) < loc + n*m
+        % Previous approach does not work with parallell toolbox!
+        s = rng;
+        rng(k);
+        buffer = rand(n*m+100,1);
+        r = reshape(buffer(1:n*m),n,m);
+        loc = 1;
+        rng(s);
+    end
+     r = reshape(buffer(loc:n*m+loc-1),n,m);
+     loc = loc + n*m;
+    
 catch
     % but rng is not available in all versions...
     s = rand('state');
