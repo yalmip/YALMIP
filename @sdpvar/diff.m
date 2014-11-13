e54e5e731f1b0fc423d382f4e0af46cb34b0c616
johanlofberg <johanl@isy.liu.se>
2014-11-13 09:33:01 +0100

    Performance improvement for large-scale diff

--- @sdpvar/diff.m
+++ @sdpvar/diff.m
@@ -5,69 +5,70 @@ X = varargin{1};
 n = X.dim(1);
 m = X.dim(2);
 
-Y = X;
-x_lmi_variables = X.lmi_variables;
-lmi_variables = [];
-
-% Fast diff of large matrices. Convert to optmized case
 switch nargin
     case 1
-        if (min([n m]) > 1)
-            Y = diff(X',1,2)';           
-            return
-        end
+        if n == 1
+            % Default is diff along first dimension, unless we only have
+            % one row. This happens to be the case in which the core function operates            
+            Y = differ(X);
+        else
+            % MATLAB stadnard, just diff along first dimension. Reuse core
+            % code by transposing matrix 
+            Y = differ(X')';
+        end            
+        return
 
-    case 2
-        if varargin{2} == 1 & (min([n m]) > 1)
-            Y = diff(X',1,2)';           
-            return
+    case 2       
+        % Note that MATLAB diff(randn(2,4),2) will cause a diff in two
+        % different directions. 
+        if n == 1
+            if varargin{2}==1 || isempty(varargin{2})
+                Y = differ(X);
+            else
+                % Recursive use
+                Y = diff(differ(X),varargin{2}-1);
+            end
+        else
+            if varargin{2}==1 || isempty(varargin{2})
+                Y = differ(X')';
+            else
+                % Recursive use
+                Y = diff(differ(X')',varargin{2}-1);
+            end           
         end
+        
     case 3
-        if varargin{2}==1 & varargin{3} == 2 & (min([n m]) > 1)
-            shift = [-speye(m-1) spalloc(m-1,1,0)] + [spalloc(m-1,1,0) speye(m-1)];
-            %            shift = shift(1:m-1,1:m);
-            shift = kron(shift,speye(n));
-            Y.basis = shift*X.basis;
-            Y.dim(1) = n;
-            Y.dim(2) = m - 1;
-            % Reset info about conic terms
-            Y.conicinfo = [0 0];
-            Y = flush(Y);
-            Y = clean(Y);
-            return
-        elseif varargin{2}==1 & varargin{3} == 1  & (min([n m]) > 1)
-            Y = diff(X',1,2)';
+        if X.dim(varargin{3})-varargin{2} <=0
+            dim = X.dim;
+            dim(varargin{3}) = 0;
+            Y = zeros(dim);
+        elseif varargin{2}==1            
+            if varargin{3} == 2
+                Y = differ(X);
+            elseif varargin{3} == 1 || isempty(varargin{3})
+                Y = differ(X')';
+            end
+        elseif varargin{2} > 1
+            % Recursive call for higher-order diff
+            Y = diff(diff(X,varargin{2}-1,varargin{3}),1,varargin{3});
             return
         end
-
     otherwise
         error('To many input arguments.')
 end
 
-% Slow but safe case. Used for higher order diff
-try
-    j = 1;
-    diffX = diff(reshape(X.basis(:,1),n,m),varargin{2:end});
-    Y.basis=diffX(:);
-    for i = 1:length(x_lmi_variables)
-        diffX = diff(reshape(X.basis(:,i+1),n,m),varargin{2:end});
-        if (norm(diffX,inf)>0)
-            Y.basis(:,j+1) = diffX(:);
-            lmi_variables = [lmi_variables x_lmi_variables(i)];
-            j = j+1;
-        end
-    end
-    if j~=1
-        Y.lmi_variables = lmi_variables;
-    else
-        Y = full(reshape(Y.basis(:,1),size(diffX,1),size(diffX,2)));
-        return
-    end
-    Y = flush(Y);
-catch
-    error(lasterr)
-end
-Y.dim(1) = size(diffX,1);
-Y.dim(2) = size(diffX,2);
+
+function Y = differ(X)
+
+n = X.dim(1);
+m = X.dim(2);
+Y = X;
+shift = [-speye(m-1) spalloc(m-1,1,0)] + [spalloc(m-1,1,0) speye(m-1)];
+shift = kron(shift,speye(n));
+Y.basis = shift*X.basis;
+Y.dim(1) = n;
+Y.dim(2) = m - 1;
 % Reset info about conic terms
 Y.conicinfo = [0 0];
+Y = flush(Y);
+Y = clean(Y);
