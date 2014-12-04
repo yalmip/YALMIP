c6ad40021805225f2e4db7fc486543b7b36239a6
johanlofberg <johanl@isy.liu.se>
2014-12-04 15:25:24 +0100

    Fixes #252

--- @sdpvar/not.m
+++ @sdpvar/not.m
@@ -1,14 +1,51 @@
 function varargout = not(varargin)
 %NOT (overloaded)
 %   
-%    y = not(x)       
-%    y = ~x    
+%    z = ~x
+%    z = not(x)
 %
-% Short for y = 1-x.
+% It is assumed that x and y are binary variables (either explicitely
+% declared using BINVAR, or constrained using BINARY.)
 %
-% It is assumed that x is a binary variable (either explicitely declared
-% using BINVAR, or constrained using BINARY.)
-%
-%    See also SDPVAR/OR, SDPVAR/AND, BINVAR, BINARY
+% See also SDPVAR/ANS, SDPVAR/OR, SDPVAR/XOR, BINVAR, BINARY
 
-varargout{1}=1-varargin{1};
+switch class(varargin{1})
+    case 'char'
+        
+        z = varargin{2};        
+        xy = [];
+        allextvars = yalmip('extvariables');
+     
+        x = varargin{3};
+        xvars = getvariables(x);
+        if (length(xvars)==1) & ismembc(xvars,allextvars)
+            x = expandnot(x,allextvars);
+        end
+                
+        varargout{1} = z == 1-x;
+        varargout{2} = struct('convexity','none','monotonicity','none','definiteness','none','model','integer');
+        varargout{3} = x;
+
+    case 'sdpvar'
+        if nargin > 1
+            error('Wrong numbers of arguments.');
+        end
+        varargout{1} = yalmip('define','not',varargin{1});
+
+    otherwise
+end
+
+function x = expandnot(x,allextvars)
+
+xmodel = yalmip('extstruct',getvariables(x));
+
+if isequal(xmodel.fcn,'not')
+    x = [];
+    for i = 1:length(xmodel.arg)
+        xi = xmodel.arg{i};
+        if  ismembc(getvariables(xi),allextvars)
+            xi = expandnot(xi,allextvars);
+        end
+        x = [x xi];
+    end
+end
