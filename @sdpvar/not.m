30bc04b40b5cc7f9e24fff05c01b998d06ce9909
johanlofberg <johanl@isy.liu.se>
2014-12-05 11:16:17 +0100

    Typo in help

--- @sdpvar/not.m
+++ @sdpvar/not.m
@@ -4,10 +4,10 @@ function varargout = not(varargin)
 %    z = ~x
 %    z = not(x)
 %
-% It is assumed that x and y are binary variables (either explicitely
+% It is assumed that x is a binary variables (either explicitely
 % declared using BINVAR, or constrained using BINARY.)
 %
-% See also SDPVAR/ANS, SDPVAR/OR, SDPVAR/XOR, BINVAR, BINARY
+% See also SDPVAR/AND, SDPVAR/OR, SDPVAR/XOR, BINVAR, BINARY
 
 switch class(varargin{1})
     case 'char'
