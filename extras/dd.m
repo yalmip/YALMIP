2c5f45c4c901fe28158fd043e9dc90a122607f86
johanlofberg <johanl@isy.liu.se>
2014-12-11 15:03:15 +0100

    Model abs manually for speed

--- extras/dd.m
+++ extras/dd.m
@@ -1,7 +1,9 @@
 function Constraint = dd(X)
 
 if issymmetric(X)
-    Constraint = [diag(X) >= sum(abs(X-diag(diag(X))),2)];
+    W = X-diag(diag(X));
+    Z = sdpvar(length(X));
+    Constraint = [-Z(:) <= W(:) <= Z(:), diag(X) >= sum(Z,2)];
 else
     error('dd requires a symmetric argument.');
 end
