c48dafc97e557e9c78d08459d7e702d5583fbb35
Johan Löfberg <johanl@isy.liu.se>
2015-05-07 10:51:47 +0200

    Support for new SCS format. Fixes #295

--- solvers/callscs.m
+++ solvers/callscs.m
@@ -19,7 +19,11 @@ end
 data.A = -F_struc(:,2:end);
 data.b = full(F_struc(:,1));
 data.c = full(c);
-cones = K;
+cones = [];
+cones.f = K.f;
+cones.l = K.l;
+cones.q = K.q;
+cones.s = K.s;
 
 % Now add exponential cone information
 if ~isempty(model.evalMap)
@@ -252,7 +256,28 @@ if options.savedebug
     save scsdebug data cones
 end
 
-tic
+% Extract lower diagonal form for new SCS format
+sdpA = data.A(1+cones.l + cones.f+sum(cones.q):end,:);
+sdpb = data.b(1+cones.l + cones.f+sum(cones.q):end,:);
+data.A = data.A(1:cones.l + cones.f+sum(cones.q),:);
+data.b = data.b(1:cones.l + cones.f+sum(cones.q),:);
+top = 1;
+for i = 1:length(cones.s)
+    A = sdpA(top:top + cones.s(i)^2-1,:);
+    b = sdpb(top:top + cones.s(i)^2-1,:);
+    n = cones.s(i);
+    ind = find(speye(n));
+    b(ind) = b(ind)/sqrt(2);
+    A(ind,:) = A(ind,:)/sqrt(2);
+    ind = find(tril(ones(n)));
+    A = A(ind,:);
+    b = b(ind);
+    data.A = [data.A;A];
+    data.b = [data.b;b];
+    top = top  + cones.s(i)^2;
+end
+
+t = tic;
 problem = 0;  
 switch  model.solver.tag
     case 'scs-direct'
@@ -260,15 +285,29 @@ switch  model.solver.tag
     otherwise
         [x_s,y_s,s,info] = scs_indirect(data,cones,params);
 end
-solvertime = toc;
+solvertime = toc(t);
 
 % Internal format. Only recover the original variables
-Primal = H*x_s+xsol; 
+Primal = H*x_s+xsol;
 Primal = Primal(1:length(model.c));
-if isempty(model.evalMap)
+if ~isempty(model.evalMap)
+    % No support for duals when exponential cones yet
     Dual = [];
 else
-    Dual = y_s;
+    % Map to full format from tril
+    Dual = y_s(1:cones.f+cones.l+sum(cones.q));
+    top = 1 + cones.f + cones.l + sum(cones.q);
+    for i = 1:length(cones.s)
+        n = cones.s(i);
+        sdpdual = y_s(top:top + n*(n+1)/2-1,:);        
+        Z = zeros(n);
+        Z(find(tril(ones(n)))) = sdpdual;
+        Z = (Z + Z')/2;
+        ind = find(speye(n));
+        Z(ind) = Z(ind)/sqrt(2);
+        Dual = [Dual;Z(:)];
+        top = top  + n*(n+1)/2;
+    end
 end
 
 switch info.status
