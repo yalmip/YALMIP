16b838707125e709d2dc9c022dea1bcf9f9c5830
Johan Löfberg <johanl@isy.liu.se>
2015-05-08 15:04:42 +0200

    Slight performance tweak

--- extras/uniquestripped.m
+++ extras/uniquestripped.m
@@ -2,5 +2,8 @@ function b = uniquestripped(a)
 %UNIQUESTRIPPED  Internal function (version without checkings etc.)
 
 b = sort(a(:)');
-b = b(diff([b NaN])~=0);
+i = diff([b NaN])~=0;
+if ~all(i)
+    b = b(i);
+end
 
