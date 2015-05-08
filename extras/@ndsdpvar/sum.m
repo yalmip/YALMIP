1188547e5b99c1fca8816696beabe01d74b76523
Johan Löfberg <johanl@isy.liu.se>
2015-05-08 15:05:16 +0200

    Smash slices in 3d arrays efficiently

--- extras/@ndsdpvar/sum.m
+++ extras/@ndsdpvar/sum.m
@@ -4,11 +4,17 @@ function X = sum(varargin)
 Y = varargin{1};
 X = Y;
 X.basis = [];
-for i = 1:size(Y.basis,2)
-    base = reshape(full(Y.basis(:,i)),X.dim);
-    base = sum(base,varargin{2:end});
-    X.basis = [X.basis sparse(base(:))];
+if nargin == 2 && isequal(varargin{2},3) && length(X.dim)==3
+    % smash slices
+    X.basis = kron(ones(1,X.dim(end)),speye(prod(X.dim(1:2))))*Y.basis;
+    X.dim = X.dim(1:2);
+else
+    for i = 1:size(Y.basis,2)
+        base = reshape(full(Y.basis(:,i)),X.dim);
+        base = sum(base,varargin{2:end});
+        X.basis = [X.basis sparse(base(:))];
+    end
+    X.dim = size(base);
 end
-X.dim = size(base);
 X.conicinfo = [0 0];
 X = clean(X);
