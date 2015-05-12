f14cb6f4aada2014df6f8b813c123286fee7ea7a
Johan Löfberg <johanl@isy.liu.se>
2015-05-12 17:10:52 +0200

    Recursive check for performance

--- extras/@lmi/depends.m
+++ extras/@lmi/depends.m
@@ -3,11 +3,7 @@ function LinearVariables = depends(F)
 F = flatten(F);
 
 % Get all used variables in this LMI object
-used = [];
-for i = 1:length(F.clauses)  
-    Fivar = getvariables(F.clauses{i}.data);
-    used = uniquestripped([used Fivar(:)']);
-end
+used = recursivedepends(F.clauses);
 
 % Now, determine the involved linear variables
 [mt,variabletype] = yalmip('monomtable');
@@ -16,4 +12,19 @@ if any(variabletype)%1%~isempty(nlv) & any(ismembc(used,nlv(1,:)))
     LinearVariables = LinearVariables(:)';
 else
     LinearVariables = used;
+end
+
+function used = recursivedepends(clauses)
+
+if length(clauses) > 2
+    mid = floor(length(clauses)/2);
+    used1 = recursivedepends({clauses{1:mid}});
+    used2 = recursivedepends({clauses{mid+1:end}});
+    used = uniquestripped([used1 used2]);
+else
+    used = [];
+    for i = 1:length(clauses)
+        Fivar = getvariables(clauses{i}.data);
+        used = uniquestripped([used Fivar(:)']);
+    end
 end
