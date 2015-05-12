8700f07f55f6fb74ba5a68688820b4b575d5c057
Johan Löfberg <johanl@isy.liu.se>
2015-05-12 07:39:13 +0200

    Quick code for ndsdpvar  +- scalar double

--- extras/@ndsdpvar/minus.m
+++ extras/@ndsdpvar/minus.m
@@ -3,22 +3,38 @@ function Z = minus(X,Y)
 
 dim = [];
 if isa(X,'ndsdpvar')
-    dim = X.dim;
-    X = sdpvar(X);
+    if isa(Y,'double') && numel(Y)==1
+        % Quick return for scalar  + ndsdpvar
+        Z = X;Z.basis(:,1) = Z.basis(:,1) - Y;
+        Z.conicinfo = [0 0];
+        Z.extra.opname='';
+        return
+    else
+        dim = X.dim;
+        X = sdpvar(X);
+    end
 elseif isa(X,'double')
     X = X(:);
 end
 
 if isa(Y,'ndsdpvar')
-    if isempty(dim)
-        dim = Y.dim;
+    if isa(X,'double') && numel(X)==1
+        % Quick return for scalar  + ndsdpvar
+        Z = Y;Z.basis = -Z.basis;Z.basis(:,1) = Z.basis(:,1) + X;
+        Z.conicinfo = [0 0];
+        Z.extra.opname='';
+        return
     else
-        if isequal(dim,Y.dim)
+        if isempty(dim)
+            dim = Y.dim;
         else
-            error('Dimension mismatch in nD addition')
+            if isequal(dim,Y.dim)
+            else
+                error('Dimension mismatch in nD addition')
+            end
         end
+        Y = sdpvar(Y);
     end
-    Y = sdpvar(Y);
 elseif isa(Y,'double')
     Y = Y(:);
 end
