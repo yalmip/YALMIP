bc7f8430375ad8f8ce11d6704476b85019930068
Johan Löfberg <johanl@isy.liu.se>
2015-05-12 07:12:56 +0200

    Improved help-text

--- @sdpvar/assign.m
+++ @sdpvar/assign.m
@@ -8,6 +8,10 @@ function assign(X,value,ls)
 %                   will be issued.
 %
 % ASSIGN(X,value,1) Least square assignment.
+%
+% After assigning values to decision variables, these can be communicated
+% to the solver as an initial guess by turning on the option 'usex0' in the
+% options structure sent to optimize.
 
 if nargin<3
     ls = 0;
