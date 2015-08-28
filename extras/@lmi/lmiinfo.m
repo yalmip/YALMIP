277258d3afbcf6585f68e85ab19505e0f20b6106
Johan Löfberg <johanl@isy.liu.se>
2015-08-28 10:16:53 +0200

    Typo caused duals to fail

--- extras/@lmi/lmiinfo.m
+++ extras/@lmi/lmiinfo.m
@@ -8,11 +8,15 @@ info.rlc = [];
 info.pow = [];
 F = flatten(F);
 Counter = length(F.LMIid);
+sdptop = 1;
+info.sdp = zeros(Counter,3);
 for i = 1:Counter
     Fi = F.clauses{i}.data;
     switch  F.clauses{i}.type;
         case {1,9,40}
-            info.sdp = [info.sdp;size(Fi,1) size(Fi,2) F.LMIid(i)];
+            % Slightly faster, quick fix for user case...
+            info.sdp(sdptop,:) = [size(Fi,1) size(Fi,2) F.LMIid(i)];
+            sdptop = sdptop  + 1;
         case 2
             info.lin = [info.lin;size(Fi,1) size(Fi,2) F.LMIid(i)];
         case 3
@@ -28,3 +32,4 @@ for i = 1:Counter
             error('Error in lmiinfo. Please report bug');
     end
 end
+info.sdp = info.sdp(1:sdptop-1,:);
