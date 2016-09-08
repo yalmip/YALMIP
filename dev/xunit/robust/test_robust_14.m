7fb262c9cace917cfeab7a9bbc53602fe377d633
Johan Löfberg <johanl@isy.liu.se>
2016-09-08 12:58:19 +0200

    New test-case for conic robust and momentmodel

---
+++ dev/xunit/robust/test_robust_14.m
@@ -0,0 +1,10 @@
+function test_robust_14
+
+sdpvar t x 
+w = sdpvar(2,1);
+M = momentmodel([w'*w == 1, w>=0.5])
+P = sosmodel(sos(1 + w(1)*x + w(2)*x^2 - t),[],[],[t;w]);
+optimize([M,P,uncertain(M)],-t)
+mbg_asserttrue(sol.problem == 0);
+mbg_asserttolequal(double(t),.625, 1e-3);
+
