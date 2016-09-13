84f2dcffee4807692ec494d8130013dff903f252
Johan Löfberg <johanl@isy.liu.se>
2016-09-13 17:26:47 +0200

    Real fix for #345

---
+++ dev/xunit/operators/test_operator_max.m
@@ -0,0 +1,11 @@
+function test_operator_max
+
+% Test for bug #345
+a=sdpvar(1,1);
+b=sdpvar(1,1);
+C=[];
+C=[C,a==1];
+C=[C,b==max(a,0)];
+sol = optimize(C)
+assertElementsAlmostEqual(value(b),0, 'absolute',1e-4);
+
