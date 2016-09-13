bcf211d002f7dae3d9a2496f818a83210f866fb1
Johan Löfberg <johanl@isy.liu.se>
2016-09-13 12:05:50 +0200

    Partition continuous and binary variables for POP

--- modules/parametric/mpt2pop.m
+++ modules/parametric/mpt2pop.m
@@ -1,16 +1,21 @@
 function POP_problem = mpt2pop(Matrices)
 
+n = length(Matrices.free_var);
+varcon = setdiff(1:n,Matrices.binary_var_index);
+varbin = Matrices.binary_var_index;
+
 POP_problem.Ht = full(Matrices.F)';
 POP_problem.Qt = full(Matrices.Y);
 POP_problem.Q = full(Matrices.H);
-POP_problem.A = full(Matrices.G);
-POP_problem.E = [];
+POP_problem.A = full(Matrices.G(:,varcon));
+POP_problem.E = full(Matrices.G(:,varbin));
 POP_problem.F = full(Matrices.E);
 POP_problem.b = full(Matrices.W);
 POP_problem.c = full(Matrices.Cf');
 POP_problem.ct = full(Matrices.Cx');
 POP_problem.cc = full(Matrices.Cc);
-POP_problem.Aeq = full(Matrices.Aeq);
+POP_problem.Aeq = full(Matrices.Aeq(:,varcon));
+POP_problem.Eeq = full(Matrices.Aeq(:,varbin));
 POP_problem.Feq = full(Matrices.Beq);
 POP_problem.beq = full(Matrices.beq);
 POP_problem.CRA = full(Matrices.bndA);
