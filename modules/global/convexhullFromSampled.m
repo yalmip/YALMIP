37af68b765977c08573bc3b29efbf537cd4862aa
Johan Löfberg <johanl@isy.liu.se>
2016-09-23 19:44:38 +0200

    Fixes #377

---
+++ modules/global/convexhullFromSampled.m
@@ -0,0 +1,9 @@
+function [Ax,Ay,b] = convexhullFromSampled(z,fz,xL,xU);
+
+k1 = max((fz(2:end)-fz(1))./(z(2:end)-xL))+1e-12;
+k2 = min((fz(2:end)-fz(1))./(z(2:end)-xL))-1e-12;
+k3 = min((fz(1:end-1)-fz(end))./(z(1:end-1)-xU))+1e-12;
+k4 = max((fz(1:end-1)-fz(end))./(z(1:end-1)-xU))-1e-12;
+Ax = [-k1;k2;-k3;k4];
+Ay = [1;-1;1;-1];
+b =  [k1*(-z(1)) + fz(1);-(k2*(-z(1)) + fz(1));k3*(-z(end)) + fz(end);-(k4*(-z(end)) + fz(end))];
