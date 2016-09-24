3c111382e4211fbbb78dab08d3e06d3cf0b0b1a4
Johan Löfberg <johanl@isy.liu.se>
2016-09-24 15:06:19 +0200

    Another fi for cplex - matlab 2016 bug

--- extras/prunecplexoptions.m
+++ extras/prunecplexoptions.m
@@ -1,7 +1,11 @@
 function options = prunecplexoptions(options)
 
 try
-    o1 = cplexoptimset('cplex');
+    if strfind(version,'2016')
+        o1 = cplexoptimset;
+    else
+        o1 = cplexoptimset('cplex');
+    end
     o2 = options.cplex;
     n = fieldnames(o1);
     for i = 1:length(n)
