5002f162bde20238d320143aa85a03394af17ca1
Johan Löfberg <johanl@isy.liu.se>
2016-09-26 09:31:52 +0200

    Wrong segment detected

--- operators/interp1_internal.m
+++ operators/interp1_internal.m
@@ -27,14 +27,14 @@ yv = varargin{2};
 if xL <= xv(1)
     index1 = 1;
 else
-    index1 = find(xL > xv);index1 = max(1,min(index1)-1);
+    index1 = find(xL > xv);index1 = max(1,max(index1));
 end
-
 if xU >= xv(end)
     index2 = length(xv);
 else
     index2 = find(xv < xU);index2 = min(length(xv),max(index2)+1);
 end
+
 if isequal(varargin{3},'linear')
     Z = yv(index1:index2);
     L = min(yv(index1:index2));
@@ -42,7 +42,7 @@ if isequal(varargin{3},'linear')
 else
     N = ceil((xv(index2)-xv(index1))/(mean(diff(xv))/100));
     z = linspace(xv(index1),xv(index2),N);
-    yz = interp1(xv,yv,z,varargin{3});    
+    yz = interp1(xv,yv,z,varargin{3});   
     L = min(yz);
-    U = max(yz);
+    U = max(yz);    
 end
