3d8705895130d5b51318d87c81ea437b58f4d58e
Johan Löfberg <johanl@isy.liu.se>
2016-09-26 14:26:56 +0200

    Allow user-specified samplers

--- extras/@optimizer/sample.m
+++ extras/@optimizer/sample.m
@@ -6,18 +6,30 @@ allSamples = {};
 for k = 1:N
     cells = cell(1,length(self.diminOrig));
     for i = 1:length(self.diminOrig)
-        if ~isempty(self.input.stochastics{i});
-            temp = {@random,self.input.stochastics{i}.name,self.input.stochastics{1}.parameters{:},self.diminOrig{i}};
-            sampledData = feval(temp{:});
-            cells{i} = sampledData;
-        else
-            cells{i} = [];
+        if isfield(self.input,'stochastics')
+            if ~isempty(self.input.stochastics{i});
+                if isa(self.input.stochastics{i}.name,'function_handle')
+                    temp = {self.input.stochastics{i}.name,self.input.stochastics{1}.parameters{:}};
+                    sampledData = feval(temp{:});
+                    cells{i} = sampledData;
+                else
+                    temp = {@random,self.input.stochastics{i}.name,self.input.stochastics{1}.parameters{:},self.diminOrig{i}};
+                    sampledData = feval(temp{:});
+                    cells{i} = sampledData;
+                end
+            else
+                cells{i} = [];
+            end
         end
     end
-    y.type = '{}';
-    y.subs{1} = cells;
-    y.subs{2} = 'nosolve';
-    allSamples{end + 1} = subsref(self,y);
+    if ~isempty(cells)
+        y.type = '{}';
+        y.subs{1} = cells;
+        y.subs{2} = 'nosolve';
+        allSamples{end + 1} = subsref(self,y);
+    else
+        return
+    end
 end
 self = horzcat(allSamples{:});
 
