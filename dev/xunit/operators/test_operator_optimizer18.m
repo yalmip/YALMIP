75fa3878684a87335922ea699a3f347047be3cb9
Johan Löfberg <johanl@isy.liu.se>
2016-09-29 19:56:44 +0200

    Avoid test failure due to random effects in test problem

--- dev/xunit/operators/test_operator_optimizer18.m
+++ dev/xunit/operators/test_operator_optimizer18.m
@@ -5,9 +5,9 @@ x = sdpvar(2,1);
 c = sdpvar(2,1);
 P = optimizer([c'*x <= 1],-sum(x),ops,{c},[x]);
 S = [];
-for i = 1:50
+for i = 1:500
     ci = randn(2,1);ci = ci/norm(ci);
     S = [S,P{c == ci,'nosolve'}];
 end
 sol = S{[]};
-mbg_asserttolequal(sol,[.7;.7],1e-1);
+mbg_asserttolequal(sol,[.7;.7],2e-1);
