a1d8634a7477c6f69469aaa165b3aa528dba9896
Johan Löfberg <johanl@isy.liu.se>
2016-09-30 11:37:24 +0200

    Better display of optimizer

--- extras/@optimizer/display.m
+++ extras/@optimizer/display.m
@@ -9,7 +9,7 @@ m = 0;
 for i = 1:length(X.dimoutOrig)
     m = m + prod(X.dimoutOrig{i});
 end
-text = ['Optimizer object with ' num2str(n) ' inputs and ' num2str(m) ' outputs.'];
+text = ['Optimizer object with ' num2str(n) ' inputs (' num2str(length(X.diminOrig)) ' blocks) and ' num2str(m) ' outputs (' num2str(length(X.dimoutOrig)) ' blocks).'];
 if isempty(X.ParametricSolution)
     text = [text ' Solver: ' upper(X.model.solver.tag)];
 else
