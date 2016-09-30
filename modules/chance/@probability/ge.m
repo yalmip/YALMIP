d52ab4409f6b4ac97a274b380d8369afc09852a9
Johan Löfberg <johanl@isy.liu.se>
2016-09-30 13:13:49 +0200

    Don't check level if it is a decision variable

--- modules/chance/@probability/ge.m
+++ modules/chance/@probability/ge.m
@@ -4,7 +4,7 @@ if isa(P,'double') & isa(level,'probability')
     error('Currently only supports [probability(f) >= level')
 end
 
-if level < 0 || level > 1
+if isa(level,'double') && (level < 0 || level > 1)
     error('The confidence level must be between 0 and 1');
 end
 
