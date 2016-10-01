c30f1b175365f3c4d4f7130f081efa262c3ba768
Johan Löfberg <johanl@isy.liu.se>
2016-10-01 15:31:17 +0200

    Handle combinations of normal distributions

--- modules/chance/derandomize.m
+++ modules/chance/derandomize.m
@@ -17,12 +17,12 @@ keep = ones(length(F),1);
 keep(randomDeclarations)=0;
 keep(chanceDeclarations)=0;
 randomVariables = extractRandomDefinitions(F(randomDeclarations));
+[randomVariables,map] = mergeDistributions(randomVariables);
 groupedChanceConstraints = groupchanceconstraints(F);
 
 [Fderandomized,eliminatedConstraints] = deriveChanceModel(groupedChanceConstraints,randomVariables);
-
-Fderandomized = Fderandomized + F(find(keep)) + F(find(keep(~eliminatedConstraints))) + F(randomDeclarations);
-
+Fderandomized = Fderandomized + F(find(keep)) + F(find(keep(~eliminatedConstraints)));
+Fderandomized(find(is(Fderandomized,'random'))) = [];
 
 function [Fderandomized,eliminatedConstraints] = deriveChanceModel(groupedChanceConstraints,randomVariables);
 
@@ -89,24 +89,40 @@ for uncertaintyGroup = 1:length(randomVariables)
             if strcmp(func2str(randomVariables{uncertaintyGroup}.distribution.name),'random')                
                 confidencelevel = struct(groupedChanceConstraints{ic}).clauses{1}.confidencelevel;
                 switch randomVariables{uncertaintyGroup}.distribution.parameters{1}
-                    case 'normal'
+                    case {'normal','normalm'}
                         theMean    = randomVariables{uncertaintyGroup}.distribution.parameters{2};
                         covariance = randomVariables{uncertaintyGroup}.distribution.parameters{3};
                         gamma = icdf('normal',confidencelevel,0,1);
+                        if min(size(covariance))==1
+                            covariance = diag(covariance);
+                        end
                         if isa(covariance,'sdpvar')
                             error('Covariance cannot be an SDPVAR in normal distribution. Maybe you meant to use factorized covariance in ''normalf''');
                         end
                         if isempty(x)
                             Fderandomized = [Fderandomized, b + c_wTbase*theMean >= gamma*norm(chol(covariance)*(c_wTbase'))];
                         else
-                            Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm(chol(covariance)*(AAA'*x+c_wTbase'))];
+                            if isa(gamma,'sdpvar')
+                                % This will be a nasty nonlinear model, so
+                                % we cannot use SOCP-based norm operator
+                                e = chol(covariance)*(AAA'*x+c_wTbase');
+                                %Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*sqrtm(e'*e)];
+                                Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm_callback(e)];
+                            else
+                                Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm(chol(covariance)*(AAA'*x+c_wTbase'))];
+                            end
                         end
                         eliminatedConstraints(ic)=1;
                     case 'normalf'
                         theMean    = randomVariables{uncertaintyGroup}.distribution.parameters{2};
                         covariance = randomVariables{uncertaintyGroup}.distribution.parameters{3};
                         gamma = icdf('normal',confidencelevel,0,1);
-                        Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm(covariance*(AAA'*x+c_wTbase'))];
+                        if isa(gamma,'sdpvar')
+                            e = covariance*(AAA'*x+c_wTbase');
+                            Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm_callback(e)];
+                        else
+                            Fderandomized = [Fderandomized, b + c_wTbase*theMean - (ccc + AAA*theMean)'*x >= gamma*norm(covariance*(AAA'*x+c_wTbase'))];
+                        end
                         eliminatedConstraints(ic)=1;
                     otherwise
                         error('Distribution not supported');
@@ -114,4 +130,97 @@ for uncertaintyGroup = 1:length(randomVariables)
             end
         end
     end
+end
+
+function [merged,map] = mergeDistributions(randomVariables);
+
+merged = {};
+map = [];
+used = zeros(1,length(randomVariables));
+mergedindex = 1;
+for i = 1:length(randomVariables)
+    if ~used(i)
+        this = randomVariables{i};
+        used(i) = 1;
+        for j = i+1:length(randomVariables)
+            if ~used(j)
+                that = randomVariables{j};
+                if strcmp(func2str(this.distribution.name),'random') && strcmp(func2str(that.distribution.name),'random')
+                    if mergable(this.distribution.parameters{1},that.distribution.parameters{1})
+                        % Same distribution                                                  
+                        this = merge2distributions(this,that);                            
+                        map(j) = mergedindex;
+                        used(j) = 1;                      
+                    end
+                end
+            end
+        end
+        merged{mergedindex} = this;
+        map(i) = mergedindex;
+        mergedindex = mergedindex + 1;
+    end
+end
+    
+    
+function C = merge2distributions(A,B)
+
+C = A;
+C.variables = [A.variables;B.variables];
+if any(strcmp(A.distribution.parameters{1},{'normal','normalf','normalm'})) || any(strcmp(A.distribution.parameters{1},{'normal','normalf','normalm'}))
+
+    % A bit messy as there are two additional forms for normal
+    % distributions, used to define mv normals, and normals with a
+    % factorized covariance
+    
+    % First, normalize to matrix format
+    dimAvar = size(A.distribution.parameters{3});
+    dimBvar = size(B.distribution.parameters{3});
+    if dimAvar(1) ~= dimAvar(2)
+        varA = diag(A.distribution.parameters{3});
+    else
+        varA = A.distribution.parameters{3};
+    end
+    if dimBvar(1) ~= dimBvar(2)
+        varB = diag(B.distribution.parameters{3});
+    else
+        varB = B.distribution.parameters{3};
+    end
+    % Is A in factor form, but not B, and vice versa. If so, put the other
+    % one in factor form too    
+    if strcmp(A.distribution.parameters{1},'normalf') &&  any(strcmp(B.distribution.parameters{1},{'normal','normalm'}))
+        varB = chol(varB);        
+    elseif strcmp(B.distribution.parameters{1},'normalf') &&  any(strcmp(A.distribution.parameters{1},{'normal','normalm'}))
+        varA = chol(varA);        
+    end
+    varC = blkdiag(varA,varB);
+    
+    % Sort out various combinations of normal stuff in different forms
+    if strcmp(A.distribution.parameters{1},'normal') && strcmp(B.distribution.parameters{1},'normal')
+        varC = diag(varC);
+        C.distribution.parameters{1} = 'normal';
+        C.distribution.parameters{2} = [A.distribution.parameters{2};B.distribution.parameters{2}];
+        C.distribution.parameters{3} = diag(varC);        
+    elseif strcmp(A.distribution.parameters{1},'normalf') ||  any(strcmp(B.distribution.parameters{1},'normalf'))
+        C.distribution.parameters{1} = 'normalf';
+        C.distribution.parameters{2} = [A.distribution.parameters{2};B.distribution.parameters{2}];
+        C.distribution.parameters{3} = varC;        
+    else
+        C.distribution.parameters{1} = 'normalm';
+        C.distribution.parameters{2} = [A.distribution.parameters{2};B.distribution.parameters{2}];
+        C.distribution.parameters{3} = varC;        
+    end        
+else
+    for k = 2:length(A.distribution.parameters)
+        C.distribution.parameters{k} = [A.distribution.parameters{k};B.distribution.parameters{k}];
+    end    
+end
+    
+
+function yesno = mergable(a,b)
+if isequal(a,b)
+    yesno = 1
+elseif any(strcmp(a,{'normal','normalf','normalm'})) && any(strcmp(b,{'normal','normalf','normalm'}))
+    yesno = 1;
+else
+    yesno = 0;
 end
