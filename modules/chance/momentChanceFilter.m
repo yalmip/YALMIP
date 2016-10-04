8ef2d7ee29c5a4fd1b22d9239b1f969463573e8b
Johan Löfberg <johanl@isy.liu.se>
2016-10-04 08:21:22 +0200

    Cleaned up expression

--- modules/chance/momentChanceFilter.m
+++ modules/chance/momentChanceFilter.m
@@ -2,10 +2,10 @@ function model = momentChanceFilter(b,c,distribution,confidencelevel,w,options)
 % Chance filter for distribution only specified by mean and variance
 theMean    = distribution.parameters{2};
 covariance = distribution.parameters{3};
-X = covariance + theMean*theMean';
-S = chol(X);
+
+S = chol(covariance);
 gamma = sqrtm(confidencelevel);
-e = [S*c+b*(inv(S')*theMean);b*sqrtm(1-theMean'*inv(X)*theMean)];
+e = [S*c;b + c'*theMean];
 if isa(gamma,'sdpvar')
     model =  b + c'*theMean >= gamma*norm_callback(e);
 else
