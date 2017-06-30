8f95f8a00670d5ae3db3c668a03d5877af961a08
Johan Löfberg <johanl@isy.liu.se>
2017-06-30 10:00:57 +0200

    Implements #428

--- @sdpvar/interp1.m
+++ @sdpvar/interp1.m
@@ -13,6 +13,20 @@ switch class(varargin{3})
             varargin{4} = 'linear';
         end
         
+        if ~isa(varargin{1},'double') || ~isa(varargin{2},'double')
+            error('First 2 arguments in interp1 should be approximation data');
+        end
+        
+        if any(diff(varargin{1})<0)
+            error('First arguments in interp1 should be monotonically increasing');
+        end
+        
+        if isequal(varargin{4},'graph')
+            if  ~isconvexdata(varargin{1},varargin{2}) && ~isconvexdata(varargin{1},-varargin{2})
+                error('Data has to be convex or concave for graph approximant');
+            end
+        end
+        
         % Reorder arguments to make sure sdpvar is first argument.
         % Use local version of interp to deal with this
         % Arguments xi yi x method -> x xi yi method
@@ -20,4 +34,12 @@ switch class(varargin{3})
      
     otherwise
         error('SDPVAR/INTERP1 called with strange argument!');
+end
+
+function isconvex = isconvexdata(xi,yi)
+finterp = (yi(1:end-2) + yi(3:end))/2;
+if all(finterp >= yi(2:end-1))
+    isconvex = 1;
+else
+    isconvex = 0;
 end
--- operators/interp1_internal.m
+++ operators/interp1_internal.m
@@ -2,10 +2,10 @@ function varargout = interp1_internal(varargin)
 %INTERP1_INTERNAL (overloaded)
 
 switch class(varargin{1})
-
-    case 'double'        
+    
+    case 'double'
         varargout{1} = interp1(varargin{2},varargin{3},varargin{1},varargin{4});
-      
+        
     case 'char'
         
         t = varargin{2};
@@ -14,15 +14,43 @@ switch class(varargin{1})
         yi = varargin{5};
         method = varargin{6};
         
-        if strcmpi(varargin{end},'milp')                        
-            lambda = sdpvar(length(xi),1)            
-            Model = [sos(lambda), x == lambda'*xi(:), t == lambda'*yi(:),lambda>=0, sum(lambda)==1];
-            operator = struct('convexity','none','monotonicity','none','definiteness','none','model','integer');
+        if strcmpi(varargin{1},'graph')
+            % Convexity propagation wants epi-graph models, so is that what
+            % we have used for modelling?
+            if strcmpi(method,'graph')
+                if isconvexdata(xi,yi)
+                    % Convex case, create epi-graph
+                    Model = [min(xi) <= varargin{3} <= max(xi)];
+                    grads = diff(yi)./diff(xi);
+                    Model = [Model, yi(1:end-1) + grads.*(x - xi(1:end-1)) <= t];
+                    operator = struct('convexity','convex','monotonicity','none','definiteness','none','model','graph');
+                elseif isconvexdata(xi,-yi)
+                    Model = [min(xi) <= varargin{3} <= max(xi)];
+                    grads = diff(yi)./diff(xi);
+                    Model = [Model, yi(1:end-1) + grads.*(x - xi(1:end-1)) >= t];
+                    operator = struct('convexity','concave','monotonicity','none','definiteness','none','model','graph');
+                else
+                    error('Trying to create graph approximant using non-convex data');
+                end
+            else
+                Model = [];
+                operator = [];
+            end
         else
-            Model = [min(xi) <= varargin{3} <= max(xi)];
-            operator = struct('convexity','none','monotonicity','none','definiteness','none','model','callback');          
-            operator.bounds = @bounds;
-        end            
+            if strcmpi(method,'milp')
+                lambda = sdpvar(length(xi),1)
+                Model = [sos(lambda), x == lambda'*xi(:), t == lambda'*yi(:),lambda>=0, sum(lambda)==1];
+                operator = struct('convexity','none','monotonicity','none','definiteness','none','model','integer');
+            elseif strcmpi(method,'graph')
+                Model = [];
+                operator = [];
+            else
+                Model = [min(xi) <= varargin{3} <= max(xi)];
+                operator = struct('convexity','none','monotonicity','none','definiteness','none','model','callback');
+                operator.bounds = @bounds;
+            end
+        end
+        
         varargout{1} = Model;
         varargout{2} = operator;
         varargout{3} = varargin{3};
@@ -53,9 +81,17 @@ if isequal(varargin{3},'linear')
 else
     N = ceil((xv(index2)-xv(index1))/(mean(diff(xv))/100));
     z = linspace(xv(index1),xv(index2),N);
-    yz = interp1(xv,yv,z,varargin{3});   
+    yz = interp1(xv,yv,z,varargin{3});
     % To account for finite grid, we add a precision dependent margin
     dz = (z(2)-z(1));
     L = min(yz)-dz;
-    U = max(yz)+dz;    
+    U = max(yz)+dz;
+end
+
+function isconvex = isconvexdata(xi,yi)
+finterp = (yi(1:end-2) + yi(3:end))/2;
+if all(finterp >= yi(2:end-1))
+    isconvex = 1;
+else
+    isconvex = 0;
 end
