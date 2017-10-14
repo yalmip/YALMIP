440870f6d665ae493f8b15c6f5d9b337ce20dbe9
Johan Löfberg <johanl@isy.liu.se>
2017-10-14 12:27:00 +0200

    Fixes #447 nameclash on crossentropy

---
+++ @sdpvar/crossentropy.m
@@ -0,0 +1,35 @@
+function varargout = crossentropy(varargin)
+% CROSSENTROPY
+%
+% y = CROSSENTROPY(x,y)
+%
+% Computes/declares cross entropy -sum(x.*log(y))
+%
+% See also ENTROPY, KULLBACKLEIBLER
+
+switch class(varargin{1})
+       
+    case {'sdpvar','ndsdpvar'}
+        
+        varargin{1} = reshape(varargin{1},[],1);
+        varargin{2} = reshape(varargin{2},[],1);
+        
+        if length(varargin{1})~=length(varargin{2})
+            if length(varargin{1})==1
+                varargin{1} = repmat(varargin{1},length(varargin{2}),1);
+            elseif  length(varargin{2})==1
+                varargin{2} = repmat(varargin{2},length(varargin{1}),1);
+            else
+                error('Dimension mismatch in crossentropy')
+            end
+        end
+        
+        varargout{1} = yalmip('define','crossentropy_internal',[varargin{1};varargin{2}]);
+            
+    otherwise
+        error('SDPVAR/CROSSENTROPY called with CHAR argument?');
+end
+
+
+
+
--- operators/crossentropy_internal.m
+++ operators/crossentropy_internal.m
@@ -1,4 +1,4 @@
-function varargout = crossentropy(varargin)
+function varargout = crossentropy_internal(varargin)
 % CROSSENTROPY
 %
 % y = CROSSENTROPY(x,y)
@@ -25,25 +25,6 @@ switch class(varargin{1})
         ce = real(ce);
         varargout{1} = -sum(ce);
         
-
-    case {'sdpvar','ndsdpvar'}
-        
-        varargin{1} = reshape(varargin{1},[],1);
-        varargin{2} = reshape(varargin{2},[],1);
-        
-        if length(varargin{1})~=length(varargin{2})
-            if length(varargin{1})==1
-                varargin{1} = repmat(varargin{1},length(varargin{2}),1);
-            elseif  length(varargin{2})==1
-                varargin{2} = repmat(varargin{2},length(varargin{1}),1);
-            else
-                error('Dimension mismatch in crossentropy')
-            end
-        end
-        
-        varargout{1} = yalmip('define',mfilename,[varargin{1};varargin{2}]);
-        
-
     case 'char'
 
         X = varargin{3};
