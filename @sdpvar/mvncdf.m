8a4922f56f7a938b9d5e4cfb3af5b52b80c2a530
Johan Löfberg <johanl@isy.liu.se>
2017-11-21 08:45:49 +0100

    Fixes #462 Add support for mvncdf

---
+++ @sdpvar/mvncdf.m
@@ -0,0 +1,29 @@
+function varargout = mvncdf(varargin)
+%MVNCDF
+%
+% y = MVNCDF(x)
+%
+% Computes/declares multivariate normal cumulative distribution function
+
+switch class(varargin{1})
+
+    case {'sdpvar','ndsdpvar'}
+ 
+        varargin{1} = reshape(varargin{1},[],1);
+        varargout{1} = yalmip('define',mfilename,varargin{1});        
+
+    case 'char'
+
+        X = varargin{3};
+        F = (X >= 0);
+
+        operator = struct('convexity','none','monotonicity','none','definiteness','positive','model','callback');
+        operator.range = [0 1];
+        operator.domain = [0 inf];                        
+        varargout{1} = F;
+        varargout{2} = operator;
+        varargout{3} = X;
+
+    otherwise
+        error('SDPVAR/MVNCDF called with CHAR argument?');
+end
