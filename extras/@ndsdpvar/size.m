bce69e02d37ab85aa672432c729cef6466422e9f
Johan Löfberg <johanl@isy.liu.se>
2018-02-01 19:01:41 +0100

    Fixes #469

--- extras/@ndsdpvar/size.m
+++ extras/@ndsdpvar/size.m
@@ -1,15 +1,22 @@
-function s = size(X,d)
+function varargout = size(X,d)
 % SIZE (overloaded)
 
+if nargin == 1 && nargout > 1 && nargout == length(X.dim)
+    for i = 1:length(X.dim);
+        varargout{i} = X.dim(i);
+    end
+    return
+end
+
 if nargin == 1
-    s = X.dim;
+    varargout{1} = X.dim;
 else
     if (d < 1) || d~=round(d)
         error('Dimension argument must be a positive integer scalar in the range 1 to 2^31.');
     end
     if d <= length(X.dim)
-        s = X.dim(d);
+        varargout{1} = X.dim(d);
     else
-        s = 1;
+        varargout{1} = 1;
     end
 end
