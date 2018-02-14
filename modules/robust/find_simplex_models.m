5d39a183f1cd6daf61b3a35c68034f76ee829c95
Johan Löfberg <johanl@isy.liu.se>
2018-02-14 09:07:20 +0100

    Fixes #473

--- modules/robust/find_simplex_models.m
+++ modules/robust/find_simplex_models.m
@@ -6,13 +6,22 @@ for i = 1:length(p)
         continue
     elseif any(p{i}.K.q > 0) | any(p{i}.K.s > 0)
         continue
+    elseif p{i}.K.f ~= 1
+        continue
     else
         aux = p{i};
-        aux.F_struc(1:p{i}.K.f,:) = [];
-        aux.K.f = 0;
-        [aux,lower,upper] = find_simple_variable_bounds(aux);
-        if all(lower == 0) | all(upper == 1) & p{i}.K.f == 1 & aux.K.l == 0
-            simplex_model(i)=1;
+        b = aux.F_struc(1,1);
+        a = aux.F_struc(1,2:end);
+        if all(abs(a) == 1)
+            b = b/sign(-a(1));
+            aux.F_struc(1:p{i}.K.f,:) = [];
+            aux.K.f = 0;
+            [aux,lower,upper] = find_simple_variable_bounds(aux);
+            if all(lower == 0) & aux.K.l == 0 & all((upper == b) | isinf(upper))
+                simplex_model(i)=1;
+            end
+        else
+            continue
         end
     end
 end
