906de2004ce1b0424a2b1e24647f13e37aa9b82c
Johan Löfberg <johanl@isy.liu.se>
2018-05-23 09:50:52 +0200

    Don't waste effort to set initials on auxilliary normalizing if none available

--- extras/NormalizeCallback.m
+++ extras/NormalizeCallback.m
@@ -1,23 +1,24 @@
 function F = NormalizeCallback(varargin)
 
-z_normalizing = varargin{end};
-for i = 3:nargin-1
+doAssignInitials = varargin{end};
+z_normalizing = varargin{end-1};
+for i = 3:nargin-2
     if isa(varargin{i},'sdpvar')
         X = varargin{i};
         break
     end
 end
-%X = varargin{3};
 n = length(X);
 if isequal(getbase(X),[spalloc(n,1,0) speye(n)])
     F = lmi([]);
 else
-    dX = value(X);
-    if ~all(isnan(dX))
-        assign(z_normalizing,dX);
+    if doAssignInitials
+        dX = value(X);
+        if ~all(isnan(dX))
+            assign(z_normalizing,dX);
+        end
     end
-    try
-        %[M,m] = derivebounds(X);
+    try        
         F = X == z_normalizing;
     catch
         disp('Report bug in NORMALIZECALLBACK');
