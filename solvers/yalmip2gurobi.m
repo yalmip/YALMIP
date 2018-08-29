1ea4bcca8163b1f0ec96a73372ef469ee5380fa5
Johan Löfberg <johanl@isy.liu.se>
2018-08-29 11:30:13 +0200

    Missing ;

--- solvers/yalmip2gurobi.m
+++ solvers/yalmip2gurobi.m
@@ -118,7 +118,7 @@ if ~isequal(K.q,0)
     top = n_original + 1;
     for i = 1:length(K.q)
         n = K.q(i);      
-        Qi = sparse(top:top+n-1,top:top+n-1,[-1 repmat(1,1,n-1)],length(c),length(c))
+        Qi = sparse(top:top+n-1,top:top+n-1,[-1 repmat(1,1,n-1)],length(c),length(c));
         model.quadcon(i).Qc=Qi;       
         model.quadcon(i).q=zeros(length(c),1);
         model.quadcon(i).rhs=0;
