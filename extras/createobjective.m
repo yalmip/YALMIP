a131b887036a49474eac558a9ee8ff047ec4f59e
Johan Löfberg <johanl@isy.liu.se>
2018-09-04 16:25:03 +0200

    Fixes #526 performance regression in large LP compilation

--- extras/createobjective.m
+++ extras/createobjective.m
@@ -40,7 +40,7 @@ else
             lmi_variables = getvariables(h);
             base = getbase(h);base = base(2:end);
             OKindex = find(~isinf(base));
-            c=base(OKindex(1))*zeros(nvars,1);
+            c=full(base(OKindex(1))*zeros(nvars,1));
             c(lmi_variables) = base;
             Q = spalloc(nvars,nvars,0); 
             f = full(getbasematrix(h,0));
