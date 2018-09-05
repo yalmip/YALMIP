8fc3a7861e748cb5701c52d51104c8fb3fd85f12
Johan Löfberg <johanl@isy.liu.se>
2018-09-05 14:42:23 +0200

    Slightly faster check of psd-test in bnb

--- extras/checkfeasiblefast.m
+++ extras/checkfeasiblefast.m
@@ -36,8 +36,9 @@ if ~isempty(p.F_struc)
         for i = 1:length(p.K.s)
             n = p.K.s(i);
             X = reshape(vecres(top:top+n^2-1),n,n);top = top+n^2;
-            X = (X+X')/2;
-            if min(real(eig(X))) < -tol
+            X = (X+X')/2 + tol*eye(n);
+            [~,fail] = chol(X);
+            if fail%min(real(eig(X))) < -tol
                 return
             end
         end
