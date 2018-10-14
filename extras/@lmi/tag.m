97824eacc11374ab2edf5b27f401ef9a2cb0c812
Johan Löfberg <johanl@isy.liu.se>
2018-10-14 17:00:01 +0200

    Simple trial for general framework fixing QP representation of the Huber operator #570

--- extras/@lmi/tag.m
+++ extras/@lmi/tag.m
@@ -8,6 +8,8 @@ if nargin == 1
     F = F.clauses{1}.handle;
 else
 for i = 1:length(F.clauses)
-    F.clauses{i}.handle = text;
+    if ~isequal(F.clauses{i}.handle,'placeholder')
+        F.clauses{i}.handle = text;
+    end
 end
 end
