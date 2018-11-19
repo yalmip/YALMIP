dd5c77359cabcda3c732b4a98b709f2b2bf1beae
Johan Löfberg <johanl@isy.liu.se>
2018-11-19 15:22:01 +0100

    Fixes memory explosion in bnb

--- modules/global/bnb.m
+++ modules/global/bnb.m
@@ -1357,7 +1357,7 @@ function p = extractBounds(p)
 if ~isempty(p.F_struc)
     [lb,ub,used_rows_eq,used_rows_lp] = findulb(p.F_struc,p.K);
     if ~isempty(used_rows_lp)
-        used_rows_lp = used_rows_lp(~any(full(p.F_struc(p.K.f + used_rows_lp,1+p.nonlinear)),2));
+        used_rows_lp = used_rows_lp(~any((p.F_struc(p.K.f + used_rows_lp,1+p.nonlinear)),2));
         if ~isempty(used_rows_lp)
             lower_defined = find(~isinf(lb));
             if ~isempty(lower_defined)
