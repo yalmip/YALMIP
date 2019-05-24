c39a096e23f149903563e56eb149bc83b4660368
Johan Löfberg <johanl@isy.liu.se>
2019-05-24 08:04:35 +0200

    Fixes #615 fmincon erroneously selected with integer variables

--- extras/@lmi/categorizeproblem.m
+++ extras/@lmi/categorizeproblem.m
@@ -145,7 +145,7 @@ for i = 1:Counter
     
     % Any discrete variables used
     if any_discrete_variables
-        Fvar = getvariables(Fi.data);
+        Fvar = depends(Fi.data);
         int_data = int_data | any(ismembcYALMIP(Fvar,integer_variables));
         bin_data = bin_data | any(ismembcYALMIP(Fvar,binary_variables));
         par_data = par_data | any(ismembcYALMIP(Fvar,parametric_variables));
@@ -218,6 +218,8 @@ for i = 1:Counter
                 types = variabletype(getvariables(Fi.data));
                 if ~any(types)
                     deg = 1;
+                elseif any(types == 3)
+                    deg = NaN;
                 elseif any(types==1) || any(types==2)
                     deg = 2;
                 else
