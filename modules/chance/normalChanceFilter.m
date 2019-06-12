e1b6bdd461d72de8ba16047f545b0c83142e4795
Johan Löfberg <johanl@isy.liu.se>
2019-06-12 18:28:08 +0200

    Strange name on variable corrected

--- modules/chance/normalChanceFilter.m
+++ modules/chance/normalChanceFilter.m
@@ -1,7 +1,7 @@
 function newConstraint = normalChanceFilter(b,c,distribution,confidencelevel,w,options)
 theMean    = distribution.parameters{2};
 covariance = distribution.parameters{3};
-gamma = icdf('normal',confidencelevel,0,1);
+Phi_Inverse = icdf('normal',confidencelevel,0,1);
 if min(size(covariance))==1
     covariance = diag(covariance);
 end
@@ -9,8 +9,8 @@ if isa(covariance,'sdpvar')
     error('Covariance cannot be an SDPVAR in normal distribution. Maybe you meant to use factorized covariance in ''normalf''');
 end
 e = chol(covariance)*c;
-if isa(gamma,'sdpvar')    
-    newConstraint = b + c'*theMean >= gamma*norm_callback(e);    
+if isa(Phi_Inverse,'sdpvar')    
+    newConstraint = b + c'*theMean >= Phi_Inverse*norm_callback(e);    
 else
-    newConstraint =  b + c'*theMean >= gamma*norm(e);
+    newConstraint =  b + c'*theMean >= Phi_Inverse*norm(e);
 end
