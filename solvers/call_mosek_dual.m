07caec843c9ba6946417f44b0f906fecbee680a1
Johan Löfberg <johanl@isy.liu.se>
2019-06-15 19:49:50 +0200

    Fixes #629 Crash on mixed exp-socp in mosek

--- solvers/call_mosek_dual.m
+++ solvers/call_mosek_dual.m
@@ -38,7 +38,7 @@ if model.K.e>0
     for i = 1:model.K.e
         prob.cones.type = [prob.cones.type 3];
         prob.cones.subptr = [prob.cones.subptr length(prob.cones.sub)+1];
-        prob.cones.sub = [prob.cones.sub top+3 top+2 top+1];        
+        prob.cones.sub = [prob.cones.sub(:)' top+3 top+2 top+1];        
         top = top + 3;
     end
 end
