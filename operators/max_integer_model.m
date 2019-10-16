5e2ebbb04d9109adc1ad2c4af37cbfd5c4d6e0a0
Johan Löfberg <johanl@isy.liu.se>
2019-10-16 18:59:10 +0200

    Fixes #661 Implement O(nlog(n)) construction of large integer representations of min/max

--- operators/max_integer_model.m
+++ operators/max_integer_model.m
@@ -1,9 +1,29 @@
-function F = max_integer_model(X,t);
+function [F,M,m] = max_integer_model(X,t,M,m);
 
-[M,m] = derivebounds(X);
+if nargin < 3
+    [M,m] = derivebounds(X);
+end
 
-if all(M==m)
+if all(M==m) 
     F = [t == max(M)];
+    return
+end
+
+if length(X) > 10
+    % Integer model of max(X) has O(n^2) complexity in memory use as there
+    % are n constrants with n non-zeros. Recursive variant has O(nlog(n))
+    mid = floor(length(X)/2);
+    t1 = sdpvar(1);
+    t2 = sdpvar(1);
+    % t1 = max of first half
+    F1 = max_integer_model(X(1:mid),t1,M(1:mid),m(1:mid));
+    % t2 = max of second half
+    F2 = max_integer_model(X(mid+1:end),t2,M(mid+1:end),m(mid+1:end));
+    % and take max of those to max, t = max(t1,t2)
+    M3 = [max(M(1:mid));max(M(mid+1:end))];
+    m3 = [min(m(1:mid));min(m(mid+1:end))];
+    F3 = max_integer_model([t1;t2],t,M3,m3);
+    F = F1 + F2 + F3;   
     return
 end
 
