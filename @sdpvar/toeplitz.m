9fea2b7a071cced55eb4b3a2d695b0afb7dba8c9
Johan Löfberg <johanl@isy.liu.se>
2020-04-01 19:09:39 +0200

    Fixes #697 Toeplitz with vector results wrong dimension

--- @sdpvar/toeplitz.m
+++ @sdpvar/toeplitz.m
@@ -14,7 +14,11 @@ x = [extsubsref(r,p:-1:2) ; reshape(c,prod(size(c)),1)];
 cidx = (0:m-1)';
 ridx = p:-1:1;
 t = cidx(:,ones(p,1)) + ridx(ones(m,1),:);
-t = extsubsref(x,t);
+if isrow(t)
+    t = extsubsref(x,t).';
+else
+    t = extsubsref(x,t)
+end
 if isa(t,'sdpvar')
     t.conicinfo = [0 0];
 end
