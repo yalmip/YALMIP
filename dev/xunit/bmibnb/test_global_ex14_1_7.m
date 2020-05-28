27657e230066bd93530c0055e6a8637a438b200a
Johan Löfberg <johanl@isy.liu.se>
2020-05-28 18:43:32 +0200

    Wrong assertion in test

--- dev/xunit/bmibnb/test_global_ex14_1_7.m
+++ dev/xunit/bmibnb/test_global_ex14_1_7.m
@@ -51,4 +51,4 @@ F = F + (0 <= [x1 x2 x3 x4 x5 x6 x7 x8 x9] <= 10);
 %sol = optimize(F,objvar,sdpsettings('solver','bmibnb','bmibnb.upper','fmincon','allownon',1));
 %assert(sol.problem == 0)
 %assert(abs(value(objvar))<=1e-3)
-assert(1)
+assert(1 == 1)
