199d890972b24ace5bd2737c9edd8e5b6f9439d3
Johan Löfberg <johanl@isy.liu.se>
2020-05-28 13:15:59 +0200

    Fixes #747 inserting ndsdvar into empty fails

--- extras/@ndsdpvar/subsasgn.m
+++ extras/@ndsdpvar/subsasgn.m
@@ -1,6 +1,28 @@
 function X = subsasgn(X,I,Y)
 % SUBSASGN (overloaded)
 
+if isempty(X)
+    y = Y;
+    sizeY = size(Y);
+    y_lmi_variables = y.lmi_variables;    
+    X0 = subsasgn([],I,full(reshape(full(Y.basis(:,1)),sizeY)));
+    dim = size(X0);
+    y.basis = reshape(X0,prod(dim),1);    
+    for i = 1:length(y_lmi_variables)
+        X0 = subsasgn([],I,full(reshape(full(Y.basis(:,i+1)),sizeY)));
+        y.basis(:,i+1) = reshape(X0,prod(dim),1);
+    end
+    y.dim = dim;
+    % Reset info about conic terms
+    y.conicinfo = [0 0];
+    y.basis = sparse(y.basis);
+    if length(dim)>2 && ~isa(y,'ndsdpvar')
+        y = ndsdpvar(y);
+    end
+    y = flush(y);    
+    X = y;
+    return
+end
 base = reshape(1:size(X.basis,1),X.dim);
 base = subsref(base,I);
 
