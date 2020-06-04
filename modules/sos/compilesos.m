81f2daaed8854e4a22728fb99479cfa9b69a5b41
Johan Löfberg <johanl@isy.liu.se>
2020-06-04 20:03:55 +0200

    Fixes #765 automatically sort out infeasible/unbounded flags in solvesos when dualized

--- modules/sos/compilesos.m
+++ modules/sos/compilesos.m
@@ -1,4 +1,4 @@
-function [F,obj,m,everything] = compilesos(F,obj,options,params,candidateMonomials)
+function [F,obj,m,everything,modeltype] = compilesos(F,obj,options,params,candidateMonomials)
 %COMPILESOS Derive sum-of-squares model without solving
 %
 %    [F,obj,m] = compilesos(F,h,options,params,monomials) compiles the SOS
@@ -236,7 +236,10 @@ switch options.sos.model
         options.sos.model = 2;
     otherwise
 end
-             
+  
+% Save this and return to calling function so we know if we have dualized
+modeltype = options.sos.model;
+
 if ~isempty(yalmip('extvariables')) & options.sos.model == 2 & nargin<4
     disp(' ')
     disp('**Using nonlinear operators in SOS problems can cause problems.')
--- modules/sos/solvesos.m
+++ modules/sos/solvesos.m
@@ -109,7 +109,7 @@ if ~isempty(options)
     end
 end
 
-[F,obj,m,everything] = compilesos(F,obj,options,params,candidateMonomials);
+[F,obj,m,everything,modeltype] = compilesos(F,obj,options,params,candidateMonomials);
 
 p = everything.p;
 normp = everything.normp;
@@ -181,21 +181,25 @@ end
 % **********************************************
 if options.verbose > 0
     if sol.problem == 1
-        if options.sos.model == 1
+        if modeltype == 1
             disp(' ')
             disp('-> Solver reported infeasible dual problem.')
-            disp('-> Your SOS problem is probably unbounded.')
-        elseif options.sos.model == 2
+            disp('-> Your SOS problem is probably unbounded (SOS is dualized).')
+            sol.problem = 2;
+            sol.info = strrep(sol.info,'Infeasible problem','Unbounded objective function');
+        elseif modeltype == 2
             disp(' ')
             disp('-> Solver reported infeasible primal problem.')
             disp('-> Your SOS problem is probably infeasible.')
         end
     elseif sol.problem == 2
-        if options.sos.model == 1
+        if modeltype == 1
             disp(' ')
             disp('-> Solver reported unboundness of the dual problem.')
-            disp('-> Your SOS problem is probably infeasible.')
-        elseif options.sos.model == 2
+            disp('-> Your SOS problem is probably infeasible (SOS is dualized).')
+            sol.problem = 1;
+            sol.info = strrep(sol.info,'Unbounded objective function','Infeasible problem');
+        elseif modeltype == 2
             disp(' ')
             disp('-> Solver reported unboundness of the primal problem.')
             disp('-> Your SOS problem is probably unbounded.')            
@@ -203,7 +207,8 @@ if options.verbose > 0
     elseif sol.problem == 12
             disp(' ')
             disp('-> Solver reported unboundness or infeasibility of the primal problem.')
-            disp('-> Your SOS problem is probably unbounded.')            
+            disp('-> Your SOS problem is probably unbounded but to clarify you should')   
+            disp('-> solve the problem again without an objective')            
     end
 end
 
