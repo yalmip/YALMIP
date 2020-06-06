4e18766a2d9bfe875972354d01acb97ddde801b1
Johan Löfberg <johanl@isy.liu.se>
2020-06-06 14:49:24 +0200

    Improved test of inflection region #775

--- modules/global/DeriveVexityFromInflection.m
+++ modules/global/DeriveVexityFromInflection.m
@@ -2,13 +2,16 @@ function vexity = DeriveVexityFromInflection(properties,xL,xU)
 
 % [point (changes to convex = 1/changes to concave = -1) ... ]
 inflections = [-inf properties.inflection(1:2:end) inf];
+convex = [-properties.inflection(2:2:end)  properties.inflection(end)]
 vexity = 'none';
-for k = 1:length(inflections)-1
-    if xL >= inflections(k) && xU <= properties.inflection(k+1)        
-        if properties.inflection(2*k) == -1
+for k = 1:length(inflections)
+    if xL >= inflections(k) && xU <= inflections(k+1)        
+        if convex(k) == 1
             vexity = 'convex';
+            return
         else
             vexity = 'concave';
+            return
         end
     end
 end
