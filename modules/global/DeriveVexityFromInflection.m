64e8673c26d588b717068a89efb8d267ba2ea17d
Johan Löfberg <johanl@isy.liu.se>
2020-06-08 14:51:43 +0200

    Missing ;

--- modules/global/DeriveVexityFromInflection.m
+++ modules/global/DeriveVexityFromInflection.m
@@ -2,7 +2,7 @@ function vexity = DeriveVexityFromInflection(properties,xL,xU)
 
 % [point (changes to convex = 1/changes to concave = -1) ... ]
 inflections = [-inf properties.inflection(1:2:end) inf];
-convex = [-properties.inflection(2:2:end)  properties.inflection(end)]
+convex = [-properties.inflection(2:2:end)  properties.inflection(end)];
 vexity = 'none';
 for k = 1:length(inflections)
     if xL >= inflections(k) && xU <= inflections(k+1)        
