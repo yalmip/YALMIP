008f94ec85efbc13180d392fab0f8502011bf828
Johan Löfberg <johanl@isy.liu.se>
2020-06-11 09:34:45 +0200

    Generalization and robustification of rlt cut

--- modules/global/addMultipliedInequalityCuts.m
+++ modules/global/addMultipliedInequalityCuts.m
@@ -2,7 +2,7 @@ function p = addMultipliedInequalityCuts(p)
 
 newRows = [];
 for i =  p.linears
-    if p.lb(i) >= 0
+    if p.lb(i) >= -1000 | p.ub(i) <= 1000 % Avoid numerically tricky stuff
         for j = 1+p.K.f:p.K.f+p.K.l
             row = p.F_struc(j,:);
             used = find(row(2:end));
@@ -22,11 +22,22 @@ for i =  p.linears
                 end
                 if ok
                     monom = p.bilinears(monom,1);
-                    row(1+monom) = row(1+used);
-                    row(1+used) = 0;
-                    row(1+i) = row(1);
-                    row(1)=0;
-                    newRows = [newRows;row];
+                    row0 = row;
+                    if p.lb(i) >= -1000
+                        row(1+monom) = row(1+used);
+                        row(1+used) = 0;
+                        row(1+i) = row(1);
+                        row(1)=0;
+                        newRows = [newRows;row-p.lb(i)*row0];
+                    end
+                    if p.ub(i) <= 1000
+                        row = row0;
+                        row(1+monom) = row(1+used);
+                        row(1+used) = 0;
+                        row(1+i) = row(1);
+                        row(1)=0;
+                        newRows = [newRows;-row+p.ub(i)*row0];
+                    end
                 end
             end
         end
