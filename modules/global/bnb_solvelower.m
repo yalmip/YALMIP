a4243b6f13aee08eb4b7e87994548545e468764e
Johan Löfberg <johanl@isy.liu.se>
2020-09-04 19:06:11 +0200

    improved presolve when there is a zero on the diagonal

--- modules/global/bnb_solvelower.m
+++ modules/global/bnb_solvelower.m
@@ -152,11 +152,13 @@ if nnz(removethese)>0 & all(p.variabletype == 0) & isempty(p.evalMap)% ~isequal(
     
     if p.K.s(1) > 0
         top = 1 + p.K.f + p.K.l + sum(p.K.q);
+        newEqualities = [];
         for j = 1:length(p.K.s)
             X = p.F_struc(top:top + p.K.s(j)^2-1,:); 
             X = reshape(any(X,2),p.K.s(j),p.K.s(j));
             e = find(~any(X,2));
             if any(e)
+                % Not a single element is used, so simply and reduce SDP
                 Z = spalloc(p.K.s(j),p.K.s(j),length(e)*2*p.K.s(j));
                 for k = 1:length(e);
                     Z(:,e(k))=1;
@@ -165,8 +167,30 @@ if nnz(removethese)>0 & all(p.variabletype == 0) & isempty(p.evalMap)% ~isequal(
                 m = find(Z(:));
                 p.F_struc(top + m - 1,:)=[];
                 p.K.s(j) = p.K.s(j) - length(e);
+            else
+                % Look for zero diagonal. This means we can move all
+                % nonzero elements to a zero equality                
+                e = find(diag(X)==0);
+                if length(e)>0
+                    Z = spalloc(p.K.s(j),p.K.s(j),length(e)*2*p.K.s(j));
+                    for k = 1:length(e);
+                        Z(:,e(k))=1;
+                        Z(e(k),:)=1;
+                    end            
+                    m1 = find(Z(:)); % To be removed
+                    m2 = find(triu(Z,1)); % To be moved
+                    equalityRows = p.F_struc(top + m2 - 1,:);
+                    p.F_struc(top + m1 - 1,:) = [];
+                    p.K.s(j) = p.K.s(j) - length(e);   
+                    equalityRows = equalityRows(find(any(equalityRows,2)),:);
+                    newEqualities = [newEqualities;equalityRows];
+                end
             end
             top = top + p.K.s(j)^2;
+        end
+        if ~isempty(newEqualities)
+            p.F_struc = [newEqualities;p.F_struc];
+            p.K.f = p.K.f + size(newEqualities,1);
         end
     end
                           
