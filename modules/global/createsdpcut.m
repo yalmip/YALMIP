1cefe8215969dad179c2e0dd544ad79a89733ae3
Johan Löfberg <johanl@isy.liu.se>
2020-09-09 13:15:04 +0200

    #799 prepare for socp cuts

--- modules/global/createsdpcut.m
+++ modules/global/createsdpcut.m
@@ -3,48 +3,46 @@ if p.K.s > 0
     top = p.K.f+p.K.l+sum(p.K.q)+1;
     newcuts = 1;
     newF = [];
+    newQF = [];
     for i = 1:length(p.K.s)
         n = p.K.s(i);
         X = p.F_struc(top:top+n^2-1,:)*[1;x];
         X = full(reshape(X,n,n));
         [d,v] = eig(X);
-        for m = 1:length(v)
-            if v(m,m)<0
-                for j = 1:length(x)+1;
-                    newF(newcuts,j)= d(:,m)'*reshape(p.F_struc(top:top+n^2-1,j),n,n)*d(:,m);
-                end
-                % max(abs(newF(:,2:end)),[],2)
-                newF(newcuts,1)=newF(newcuts,1)+1e-6;
-                newcuts = newcuts + 1;
-                if size(p.lpcuts,1)>0
-                    dist = p.lpcuts*newF(newcuts-1,:)'/(newF(newcuts-1,:)*newF(newcuts-1,:)');
-                    if any(abs(dist-1)<1e-3)
-                        newF = newF(1:end-1,:);
-                        newcuts = newcuts - 1;
+        if any(diag(v)<0)
+            for m = 1:length(v)
+                if v(m,m)<0
+                    for j = 1:length(x)+1;
+                        newF(newcuts,j)= d(:,m)'*reshape(p.F_struc(top:top+n^2-1,j),n,n)*d(:,m);
+                    end
+                    newF(newcuts,1)=newF(newcuts,1)+1e-6;
+                    newcuts = newcuts + 1;
+                    if size(p.lpcuts,1)>0
+                        dist = p.lpcuts*newF(newcuts-1,:)'/(newF(newcuts-1,:)*newF(newcuts-1,:)');
+                        if any(abs(dist-1)<1e-3)
+                            newF = newF(1:end-1,:);
+                            newcuts = newcuts - 1;
+                        end
                     end
                 end
             end
+        end
+        if 0% v(1,1)<0
+            tempcut = [];
+            for j = 1:length(x)+1;
+                temp = d(:,1:2)'*reshape(p.F_struc(top:top+n^2-1,j),n,n)*d(:,1:2);
+                tempcut = [tempcut [temp(1,1)+temp(2,2);temp(1,1)-temp(2,2);2*temp(1,2)]];
+            end 
+            newQF = [newQF;tempcut];
         end
         top = top+n^2;
     end
 
     if ~isempty(newF)
         % Don't keep all
-        m = size(newF,2);
-        %  size(p.lpcuts)
+        m = size(newF,2);        
         p.lpcuts = [newF;p.lpcuts];
-        p.cutState = [ones(size(newF,1),1);p.cutState];
-     %   violations = p.lpcuts*[1;x];
-     %   p.lpcuts = p.lpcuts(violations<0.1,:);
-     %   p.cutState = p.cutState(violations<0.1);
-        
-        if size(p.lpcuts,1)>15*m
-            %disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-            %violations = p.lpcuts*[1;x];
-            %[i,j] = sort(violations);
-            %p.lpcuts = p.lpcuts(j(1:15*m),:);
-            %p.cutState = lpcuts = p.lpcuts(j(1:15*m),:);
-            %p.lpcuts = p.lpcuts(end-15*m+1:end,:);
-        end
+        p.cutState = [ones(size(newF,1),1);p.cutState];          
     end
+    p.socpcuts = newQF;
 end
