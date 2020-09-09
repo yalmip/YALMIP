1ed3e1ae0936c6cf6d61e0dd518ee42723720895
Johan Löfberg <johanl@isy.liu.se>
2020-09-09 13:10:55 +0200

    Clean up and refactor to allow more detailed output

--- modules/global/constraint_residuals.m
+++ modules/global/constraint_residuals.m
@@ -1,19 +1,27 @@
-function [res,X] = constraint_residuals(p,x)
+function [res,X,detailed] = constraint_residuals(p,x)
 res= [];
 X = [];
-if p.K.f>0
-    res = -abs(p.F_struc(1:p.K.f,:)*[1;x]);
+detailed.f = [];
+detailed.l = [];
+detailed.q = [];
+detailed.s = [];
+detailed.b = [];
+if p.K.f>0    
+    detailed.f = -abs(p.F_struc(1:p.K.f,:)*[1;x]);
+    res = detailed.f;
 end
 if p.K.l>0
-    res = [res;p.F_struc(p.K.f+1:p.K.f+p.K.l,:)*[1;x]];
+    detailed.l = p.F_struc(p.K.f+1:p.K.f+p.K.l,:)*[1;x];
+    res = [res;detailed.l];
 end
 if nnz(p.K.q)>0
     z = p.F_struc(1 + p.K.f+p.K.l:p.K.f+p.K.l+sum(p.K.q),:)*[1;x];
     top = 1;
     for i = 1:length(p.K.q)
         zi = z(top:top + p.K.q(i)-1);top = top + p.K.q(i);
-        res = [res;zi(1)^2-zi(2:end)'*zi(2:end)];
+        detailed.q = [detailed.q;zi(1)^2-zi(2:end)'*zi(2:end)];
     end
+    res = [res;detailed.q];
 end
 
 if (length(p.K.s)>1) | p.K.s>0
@@ -22,11 +30,9 @@ if (length(p.K.s)>1) | p.K.s>0
         n = p.K.s(i);
         X{i} = p.F_struc(top:top+n^2-1,:)*[1;x];top = top+n^2;
         X{i} = reshape(X{i},n,n);
-        try
-            res = [res;min(eig(X{i}))];
-        catch
-            1
-        end
+        detailed.s = [detailed.s;min(eig(X{i}))];
     end
+    res = [res;detailed.s];
 end
-res = [res;min([p.ub-x;x-p.lb])];
+detailed.b = min([p.ub-x;x-p.lb]);
+res = [res;detailed.b];
