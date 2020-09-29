88a4bde09430a4863f049e6aa3bb1dd6a41c411a
Johan Löfberg <johanl@isy.liu.se>
2020-09-29 13:13:19 +0200

    Some adjustments and refactoring for #818

--- modules/global/branch_and_bound.m
+++ modules/global/branch_and_bound.m
@@ -32,8 +32,10 @@ p = fixOperatorProperties(p);
 % *************************************************************************
 p = addShiftedQP(p);
 if ~isempty(p.shiftedQP)
-    if options.bmibnb.verbose>0    
-        disp('* - Shifted nonconvex QP objective using SDP');
+    if options.bmibnb.verbose>0   
+        if p.options.bmibnb.lowerpsdfix == 2
+            disp('* -Shifted nonconvex QP objective using SDP');
+        end
     end
 end
 
@@ -748,68 +750,73 @@ end
 
 function p = addShiftedQP(p)
 
-% Diagonal shift of nonconvex QP 
+% Diagonal shift of nonconvex QP
 % 0: Do nothing (i.e. will be relaxed as LP in lower bound)
 % 1: Add term to diagonal by eigenalue shift
 % 2: Solve SDP to compute diagonal convexifying shift
-% 3: 
+% 3:
 p.shiftedQP = [];
 [Q,c] = compileQuadratic(p.c,p,0);
-if nnz(Q)>0            
-    if nonconvexQuadratic(Q)        
-        if p.options.bmibnb.lowerpsdfix > 0
-            r = find(any(Q,2));
-            if nnz(Q-diag(diag(Q)))==0
-                % Trivial case, nothing to do really               
-                return
-            elseif p.options.bmibnb.lowerpsdfix == 2
-                x = sdpvar(length(r),1);
-                % FIXME: Use lower level setup
-                ops = p.options;
-                ops.solver = '';
-                ops.verbose = max(ops.verbose-1,0);
-                optimize([Q(r,r) + diag(x) >= 0, x >= 0], sum(x),ops);
-                q = zeros(length(Q),1);
-                q(r) = value(x) + 1e-6;
-            else
-                s = min(eig(Q(r,r)));
-                q = zeros(length(Q),1);
-                q(r) = -s + 1e-9;
-            end
-            Q = Q + diag(q);
-            for i = 1:length(r)
-                j = findrows(p.bilinears(:,2:3),[r(i) r(i)]);
-                if isempty(j)
-                    % Ouch, cannot do this since we don't have these
-                    % monomials in the model. Add them!
-                    newv = length(c)+1;
+if nnz(Q)>0 && p.options.bmibnb.lowerpsdfix
+    r = find(any(Q,2));
+    e = eig(Q(r,r));
+    if min(e) >= 0
+        % Already convex, so keep the compiled matrices
+        p.shiftedQP.Q = Q;
+        p.shiftedQP.c = c;
+    else
+        % Nonconvex case
+        if all(e <= 1e-6)
+            % Does not benefit from any kind of shift
+            return
+        end
+        
+       if p.options.bmibnb.lowerpsdfix == 2
+            x = sdpvar(length(r),1);
+            % FIXME: Use lower level setup
+            ops = p.options;
+            ops.solver = '';
+            ops.verbose = max(ops.verbose-1,0);
+            optimize([Q(r,r) + diag(x) >= 0, x >= 0], sum(x),ops);
+            x = value(x);
+            % SDP solver can fail numerically
+            e = eig(Q(r,r) + diag(x));
+            perturb = max(0,-min(e));
+            q = zeros(length(Q),1);
+            q(r) = value(x) + perturb + 1e-6;
+        else
+            s = min(eig(Q(r,r)));
+            q = zeros(length(Q),1);
+            q(r) = -s + 1e-9;
+        end
+        Q = Q + diag(q);
+        for i = 1:length(r)
+            j = findrows(p.bilinears(:,2:3),[r(i) r(i)]);
+            if isempty(j)
+                % Ouch, cannot do this since we don't have these
+                % monomials in the model. Add them!
+                newv = length(c)+1;
+                if ~isempty(p.F_struc)
                     p.F_struc(end,end+1) = 0;
-                    p.monomtable(end+1,end+1) = 0;
-                    p.monomtable(end,r(i)) = 2;
-                    p.variabletype(end+1) = 2;
-                    p.lb(end+1) = -inf;
-                    p.ub(end+1) = inf;
-                    p.x0(end+1) = 0;
-                    p.bilinears = [p.bilinears;newv r(i) r(i)];
-                    p.monomials = [p.monomials newv];
-                    j = size(p.bilinears,1);
-                    p.c(end+1) = 0;c(end+1) = 0;
-                    p.Q(end+1,end+1) = 0;Q(end+1,end+1) = 0;
-                    p.Quadratics = [p.Quadratics newv];
-                    p.QuadraticsList(newv,:) = [r(i) r(i)];
                 end
-                c(p.bilinears(j,1)) = c(p.bilinears(j,1)) - q(i);
-            end                       
+                p.monomtable(end+1,end+1) = 0;
+                p.monomtable(end,r(i)) = 2;
+                p.variabletype(end+1) = 2;
+                p.lb(end+1) = -inf;
+                p.ub(end+1) = inf;
+                p.x0(end+1) = 0;
+                p.bilinears = [p.bilinears;newv r(i) r(i)];
+                p.monomials = [p.monomials newv];
+                j = size(p.bilinears,1);
+                p.c(end+1) = 0;c(end+1) = 0;
+                p.Q(end+1,end+1) = 0;Q(end+1,end+1) = 0;
+                p.Quadratics = [p.Quadratics newv];
+                p.QuadraticsList(newv,:) = [r(i) r(i)];
+            end
+            c(p.bilinears(j,1)) = c(p.bilinears(j,1)) - q(r(i));
         end
     end
-    if p.options.bmibnb.lowerpsdfix
-        p.shiftedQP.Q = Q;
-        p.shiftedQP.c = c;    
-    else
-        p.shiftedQP.Q = p.Q;
-        p.shiftedQP.c = p.c;
-    end
+    p.shiftedQP.Q = Q;
+    p.shiftedQP.c = c;    
 end
-
-
 
