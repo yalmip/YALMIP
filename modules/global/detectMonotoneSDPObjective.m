313071f6e9c9e246e5881785989b4b072647f787
Johan Löfberg <johanl@isy.liu.se>
2020-10-08 10:25:32 +0200

    Missing ; and logic error

--- modules/global/detectMonotoneSDPObjective.m
+++ modules/global/detectMonotoneSDPObjective.m
@@ -1,7 +1,7 @@
 function p = detectMonotoneSDPObjective(p)
+p.monotoneobjectiveresponse = 0;
 if isempty(p.evalMap) && nnz(p.Q == 0) && all(p.variabletype == 0)
-    s = setdiff(find(p.c),[p.binary_variables p.integer_variables]);
-    p.monotoneobjectiveresponse = 0;
+    s = setdiff(find(p.c),[p.binary_variables p.integer_variables]);   
     if all(p.c(s))>0
         if nnz(p.F_struc(1:p.K.f,1 + s))==0
             if all(all(p.F_struc(p.K.f+1:p.K.f+p.K.l,1 + s)>=0))
@@ -12,7 +12,7 @@ if isempty(p.evalMap) && nnz(p.Q == 0) && all(p.variabletype == 0)
                         B = p.F_struc(top:top+p.K.s(i)^2-1,1+s);
                         % Variable enters at most one element (i.e. diagonal)
                         % and enters with positive sign
-                        working = working==1 && all(sum(B | B,1)<=1) && all(all(B >=0))
+                        working = working==1 && all(sum(B | B,1)<=1) && all(all(B >=0));
                         top = top + p.K.s(i)^2;
                     end
                     if working
