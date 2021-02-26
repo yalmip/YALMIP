46685ee01f9190f312a6ee5fac1214cd3a95df50
Johan Löfberg <johanl@isy.liu.se>
2021-02-26 11:53:36 +0100

    Caused long run time in test if mosek used by tet suite

--- dev/xunit/operators/test_operator_maxlocation.m
+++ dev/xunit/operators/test_operator_maxlocation.m
@@ -3,12 +3,12 @@ tests = functiontests(localfunctions);
 
 function test1(dummy)
 % Test for feature #313
-y = (1:10)';
-x = intvar(10,1);
+y = (1:5)';
+x = intvar(5,1);
 [val,loc] = max(x);
 sol = optimize([sort(x) == y, loc == 5],x'*x)
 assert(sol.problem == 0)
-assert(abs(value(x(5))-10) <= 1e-4);
+assert(abs(value(x(5))-5) <= 1e-4);
 
 [val,loc] = min(x);
 sol = optimize([sort(x) == y, loc == 5],x'*x)
