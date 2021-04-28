75829870e06932b35af771148f5a7d405fe84c68
Johan Löfberg <johanl@isy.liu.se>
2021-04-28 08:50:36 +0200

    Fixes #877 support gradient in atan2

--- @sdpvar/atan2.m
+++ @sdpvar/atan2.m
@@ -4,17 +4,7 @@ switch class(varargin{1})
 
     case 'sdpvar' 
         varargout{1} = yalmip('define','atan2_internal',[varargin{:}]);        
-
-    case 'char'
-
-        X = varargin{3};
-        
-        operator = CreateBasicOperator('callback');
-        operator.range = [-pi pi];
-        
-        varargout{1} = [];
-        varargout{2} = operator;
-        varargout{3} = [X(:)];
+   
     otherwise
         error('SDPVAR/atan2 called with CHAR argument?');
 end
