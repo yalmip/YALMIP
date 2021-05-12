984fea75b223b97cd615fa260e450c2a2438f8d3
Johan Löfberg <johanl@isy.liu.se>
2021-05-12 19:12:06 +0200

    Fix

---
+++ dev/tests/operators/test_operator_abs.m
@@ -0,0 +1,19 @@
+function tests = test_operator_abs
+tests = functiontests(localfunctions);
+
+function test1(dummy)
+
+sdpvar x y
+obj = abs(1+abs(x-5));
+sol = optimize([],obj)
+
+assert(sol.problem == 0)
+assert(abs(value(obj) - 1) <= 1e-4);
+
+function test2(dummy)
+sdpvar x y
+F = (abs(1+abs(x-5)) + abs(y)<=10) 
+obj = -x
+sol = optimize(F,obj)
+assert(abs(value(obj)--14) <= 1e-4);
+
