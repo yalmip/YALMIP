b5fd018ff97b77ea75ae61c9a18bf7cd8cfbdd4c
Johan Löfberg <johanl@isy.liu.se>
2021-05-13 22:00:54 +0200

    Fixes #897 deprecate strict inequalities

--- @sdpvar/lt.m
+++ @sdpvar/lt.m
@@ -1,23 +1,2 @@
 function y = lt(X,Y)
-%LT (overloaded)
-
-if isa(X,'blkvar')
-    X = sdpvar(X);
-end
-
-if isa(Y,'blkvar')
-    Y = sdpvar(Y);
-end
-
-fh = findobj( 'Type', 'Figure', 'Name', 'Kittens don''t like <!' );
-if isempty(fh)
-    figure('Name','Kittens don''t like >!');
-    imshow('strictwarning.jpg')
-end
-warning('YALMIP:strict','Strict inequalities are not supported. A non-strict has been added instead'')');
-
-try
-    y = constraint(X,'<=',Y);
-catch
-    error(lasterr)
-end
+error('Strict inequalities are not supported (<a href="yalmip.github.io/inside/strictinequalities">learn why</a>)')
