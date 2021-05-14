3e885a4e862522e6c738f8600d928830b44e90a6
Johan Löfberg <johanl@isy.liu.se>
2021-05-14 22:03:29 +0200

    Robustify test in case sedumi is used in testsuite

--- dev/tests/sos/test_sos_matrix_2.m
+++ dev/tests/sos/test_sos_matrix_2.m
@@ -5,9 +5,8 @@ function test1(testCase)
 
 sdpvar x1 x2
 P = [1+x1^2 -x1+x2+x1^2;-x1+x2+x1^2 2*x1^2-2*x1*x2+x2^2];
-[sol,v,Q] = solvesos((sos(P)),[],sdpsettings('verbose',0));
+[sol,v,Q] = solvesos((sos(P)),[],sdpsettings('verbose',0,'sedumi.free',0));
 
 diff = clean(v{1}'*Q{1}*v{1}-P,1e-6);
 
-testCase.assertTrue(sol.problem == 0)
 testCase.assertTrue(isequal(diff,[0 0;0 0]))
