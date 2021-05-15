21b7b17d0ccd7490393f6c806d5636c5f11edf3a
Johan Löfberg <johanl@isy.liu.se>
2021-05-15 23:09:18 +0200

    Add tests for cutsdp and bnb

--- dev/tests/bnb/test_bnb.m
+++ dev/tests/bnb/test_bnb.m
@@ -1,4 +1,4 @@
-function tests = test_bnb_milp_1
+function tests = test_bnb
 tests = functiontests(localfunctions);
 
 function test_milp(testCase)
@@ -205,3 +205,25 @@ sol = optimize(F+(integer(x)),D,ops);
 testCase.assertTrue(sol.problem == 0);
 testCase.assertTrue(all(abs(value(x) - [ 2     3     3     3     2     3     3]') <= 1e-3));
 testCase.assertTrue(abs(value(D)-(8+1/3)) <= 1e-3);
+
+
+function test_diw(testCase)
+[F,h] = loadsdpafile('diw_15.dat-s');
+sol = optimize(F,h,sdpsettings('solver','bnb'));
+testCase.assertTrue(sol.problem == 0);
+testCase.assertTrue(abs(value(h) - -95) <= 1e-2);
+function test_colon(testCase)
+[F,h] = loadsdpafile('coloncancer_1_100_5.dat-s')
+sol = optimize(F,h,sdpsettings('solver','bnb'));
+testCase.assertTrue(sol.problem == 0);
+testCase.assertTrue(abs(value(h) - 127.47) <= 1e-2);
+function test_clique(testCase)
+[F,h] = loadsdpafile('clique_20_k3_6_7.dat-s')
+sol = optimize(F,h,sdpsettings('solver','bnb'));
+testCase.assertTrue(sol.problem == 0);
+testCase.assertTrue(abs(value(h) - 147) <= 1e-2);
+function test_bar(testCase)
+[F,h] = loadsdpafile('2x3_3bars.dat-s')
+sol = optimize(F,h,sdpsettings('solver','bnb'));
+testCase.assertTrue(sol.problem == 0);
+testCase.assertTrue(abs(value(h) - 2.118) <= 1e-2);
