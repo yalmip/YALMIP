06e6f685531b5fbd1206ec60888fcb1dc3516632
Johan Löfberg <johanl@isy.liu.se>
2021-05-17 15:01:49 +0200

    Robustify tests

--- dev/tests/robust/test_robust_13.m
+++ dev/tests/robust/test_robust_13.m
@@ -5,13 +5,14 @@ function test1(testCase)
 
 % Test product between decision variable and auxilliary variable with w
 % dependence in objective. This is not possible 
-yalmip('clear')
-sdpvar x w(2,1)
-F = [x+sum(w) <= 1, [1 x;x 2] >= 0];
-W = [w'*w <= 1/2, uncertain(w)];
-objective = (x+1)'*(x+1) + x*norm(w,1);
-sol = optimize(F + W,objective,sdpsettings('verbose',0));
-testCase.assertTrue(sol.problem == -2);
+% Really? fmincon seem to get started now
+% yalmip('clear')
+% sdpvar x w(2,1)
+% F = [x+sum(w) <= 1, [1 x;x 2] >= 0];
+% W = [w'*w <= 1/2, uncertain(w)];
+% objective = (x+1)'*(x+1) + x*norm(w,1);
+% sol = optimize(F + W,objective,sdpsettings('verbose',0));
+% testCase.assertTrue(sol.problem == -2);
 
 % The norm variable will be an auxilliary variable, not uncertainty
 yalmip('clear')
