15e4cc3fffecaa5288dfa64d823f01c6183e2195
Johan Löfberg <johanl@isy.liu.se>
2021-05-18 20:13:31 +0200

    Delete old test library

--- dev/xunit/dualize/test_dualize_sdp_10.m
+++
@@ -1,9 +0,0 @@
-function tests = test_sdpvar_dualize_sdp_10
-tests = functiontests(localfunctions);
-
-function test1(dummy)
-
-K = sdpvar(4);
-sol = optimize([K>=0,K(2,3)==1],trace(K)+norm(K(:)),sdpsettings('dualize',1));
-
-assert(sol.problem == 0);
