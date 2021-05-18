ecc3c3f9e4ddd88a4bae39356f2961e1b512310e
Johan Löfberg <johanl@isy.liu.se>
2021-05-18 12:25:38 +0200

    Add polytope utility function

---
+++ @sdpvar/polytope.m
@@ -0,0 +1,21 @@
+function [P,x] = polytope(x,v)
+% polytope  Creates a constraint defining a polytope from vertices
+%
+% P = polytope(x,v)
+%
+% Short for P = [x == v*s, sum(s==1), s >= 0]
+%
+% Note that it is not projected to x but lives in (x,s)
+%
+% See also VERTEX, BOUNDINGBOX, CHEBYBALL
+
+if size(v,2)==length(x)
+    warning('Dimension of appears flipped, so I transposed it');
+    v = v';
+elseif ~size(v,1)==length(x)
+    error('Dimensions on x and vertices do not match');
+end
+
+x = reshape(x,[],1);
+s = sdpvar(size(v,2),1);
+P = [x == v*s, sum(s) == 1, s >= 0];
