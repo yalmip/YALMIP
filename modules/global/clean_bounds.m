8b1a5c0f54f6ba35b2cbf81b14ea7a340b8f17f5
Johan Löfberg <johanl@isy.liu.se>
2022-07-01 10:39:47 +0200

    Fixes #1146 Trivial QP in bmibnb fails

--- modules/global/clean_bounds.m
+++ modules/global/clean_bounds.m
@@ -1,9 +1,10 @@
 function p = clean_bounds(p)
-close = 1e-10>abs(p.ub - round(p.ub));
-p.ub(close) = round(p.ub(close));
-
-close = 1e-10>abs(p.lb - round(p.lb));
-p.lb(close) = round(p.lb(close));
-p.ub(p.binary_variables) = floor(p.ub(p.binary_variables) + 1e-2);
-p.lb(p.lb<-1e12) = -inf;
-p.ub(p.ub>1e12) = inf;
+if 0 % Scary. This is not correct, casuses #1146
+    close = 1e-10>abs(p.ub - round(p.ub));
+    p.ub(close) = round(p.ub(close));
+    close = 1e-10>abs(p.lb - round(p.lb));
+    p.lb(close) = round(p.lb(close));
+    p.ub(p.binary_variables) = floor(p.ub(p.binary_variables) + 1e-2);
+    p.lb(p.lb<-1e12) = -inf;
+    p.ub(p.ub>1e12) = inf;
+end
