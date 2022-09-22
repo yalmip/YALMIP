30bcfe8da431aed55f749224014dc18752c10e8a
Lucas Egidio <lucasegidio1@gmail.com>
2022-09-22 15:01:51 +0200

    added registerjacobian

--- @sdpvar/linearize.m
+++ @sdpvar/linearize.m
@@ -25,12 +25,19 @@ p0 = double(p);
 n = size(p,1);
 m = size(p,2);
 
+if ~isfield(p.extra, 'jacobian')
+    p.extra.jacobian = jacobian(p,x);
+    % assignin('caller', inputname(1), p) 
+    % this would automatically register the jacobian for the caller but 
+    % this may be an unwanted behavior so I commented it out
+end
+J = value(p.extra.jacobian);
+
 if min(n,m)>1
     plin = [];
-    J = value(jacobian(p,x));
     for i = 1:m
         plin = [plin p0(:,i)+squeeze(J(:,i,:))*(x-x0)];
     end
 else
-    plin = p0+double(jacobian(p,x))*(x-x0);
+    plin = p0+double(J)*(x-x0);
 end
