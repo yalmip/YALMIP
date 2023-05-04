574cdd86c81717db645552146db7cfdbdd1d0b74
Johan Löfberg <johanl@isy.liu.se>
2023-05-04 10:15:18 +0200

    Add some more presolve for empty rows

--- modules/global/presolve_empty_rows.m
+++ modules/global/presolve_empty_rows.m
@@ -1,9 +1,13 @@
 function p = presolve_empty_rows(p)
 
 if p.K.f > 0
-    j = find(~any(p.F_struc(1:p.K.f,:),2));
+    j = find(abs(p.F_struc(1:p.K.f,1))<eps & ~any(p.F_struc(1:p.K.f,2:end),2));
     p.F_struc(j,:)=[];
     p.K.f = p.K.f - length(j);
 end
 
-% FIXME: Add LP
+if p.K.l > 0
+    j = find(abs(p.F_struc(startofLPCone(p.K):endOfLPCone(p.K),1)<eps) & ~any(p.F_struc(startofLPCone(p.K):endOfLPCone(p.K),2:end),2));
+    p.F_struc(p.K.f + j,:)=[];
+    p.K.l = p.K.l - length(j);
+end
