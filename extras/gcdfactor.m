2202bbf7cae3325b4d2b496b800a92ef39d7e8ce
Johan Löfberg <johanl@isy.liu.se>
2023-05-23 09:26:36 +0200

    Make sure to bail early when non-integer

--- extras/gcdfactor.m
+++ extras/gcdfactor.m
@@ -1,7 +1,13 @@
 function d = gcdfactor(A)
 d = A(1);
+if d~=round(d)
+    d = 1;return
+end
 for n = 2:numel(A)
-    if d == 1
+    if d == 1 
+        return
+    elseif A(n)~=round(A(n))
+        d = 1;
         return
     else
         d = gcd(d,A(n));
