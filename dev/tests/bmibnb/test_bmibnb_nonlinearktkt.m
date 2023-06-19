566b11c26cff404e65dc34e11040e1535f263cbb
Johan Löfberg <johanl@isy.liu.se>
2023-06-19 08:41:03 +0200

    Test was not setup with options structure

--- dev/tests/bmibnb/test_bmibnb_nonlinearktkt.m
+++ dev/tests/bmibnb/test_bmibnb_nonlinearktkt.m
@@ -12,6 +12,6 @@ G = [x1^2+2*x2-4<=0;-x1<=0;-x2<=0];
 f = 2*x1^2+y1^2-5*y2;
 g = [-x1^2+2*x1-x2^2+2*y1-y2-3<=0;-x2-3*y1+4*y2+4<=0;-y1<=0;-y2<=0];
 K = kkt(g,f,[x1 x2]);
-sol = optimize([G,K,g],F);
+sol = optimize([G,K,g],F,ops);
 testCase.assertTrue(sol.problem == 0)
 testCase.assertTrue(abs(value(F)--12.6787) <= 1e-2) 
