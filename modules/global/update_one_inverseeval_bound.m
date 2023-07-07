4c95a068325f00ce406fa8378f87f3b423c3c148
Johan Löfberg <johan.lofberg@liu.se>
2023-07-07 09:06:31 +0200

    Handle edge case of inverse bound propagation

--- modules/global/update_one_inverseeval_bound.m
+++ modules/global/update_one_inverseeval_bound.m
@@ -15,7 +15,12 @@ if ~all(xL==xU)
                 p.ub(arg) = min(p.ub(arg),invfiU);
             elseif strcmpi(p.evalMap{i}.properties.monotonicity,'decreasing')
                 invfiL = real(feval(p.evalMap{i}.properties.inverse,fU,p.evalMap{i}.arg{2:end-1}));
-                invfiU = real(feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1}));
+                if isequal(fL,p.evalMap{i}.properties.range(1))
+                    % Edge-case, decreasing towards limit
+                    invfiU = p.evalMap{i}.properties.domain(2);
+                else
+                    invfiU = real(feval(p.evalMap{i}.properties.inverse,fL,p.evalMap{i}.arg{2:end-1}));
+                end
                 p.lb(arg) = max(p.lb(arg),invfiL);
                 p.ub(arg) = min(p.ub(arg),invfiU);
             end
