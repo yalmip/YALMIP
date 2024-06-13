1bdc7029e9729dce07bf61c59bfeddba64e0545c
Johan Löfberg <johan.lofberg@liu.se>
2024-06-13 12:51:24 +0200

    Add flag to reduce furter

--- modules/global/smashFixed.m
+++ modules/global/smashFixed.m
@@ -5,6 +5,23 @@ if p.feasible && ~isempty(p.F_struc)
         p.F_struc(:,1) = p.F_struc(:,1) + p.F_struc(:,1+r)*p.lb(r);
         if nargin == 1 || strcmp(action,'keep')
             p.F_struc(:,1+r) = 0;
+        elseif strcmp(action,'reduce')
+            % Not used, just testing
+            p.F_struc(:,1+r) = [];
+            p.f = p.f + p.c(r)'*p.lb(r);
+            p.c(r) = [];
+            p.lb(r)=[];
+            p.ub(r)=[];                         
+            p.Q(:,r)=[];
+            p.Q(r,:)=[];
+            p.variabletype(r) = [];
+            p.monomtable(r,:) = [];
+            p.monomtable(:,r) = [];
+            kept = setdiff(1:length(p.c),r);
+            [~,p.binary_variables] = find(ismember(kept,p.binary_variables));
+            [~,p.integer_variables] = find(ismember(kept,p.binary_variables));
+            p = presolve_strengthen_coefficients(p);
+            p = presolve_empty_rows(p);                   
         else
             p.F_struc(:,1+r) = [];
         end
