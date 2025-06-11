dc6d91ffe3e8ce56c68a70d28f88dc8d61c3949e
Filipe Barbosa <filipe.barbosa@liu.se>
2025-06-11 17:03:23 +0200

    Added log term to account for the asymptotic behavior of the root(probit)

--- modules/chance/normalChanceFilterConicFormulationRoot.m
+++ modules/chance/normalChanceFilterConicFormulationRoot.m
@@ -2,17 +2,6 @@ function [newConstraint] = normalChanceFilterConicFormulationRoot(c,b,gamma)
 %NORMALCHANCEFILTERCONICFORMULATIONROOT Exponential cone formulation of the
 %disjoint contraints using sqrt(probit).
 
-if isa(gamma,'sdpvar')
-    aa = -0.163460523135549;
-    bb = 1.996987289085205e+03;
-    cc = -1.232492830923356;
-    kk = 1.898392103622973;
-    rootPhi_Inverse = aa*lambertw(bb*gamma)+kk+cc*gamma;
-else
-    % The approximation will be the exact value if gamma is fixed
-    rootPhi_Inverse = sqrt(icdf('normal',1-gamma,0,1));
-end
-
 % probability(b(x) + c(x)'*w >= 0)...
 
 % separate c0 and ci
@@ -20,6 +9,7 @@ data = getbase(c);
 c0 = data(:,1);
 ci = data(:,2:end);
 
+% pre-processing
 a = 0;
 if isa(c,'sdpvar')
     % compute ri = ||c0+ci|| corresponding to the feedbacks
@@ -36,9 +26,25 @@ else
     a = inv(r);
 end
 
-sdpvar t; % epigraph variable
+sdpvar t y; % epigraph variable
 
-newConstraint = [norm([b-a,2*t]',2) <= b+a,
-    rootPhi_Inverse <= t];
+% approximating and formulating
+if isa(gamma,'sdpvar')
+    aa = -0.099181619811223;
+    bb = 2.746525371530841e+03;
+    cc = -1.305888804476780;
+    kk = 1.579829567439729;
+    dd = -0.043478545831448;
+    rootPhi_Inverse = aa*lambertw(bb*gamma)+kk+cc*gamma+dd*y;
+
+    newConstraint = [norm([b-a,2*t]',2) <= b+a,
+        rootPhi_Inverse <= t,
+        expcone([y;1;gamma])];
+else
+    % The approximation will be the exact value if gamma is fixed
+    rootPhi_Inverse = sqrt(icdf('normal',1-gamma,0,1));
+
+    newConstraint = [norm([b-a,2*t]',2) <= b+a,
+        rootPhi_Inverse <= t];
 end
-
+end
