0f366225e0b4e2e1be047ca14d7466a1f072f563
Filipe Barbosa <filipe.barbosa@liu.se>
2025-06-11 17:03:44 +0200

    Added log term to account for the asymptotic behavior of the log(probit)

--- modules/chance/normalChanceFilterConicFormulationLog.m
+++ modules/chance/normalChanceFilterConicFormulationLog.m
@@ -2,17 +2,6 @@ function [newConstraint] = normalChanceFilterConicFormulationLog(c,b,gamma)
 %NORMALCHANCEFILTERCONICFORMULATIONLOG Exponential cone formulation of the
 %disjoint contraints using an approximation of log(probit).
 
-if isa(gamma,'sdpvar')
-    aa = -0.196671288384826;
-    bb = 1.674000760107396e+03;
-    cc = -2.899789120102472;
-    kk = 1.283146603876050;
-    logPhi_InverseApproximation = aa*lambertw(bb*gamma)+kk+cc*gamma;
-else
-    % The approximation will be the exact value if gamma is fixed
-    logPhi_InverseApproximation = log(icdf('normal',1-gamma,0,1));
-end
-
 % probability(b(x) + c(x)'*w >= 0)...
 
 % separate c0 and ci
@@ -20,6 +9,7 @@ data = getbase(c);
 c0 = data(:,1);
 ci = data(:,2:end);
 
+% pre-processing...
 a = 0;
 if isa(c,'sdpvar')
     % compute ri = ||c0+ci|| corresponding to the feedbacks
@@ -35,9 +25,28 @@ else
     a = log(r);
 end
 
-sdpvar z t; % epigraph variables
+sdpvar z t y; % epigraph variables
 
-newConstraint = [a + logPhi_InverseApproximation <= t,
-    expcone([t;1;z]),
-    z == b];
+% approximating and formulating
+if isa(gamma,'sdpvar')
+    aa = -0.126076585404480;
+    bb = 3.641618927421549e+02;
+    cc = -2.889803452561948;
+    kk = 0.718561982271681;
+    dd = -0.065102814535832;
+
+    logPhi_InverseApproximation = aa*lambertw(bb*gamma)+kk+cc*gamma+dd*y;
+
+    newConstraint = [a + logPhi_InverseApproximation <= t,
+        expcone([t;1;z]),
+        z == b,
+        expcone([y;1;gamma])];
+else
+    % The approximation will be the exact value if gamma is fixed
+    logPhi_InverseApproximation = log(icdf('normal',1-gamma,0,1));
+
+    newConstraint = [a + logPhi_InverseApproximation <= t,
+        expcone([t;1;z]),
+        z == b];
+end
 end
