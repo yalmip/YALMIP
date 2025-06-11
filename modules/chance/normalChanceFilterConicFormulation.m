3f19ed361274f9b515dcec751d2f8bb19fce43ed
Filipe Barbosa <filipe.barbosa@liu.se>
2025-06-11 17:02:19 +0200

    Added log term to account for the asymptotic behavior of the probit

--- modules/chance/normalChanceFilterConicFormulation.m
+++ modules/chance/normalChanceFilterConicFormulation.m
@@ -2,19 +2,24 @@ function [newConstraint] = normalChanceFilterConicFormulation(b,c,e,theMean,gamm
 %NORMALCHANCEFILTERCONICAPPROXIMATION Exponential cone formulation using
 %an approximation of the probit function.
 
+sdpvar y % epigraph variable for asymptotic bahavior
 
 % One upper bound...
 if isa(gamma,'sdpvar')
-    aa = 0.499492956059166;
-    bb = 8.082867432374761e+03;
-    cc = -1.475743096725997;
-    kk =  3.965651977413067;
-    Phi_InverseApproximation = -aa*lambertw(bb*gamma)+kk+cc*gamma;
+    aa = -0.261576448571477;
+    bb = 3.488899999998545e+05;
+    cc = -1.868722313969487;
+    kk =  3.405748225416125;
+    dd = -0.128360154389090;
+
+    Phi_InverseApproximation = aa*lambertw(bb*gamma)+kk+cc*gamma+dd*y;
+   
+    newConstraint = [b + c'*theMean >= Phi_InverseApproximation*norm(e),
+        expcone([y;1;gamma])];
 else
     % The approximation will be the exact value if gamma is fixed
     Phi_InverseApproximation = icdf('normal',1-gamma,0,1);
+    newConstraint =  b + c'*theMean >= Phi_InverseApproximation*norm(e);
 end
-
-newConstraint =  b + c'*theMean >= Phi_InverseApproximation*norm(e);
 end
 
