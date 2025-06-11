de2a7cf366cf85b64859fec9bab5e52eed9b7db7
Filipe Barbosa <filipe.barbosa@liu.se>
2025-06-11 17:02:57 +0200

    Added log term to account for the asymptotic behavior of the inv(probit)

--- modules/chance/normalChanceFilterConicFormulationInv.m
+++ modules/chance/normalChanceFilterConicFormulationInv.m
@@ -2,17 +2,6 @@ function [newConstraint] = normalChanceFilterConicFormulationInv(c,b,gamma)
 %NORMALCHANCEFILTERCONICFORMULATIONINV Exponential cone formulation of the
 %disjoint contraints using an approximation of 1/probit.
 
-if isa(gamma,'sdpvar')
-    aa = 0.050229622348771;
-    bb = 7.573772400040184e+04;
-    cc = 2.732774841525416;
-    kk = 0.150527341988232;
-    invPhi_InverseApproximation = aa*lambertw(bb*gamma)+kk+cc*gamma;
-else
-    % The approximation will be the exact value if gamma is fixed
-    invPhi_InverseApproximation = inv(icdf('normal',1-gamma,0,1));
-end
-
 % probability(b(x) + c(x)'*w >= 0)...
 
 % separate c0 and ci
@@ -20,6 +9,7 @@ data = getbase(c);
 c0 = data(:,1);
 ci = data(:,2:end);
 
+% pre-processing
 a = 0;
 if isa(c,'sdpvar')
     % compute ri = ||c0+ci|| corresponding to the feedbacks
@@ -35,8 +25,26 @@ else
     a = sqrt(r);
 end
 
-sdpvar t z; % epigraph variables
+sdpvar t y; % epigraph variables
 
-newConstraint = [norm([b-t,2*a]) <= b + t,
-    t <= invPhi_InverseApproximation];
+% approximating and formulating
+if isa(gamma,'sdpvar')
+    aa = 0.045463730850896;
+    bb = 1.186500274690110e+03;
+    cc = 2.676791341187682;
+    kk = 0.374878600022031;
+    dd = 0.012244142944388;
+
+    invPhi_InverseApproximation = aa*lambertw(bb*gamma)+kk+cc*gamma+dd*y;
+
+    newConstraint = [norm([b-t,2*a]) <= b + t,
+        t <= invPhi_InverseApproximation,
+        expcone([y;1;gamma])];
+else
+    % The approximation will be the exact value if gamma is fixed
+    invPhi_InverseApproximation = inv(icdf('normal',1-gamma,0,1));
+
+    newConstraint = [norm([b-t,2*a]) <= b + t,
+        t <= invPhi_InverseApproximation];
+end
 end
