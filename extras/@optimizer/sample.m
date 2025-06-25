6b8fe8c738c117a0e08ef0b42926ccfcf740ab6b
Johan Löfberg <johanl@isy.liu.se>
2025-06-25 10:12:39 +0200

    Clean up data sampler for new notation on Gaussians

--- extras/@optimizer/sample.m
+++ extras/@optimizer/sample.m
@@ -14,13 +14,15 @@ function self = sample(self,N)
 %
 %   EXAMPLE
 %    sdpvar x(2,1) w(2,1)
-%    F = [-10 <= x + w <= 10, uncertain(w,'unif',2,3)]
+%    F = [-10 <= x + w <= 10, uncertain(w,'uniform',-1,1)]
 %    P = optimizer(F,sum(x),[],w,x)
+%    % Should shrink the box to roughly -9<=x<=9
 %    Q = sample(P,10);
 %    plot(Q);
+%    % Solve problem (note, there are no parameters!)
 %    xoptimal = Q([]);
 %
-%   See also OPTIMIZER, UNCERTAIN
+%   See also OPTIMIZER, UNCERTAIN, PROBABILITY
 
 if nargin < 2
     N = 1;
@@ -32,35 +34,6 @@ for k = 1:N
         if isfield(self.input,'stochastics')
             if ~isempty(self.input.stochastics{i});
                 sampledData = dataSampler(self.input.stochastics{i},self.diminOrig{i});
-%                 temp = {self.input.stochastics{i}.name,self.input.stochastics{i}.parameters{:},self.diminOrig{i}};
-%                 if strcmp(func2str(temp{1}),'random')
-%                     % We're most likely using a standard distribution from
-%                     % the random command. However, we must check for some
-%                     % additions to the standard cases
-%                     % 1. 'normalf' distribution, factorized matrix covariance
-%                     % 2. 'normalm' distribution, matrix covariance
-%                     % 3. sdpvar objects in arguments
-%                     if strcmp(temp{2},'normal')                        
-%                         sampledData = feval(temp{1:2},0,1,temp{5});
-%                         sampledData = sqrtm(temp{4})*sampledData;
-%                         sampledData = sampledData + temp{3};
-%                     elseif strcmp(temp{2},'normalf')
-%                         sampledData = feval(temp{1:2},0,1,temp{5});
-%                         sampledData = temp{4}*sampledData;
-%                         sampledData = sampledData + temp{3};
-%                     elseif strcmp(temp{2},'normalm')
-%                         sampledData = feval(temp{1:2},0,1,temp{5});
-%                         sampledData = chol(temp{4})*sampledData;
-%                         sampledData = sampledData + temp{3};
-%                     elseif ~any(cellfun('isclass',temp,'sdpvar'))
-%                         % simple numerical sample of standard distriution
-%                         sampledData = feval(temp{:});
-%                     else
-%                         error('Cannot sample from this description')
-%                     end
-%                 else
-%                     sampledData = feval(temp{:});
-%                 end
                 cells{i} = sampledData;
             else
                 cells{i} = [];
